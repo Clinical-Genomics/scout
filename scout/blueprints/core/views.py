a1f592e251f688bee3f34c9240345bd7adf84030
Robin Andeer <robin.andeer@gmail.com>
2014-10-13 08:09:06 +0200

    add fixtures data for testing

--- scout/blueprints/core/views.py
+++ scout/blueprints/core/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+import os
+import io
+import json
 
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 from ...decorators import templated
 
@@ -12,4 +15,10 @@ core = Blueprint('core', __name__, template_folder='templates')
 @templated('cases.html')
 def cases():
   """View all cases."""
-  return dict()
+  # fetch variants from the data store
+  project_root = '/'.join(current_app.root_path.split('/')[0:-1])
+  data_path = os.path.join(project_root, 'tests/fixtures/variants.json')
+  with io.open(data_path, encoding='utf-8') as handle:
+    variants = json.load(handle)
+
+  return dict(variants=variants)
