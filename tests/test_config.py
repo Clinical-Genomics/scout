8680c39cd5d13d5488624eb31c5e052acb4d4965
Robin Andeer <robin.andeer@gmail.com>
2014-10-17 07:59:44 +0200

    make tests pass

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from scout.app import create_app
-from scout.config import BaseConfig, DefaultConfig
+from scout.config import BaseConfig
 
 
-def test_production_config():
-  assert BaseConfig.PROJECT == 'prod'
+def test_base_config():
+  """Test base config values."""
+  assert BaseConfig.PROJECT == 'scout'
+  assert BaseConfig.DEBUG == False
