5de7ff2eab3dff826facd6b915052b803a3ecda4
Robin Andeer <robin.andeer@gmail.com>
2014-10-23 10:01:55 +0200

    update core views + templates

--- scout/blueprints/core/views.py
+++ scout/blueprints/core/views.py
@@ -14,3 +14,26 @@ def cases():
   """View all cases."""
   # fetch cases from the data store
   return dict(cases=current_app.db.cases())
+
+
+@core.route('/cases/<case_id>')
+@templated('case.html')
+def case(case_id):
+  """View one specific case."""
+  # fetch a single, specific case from the data store
+  return dict(case=current_app.db.case(case_id))
+
+
+@core.route('/<case_id>/variants')
+@templated('variants.html')
+def variants(case_id):
+  """View all variants for a single case."""
+  # fetch all variants for a specific case
+  return dict(variants=current_app.db.variants(case_id))
+
+
+@core.route('/variants/<variant_id>')
+@templated('variant.html')
+def variant(variant_id):
+  """View a single variant in a single case."""
+  return dict(variant=current_app.db.variant(variant_id))
