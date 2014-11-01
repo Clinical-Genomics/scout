c89d216c2a0bc380a0d8ce6dc5c2f8581eb6a859
Robin Andeer <robin.andeer@gmail.com>
2014-11-01 16:19:41 +0100

    redesign cases view

--- scout/blueprints/core/views.py
+++ scout/blueprints/core/views.py
@@ -11,7 +11,11 @@ core = Blueprint('core', __name__, template_folder='templates')
 @core.route('/cases')
 @templated('cases.html')
 def cases():
-  """View all cases."""
+  """View all cases.
+
+  The purpose of this page is to display all cases related to an
+  institute. It should also give an idea of which
+  """
   # fetch cases from the data store
   return dict(cases=current_app.db.cases())
 
--- scout/blueprints/frontend/__init__.py
+++ scout/blueprints/frontend/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-The Frontend blueprint takes case of all views that are available
-without a login. This includes the landing page and about etc.
+The Frontend blueprint takes care of all views that are available
+without a login. This currently only includes the landing page.
 """
 from __future__ import absolute_import, unicode_literals
 
