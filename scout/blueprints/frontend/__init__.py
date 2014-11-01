ec872f7f86d460c628306deeafc2cc5a8b905d2d
Robin Andeer <robin.andeer@gmail.com>
2014-11-01 07:56:51 +0100

    add mkdocs master to pip install

--- scout/blueprints/frontend/__init__.py
+++ scout/blueprints/frontend/__init__.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+The Frontend blueprint takes case of all views that are available
+without a login. This includes the landing page and about etc.
+"""
 from __future__ import absolute_import, unicode_literals
 
 from .core import init_blueprint
