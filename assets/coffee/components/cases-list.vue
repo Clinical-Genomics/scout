e8cb552ad24807c59bd601c17c8968e52b784793
Robin Andeer <robin.andeer@gmail.com>
2014-11-18 07:42:44 +0100

    update cases list to match new models

--- assets/coffee/components/cases-list.vue
+++ assets/coffee/components/cases-list.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-repeat="cases | filterBy filterKey" class="cases-item">
 
-      <a class="cases-item-title" href="{{ baseUrl }}/{{ name }}">
+      <a class="cases-item-title" href="{{baseUrl}}/{{display_name}}">
         <div class="cases-item-indicator"></div>
-        {{name}}
+        {{display_name}}
       </a>
 
       <div class="cases-item-bonus">
 
-        <a href="{{ baseUrl }}/{{ name }}/variants" class="cases-item-link">
+        <a href="{{baseUrl}}/{{display_name}}/variants" class="cases-item-link">
           Variants
         </a>
 
