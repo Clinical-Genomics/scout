ddfc4c213ab727508ef680f9384916169464bf17
Robin Andeer <robin.andeer@gmail.com>
2014-12-08 09:39:09 +0100

    add tasks

--- tasks.py
+++ tasks.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, unicode_literals
 from invoke import run, task
 from invoke.util import log
 
+from mongoengine import connect
+from scout.models import User, Whitelist, Institute
+
 
 @task
 def test():
@@ -14,6 +17,34 @@ def test():
 def testall():
   """test-all - run tests on every Python version with tox."""
   run('tox')
+
+
+@task(name='init-data')
+def init_data():
+  """Bootstrap the database with demo data."""
+  # load database with cases, variants, and a default institute
+  for case in [21, 22, 23, 31, 32, 33, 34, 51, 53, 54]:
+    run("python -m scout.ext.backend.load_mongo "
+        "./tests/vcf_examples/%(case)d/variants.vcf "
+        "./tests/vcf_examples/%(case)d/%(case)d_pedigree.txt "
+        "./configs/config_test.ini -type cmms" % dict(case=case))
+
+
+@task(name='add-user')
+def add_user(email, name='Paul Anderson'):
+  """Setup a new user for the database with a default institute."""
+  connect('variantDatabase', host='localhost', port=27017)
+
+  institute = Institute.objects.first()
+  Whitelist(email=email).save()
+  # create a default user
+  user = User(
+    email=email,
+    name=name,
+    roles=['admin'],
+    institutes=[institute]
+  )
+  user.save()
 
 
 @task
