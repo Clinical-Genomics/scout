8a699264668a1145770677a1801808ffb671452b
Mans Magnusson <monsunas@gmail.com>
2014-12-19 16:50:29 +0100

    New structure works

--- scout/models/variant.py
+++ scout/models/variant.py
@@ -104,63 +104,6 @@ class Gene(EmbeddedDocument):
   sift_prediction = StringField(choices=CONSEQUENCE)
   polyphen_prediction = StringField(choices=CONSEQUENCE)
 
-SO_TERMS = (
-  'transcript_ablation',
-  'splice_donor_variant',
-  'splice_acceptor_variant',
-  'stop_gained',
-  'frameshift_variant',
-  'stop_lost',
-  'initiator_codon_variant',
-  'transcript_amplification',
-  'inframe_insertion',
-  'inframe_deletion',
-  'missense_variant',
-  'splice_region_variant',
-  'incomplete_terminal_codon_variant',
-  'stop_retained_variant',
-  'synonymous_variant',
-  'coding_sequence_variant',
-  'mature_miRNA_variant',
-  '5_prime_UTR_variant',
-  '3_prime_UTR_variant',
-  'non_coding_exon_variant',
-  'non_coding_transcript_exon_variant',
-  'non_coding_transcript_variant',
-  'nc_transcript_variant',
-  'intron_variant',
-  'NMD_transcript_variant',
-  'non_coding_transcript_variant',
-  'upstream_gene_variant',
-  'downstream_gene_variant',
-  'TFBS_ablation',
-  'TFBS_amplification',
-  'TF_binding_site_variant',
-  'regulatory_region_ablation',
-  'regulatory_region_amplification',
-  'regulatory_region_variant',
-  'feature_elongation',
-  'feature_truncation',
-  'intergenic_variant'
-)
-
-FEATURE_TYPES = (
-  'exonic',
-  'splicing',
-  'ncRNA_exonic',
-  'intronic',
-  'ncRNA',
-  'upstream',
-  '5UTR',
-  '3UTR',
-  'downstream',
-  'TFBS',
-  'regulatory_region',
-  'genomic_feature',
-  'intergenic_variant'
-)
-
-
 class VariantCommon(EmbeddedDocument):
   genes = MapField(EmbeddedDocumentField(Gene))
   # Gene ids:
