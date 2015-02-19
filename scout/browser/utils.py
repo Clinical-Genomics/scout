eceb112c8f6d4f5272226aaf173d74bca24a70db
Robin Andeer <robin.andeer@gmail.com>
2015-02-19 08:45:10 +0100

    add sanity check to IGV init route

--- scout/browser/utils.py
+++ scout/browser/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from flask import abort, flash
 
 
 def build_igv_url(chrom, start, end, vcf_file, bam_files,
@@ -10,10 +11,15 @@ def build_igv_url(chrom, start, end, vcf_file, bam_files,
   static_vcf = "{}/{}".format(static_base, vcf_file)
   static_bams = ["{}/{}".format(static_base, bam) for bam in bam_files]
 
+  # check so that all files are set
+  if vcf_file is None or len(bam_files) == 0:
+    flash("Can't find all required files to launch IGV.")
+    return abort(404)
+
   # compose the full URL
   broad_url = ("http://www.broadinstitute.org/igv/projects/current/igv.php"
-               "?sessionURL={vcf},{','.join(bams)}"
+               "?sessionURL={vcf},{bams}"
                "&genome=hg19&locus={locus}")\
-               .format(vcf=static_vcf, bams=static_bams, locus=locus)
+               .format(vcf=static_vcf, bams=','.join(static_bams), locus=locus)
 
   return broad_url
