863486d7b0c88e180536f619370ed0625fab8ea4
Robin Andeer <robin.andeer@gmail.com>
2015-02-27 10:05:46 +0100

    add redirect URL for "generate HPO gene list"

--- scout/core/views.py
+++ scout/core/views.py
@@ -229,6 +229,19 @@ def variants(institute_id, case_id, variant_type):
               variant_type=variant_type)
 
 
+@core.route('/<institute_id>/<case_id>/<variant_type>/hpo_redirect')
+@login_required
+def hpo_gene_list_redirect(institute_id, case_id, variant_type):
+  # redirect user to variants list after querying HPO
+  validate_user(current_user, institute_id)
+  case_model = get_document_or_404(Case, case_id)
+
+  return redirect(url_for('.variants', institute_id=institute_id,
+                          case_id=case_id,
+                          variant_type=variant_type,
+                          hgnc_symbols=case_model.hpo_gene_ids))
+
+
 @core.route('/<institute_id>/<case_id>/variants/<variant_id>')
 @templated('variant.html')
 @login_required
