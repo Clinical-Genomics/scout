d901e4734ad6005631dc4495567ed8745f28ab82
Mans Magnusson <monsunas@gmail.com>
2015-04-01 20:28:52 +0200

    Started logging in load_mongo

--- scout/ext/backend/load_mongo.py
+++ scout/ext/backend/load_mongo.py
@@ -27,29 +27,38 @@ import os
 import io
 import json
 import click
+import logging
 
 from datetime import datetime
 from pymongo import (ASCENDING, DESCENDING)
 from mongoengine import connect, DoesNotExist
 from mongoengine.connection import get_db
-from vcf_parser import VCFParser
-from pprint import pprint as pp
-import scout
 
 from .config_parser import ConfigParser
 from .utils import (get_case, get_institute, get_mongo_variant)
 from ...models import (Institute, Case)
 from ..._compat import iteritems
+from ....log import init_log
+
+
+from vcf_parser import VCFParser
+
+from pprint import pprint as pp
+
+import scout
 
 
 def load_mongo_db(scout_configs, config_file=None, family_type='cmms',
                   mongo_db='variantDatabase', variant_type='clinical',
                   username=None, password=None, port=27017,
-                  rank_score_treshold = 0, host='localhost',verbose = False):
+                  rank_score_treshold = 0, host='localhost',verbose = False,
+                  log_file=None, log_level=None):
   """Populate a moongo database with information from ped and variant files."""
   # get root path of the Flask app
   # project_root = '/'.join(app.root_path.split('/')[0:-1])
-
+  logger = logging.getLogger(__name__)
+  init_log(logger, logfile, loglevel)
+  
   ####### Check if the vcf file is on the proper format #######
   vcf_file = scout_configs['load_vcf']
   splitted_vcf_file_name = os.path.splitext(vcf_file)
@@ -58,29 +67,30 @@ def load_mongo_db(scout_configs, config_file=None, family_type='cmms',
     if vcf_ending == '.gz':
       vcf_ending = os.path.splitext(splitted_vcf_file_name)[-1]
       if vcf_ending != '.vcf':
-        print("Please use the correct prefix of your vcf file('.vcf/.vcf.gz')",
-               file=sys.stderr)
-        sys.exit(0)
+        logger.error("Please use the correct prefix of your vcf file"\
+                        "('.vcf/.vcf.gz')")
+        raise Exception
     else:
       if vcf_ending != '.vcf':
-        print("Please use the correct prefix of your vcf file('.vcf/.vcf.gz')",
-                file=sys.stderr)
-        sys.exit(0)
-
+        logger.error("Please use the correct prefix of your vcf file('.vcf/.vcf.gz')")
+        raise Exception
+  
+  logger.info("Connecting to {0}".format(mongo_db))
   connect(mongo_db, host=host, port=port, username=username,
           password=password)
 
   variant_database = get_db()
 
   ped_file = scout_configs['ped']
-
-  if verbose:
-    print("\nvcf_file:\t%s\nped_file:\t%s\nconfig_file:\t%s\nfamily_type:\t%s\n"
-          "mongo_db:\t%s\ninstitutes:\t%s\n" % (vcf_file, ped_file,
-          config_file, family_type, mongo_db, ','.join(scout_configs['institutes'])),
-          file=sys.stderr)
+  
+  logger.info("Starting to load database with:"\
+              "\nvcf_file:\t{0}\nped_file:\t{1}\nconfig_file:\t{2}\n"\
+              "family_type:\t{3}\nmongo_db:\t{4}\ninstitutes:\t{5}\n".format(
+                vcf_file, ped_file, config_file, family_type, mongo_db, 
+                ','.join(scout_configs['institutes'])))
 
   ######## Parse the config file to check for keys ########
+  logger.info("Parsing config file")
   config_object = ConfigParser(config_file)
 
   ######## Add the institute to the mongo db: ########
@@ -89,30 +99,30 @@ def load_mongo_db(scout_configs, config_file=None, family_type='cmms',
   institutes = []
   for institute_name in scout_configs['institutes']:
     institutes.append(get_institute(institute_name))
-
+  logger.info("Institutes found: {0}".format(institutes))
+  
   # If the institute exists we work on the old one
   for i, institute in enumerate(institutes):
     try:
       if Institute.objects.get(internal_id = institute.internal_id):
         institutes[i] = Institute.objects.get(internal_id = institute.internal_id)
     except DoesNotExist:
-      if verbose:
-        print('New institute!', file=sys.stderr)
-
+        logger.info('New institute!')
 
   ######## Get the cases and add them to the mongo db: ########
 
+  logger.info("Get the cases from ped file")
   case = get_case(scout_configs, family_type)
 
-  if verbose:
-    print('Case found in %s: %s' % (ped_file, case.display_name),
-          file=sys.stderr)
+  logger.info('Case found in {0}: {1}'.format(ped_file, case.display_name))
 
   # Add the case to its institute(s)
+  logger.info("Adding cases to the institutes")
   for institute_object in institutes:
     if case not in institute_object.cases:
       institute_object.cases.append(case)
-
+      logger.debug("Adding {0} to {1}".format(case, institute_object.internal_id))
+  
     institute_object.save()
 
   try:
@@ -124,12 +134,12 @@ def load_mongo_db(scout_configs, config_file=None, family_type='cmms',
       existing_case.clinical_gene_lists = case.clinical_gene_lists
     existing_case.save()
   except DoesNotExist:
-    if verbose:
-      print('New case!', file=sys.stderr)
+    logger.info('New case!')
     case.save()
 
   ######## Get the variants and add them to the mongo db: ########
-
+  
+  logging.info("Setting up a variant parser")
   variant_parser = VCFParser(infile=vcf_file, split_variants=True)
   nr_of_variants = 0
   start_inserting_variants = datetime.now()
@@ -178,6 +188,20 @@ def load_mongo_db(scout_configs, config_file=None, family_type='cmms',
   ensure_indexes(variant_database)
 
   return
+
+def update_cases(case):
+  """
+  Update cases in the mongo database.
+  
+  If a case is already existing (in case of a rerun), we need to update
+  the existing one in a correct manner.
+  
+  Othervise insert the case.
+  
+  Arguments:
+    case (Case): A case object.
+  """
+  
 
 def update_local_frequencies(variant_database):
   """
