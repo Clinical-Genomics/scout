fbc0bf1c42519cb6df2686a6e8dc9198aa879f87
Robin Andeer <robin.andeer@gmail.com>
2015-04-09 14:52:25 +0200

    update cases view to support new ODM

--- scout/commands/wipe_and_load_database.py
+++ scout/commands/wipe_and_load_database.py
@@ -9,8 +9,6 @@ Created by Måns Magnusson on 2015-01-14.
 Copyright (c) 2015 __MoonsoInc__. All rights reserved.
 
 """
-
-
 from __future__ import absolute_import, unicode_literals, print_function
 
 import sys
@@ -19,19 +17,18 @@ import logging
 
 import click
 
-import scout
-
 from tempfile import NamedTemporaryFile
 from pprint import pprint as pp
 from pymongo import MongoClient, Connection
 from mongoengine import connect, DoesNotExist
 from mongoengine.connection import _get_db
 
-from . import (wipe, load)
+from .. import __file__
+from ..ext.backend import ConfigParser
+from .load_database import load
+from .wipe_database import wipe
 
-from scout.ext.backend import ConfigParser
-
-BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(scout.__file__), '..'))
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 @click.command()
 @click.option('-config', '--config_file',
@@ -65,165 +62,165 @@ BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(scout.__file__), '..'))
 def wipe_and_load(ctx, config_file, mongo_db, username, password, port, host,
                   verbose):
   """
-  Drop the mongo database given and rebuild it again with the test cases from 
+  Drop the mongo database given and rebuild it again with the test cases from
   tests/vcf_data/.
   """
 
   logger = logging.getLogger(__name__)
   scout_configs = {}
-  
+
   logger.info("Running wipe_and_load")
-  
+
   if not mongo_db:
     logger.warning("Please specify a database to wipe and populate with flag"\
                    " '-db/--mongo-db'.")
     sys.exit(0)
-  
+
   ######## Wipe cases and variants from the existing database ######
   ctx.invoke(
-                  wipe, 
-                  mongo_db=mongo_db, 
-                  username=username, 
-                  password=password, 
-                  port=port, 
+                  wipe,
+                  mongo_db=mongo_db,
+                  username=username,
+                  password=password,
+                  port=port,
                   verbose=verbose
                 )
-  
+
   ######## Update the paths from the config file #########
-  
+
   # Start with the clinical variants for test case 1:
-  
+
   scout_config_file_1_clinical = os.path.join(
                                 BASE_PATH,
                                 'tests/vcf_examples/1/scout_config_test_clinical.ini'
                                 )
   clinical_1_config = ConfigParser(scout_config_file_1_clinical)
-  
+
   clinical_1_config['load_vcf'] = os.path.join(
                                           BASE_PATH,
                                           clinical_1_config['load_vcf']
                                         )
-  
+
   clinical_1_config['igv_vcf'] = os.path.join(
                                           BASE_PATH,
                                           clinical_1_config['igv_vcf']
                                         )
-  
+
   clinical_1_config['ped'] = os.path.join(
                                           BASE_PATH,
                                           clinical_1_config['ped']
                                         )
-  
+
   clinical_1_config['madeline'] = os.path.join(
                                           BASE_PATH,
                                           clinical_1_config['madeline']
                                         )
-  
+
   clinical_1_config['coverage_report'] = os.path.join(
                                           BASE_PATH,
                                           clinical_1_config['coverage_report']
                                         )
 
   # Save the updated information to a temporary file:
-  
+
   case_1_clinical_temp = NamedTemporaryFile(delete=False)
   case_1_clinical_temp.close()
   clinical_1_config.filename = case_1_clinical_temp.name
   clinical_1_config.write()
-  
+
   # pp(dict(clinical_1_config))
   # sys.exit()
   # Load the family 1 clinical data:
   ctx.invoke(load,
                   scout_config_file = case_1_clinical_temp.name,
-                  vcf_config_file=config_file, 
-                  family_type='cmms', 
-                  mongo_db=mongo_db, 
+                  vcf_config_file=config_file,
+                  family_type='cmms',
+                  mongo_db=mongo_db,
                   username=username,
-                  variant_type='clinical', 
-                  password=password, 
-                  port=port, 
-                  host=host, 
+                  variant_type='clinical',
+                  password=password,
+                  port=port,
+                  host=host,
                   verbose=verbose
                   )
-  
+
   scout_config_file_1_research = os.path.join(
                                 BASE_PATH,
                                 'tests/vcf_examples/1/scout_config_test_research.ini'
                                 )
-  
+
   research_1_config = ConfigParser(scout_config_file_1_research)
-  
+
   research_1_config['load_vcf'] = os.path.join(
                                           BASE_PATH,
                                           research_1_config['load_vcf']
                                         )
-  
+
   research_1_config['igv_vcf'] = os.path.join(
                                           BASE_PATH,
                                           research_1_config['igv_vcf']
                                         )
-  
+
   research_1_config['ped'] = os.path.join(
                                           BASE_PATH,
                                           research_1_config['ped']
                                         )
-  
+
   research_1_config['madeline'] = os.path.join(
                                           BASE_PATH,
                                           research_1_config['madeline']
                                         )
-  
+
   research_1_config['coverage_report'] = os.path.join(
                                           BASE_PATH,
                                           research_1_config['coverage_report']
                                         )
-  
+
   # Save the updated information to a temporary file:
-  
+
   case_1_research_temp = NamedTemporaryFile(delete=False)
   case_1_research_temp.close()
   research_1_config.filename = case_1_research_temp.name
   research_1_config.write()
-  
+
   # Load the family 1 clinical data:
   ctx.invoke(load,
                   scout_config_file = case_1_research_temp.name,
-                  vcf_config_file=config_file, 
-                  family_type='cmms', 
-                  mongo_db=mongo_db, 
+                  vcf_config_file=config_file,
+                  family_type='cmms',
+                  mongo_db=mongo_db,
                   username=username,
-                  variant_type='research', 
-                  password=password, 
-                  port=port, 
-                  host=host, 
+                  variant_type='research',
+                  password=password,
+                  port=port,
+                  host=host,
                   verbose=verbose
                   )
-  
-  
-  
+
+
+
   scout_config_file_coriell_clinical = os.path.join(
                                 BASE_PATH,
                                 'tests/vcf_examples/P575_coriell/scout_config_test_clinical.ini'
                                 )
-  
+
   clinical_coriell_config = ConfigParser(scout_config_file_coriell_clinical)
-  
+
   clinical_coriell_config['load_vcf'] = os.path.join(
                                           BASE_PATH,
                                           clinical_coriell_config['load_vcf']
                                         )
-  
+
   clinical_coriell_config['igv_vcf'] = os.path.join(
                                           BASE_PATH,
                                           clinical_coriell_config['igv_vcf']
                                         )
-  
+
   clinical_coriell_config['ped'] = os.path.join(
                                           BASE_PATH,
                                           clinical_coriell_config['ped']
                                         )
-  
+
   clinical_coriell_config['madeline'] = os.path.join(
                                           BASE_PATH,
                                           clinical_coriell_config['madeline']
@@ -232,84 +229,84 @@ def wipe_and_load(ctx, config_file, mongo_db, username, password, port, host,
                                           BASE_PATH,
                                           clinical_coriell_config['coverage_report']
                                         )
-  
+
   # Save the updated information to a temporary file:
-  
+
   coriell_clinical_temp = NamedTemporaryFile(delete=False)
   coriell_clinical_temp.close()
   clinical_coriell_config.filename = coriell_clinical_temp.name
   clinical_coriell_config.write()
-  
+
   # Load the coriell family clinical data:
   ctx.invoke(load,
                   scout_config_file = coriell_clinical_temp.name,
-                  vcf_config_file=config_file, 
-                  family_type='cmms', 
-                  mongo_db=mongo_db, 
+                  vcf_config_file=config_file,
+                  family_type='cmms',
+                  mongo_db=mongo_db,
                   username=username,
-                  variant_type='clinical', 
-                  password=password, 
-                  port=port, 
-                  host=host, 
+                  variant_type='clinical',
+                  password=password,
+                  port=port,
+                  host=host,
                   verbose=verbose
                   )
-  
-  
+
+
   scout_config_file_coriell_research = os.path.join(
                                 BASE_PATH,
                                 'tests/vcf_examples/P575_coriell/scout_config_test_research.ini'
                                 )
-  
-  
+
+
   research_coriell_config = ConfigParser(scout_config_file_coriell_research)
-  
+
   research_coriell_config['load_vcf'] = os.path.join(
                                           BASE_PATH,
                                           research_coriell_config['load_vcf']
                                         )
-  
+
   research_coriell_config['igv_vcf'] = os.path.join(
                                           BASE_PATH,
                                           research_coriell_config['igv_vcf']
                                         )
-  
+
   research_coriell_config['ped'] = os.path.join(
                                           BASE_PATH,
                                           research_coriell_config['ped']
                                         )
-  
+
   research_coriell_config['madeline'] = os.path.join(
                                           BASE_PATH,
                                           research_coriell_config['madeline']
                                         )
-  
+
   research_coriell_config['coverage_report'] = os.path.join(
                                           BASE_PATH,
                                           research_coriell_config['coverage_report']
                                         )
-  
+
   # Save the updated information to a temporary file:
-  
+
   coriell_research_temp = NamedTemporaryFile(delete=False)
   coriell_research_temp.close()
   research_coriell_config.filename = coriell_research_temp.name
   research_coriell_config.write()
-  
+
   # Load the family 1 clinical data:
   ctx.invoke(load,
                   scout_config_file = coriell_research_temp.name,
-                  vcf_config_file=config_file, 
-                  family_type='cmms', 
-                  mongo_db=mongo_db, 
+                  vcf_config_file=config_file,
+                  family_type='cmms',
+                  mongo_db=mongo_db,
                   username=username,
-                  variant_type='research', 
-                  password=password, 
-                  port=port, 
-                  host=host, 
+                  variant_type='research',
+                  password=password,
+                  port=port,
+                  host=host,
                   verbose=verbose
                   )
-  
-  
+
+
 
 if __name__ == '__main__':
   from ..log import init_log
