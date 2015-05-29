af87464bfba28d8feb03b256426e08ddd73e7b91
Robin Andeer <robin.andeer@gmail.com>
2015-05-29 14:07:52 +0200

    handle non-existing institute queries

--- scout/core/utils.py
+++ scout/core/utils.py
@@ -2,13 +2,17 @@
 from __future__ import absolute_import, unicode_literals
 
 from flask import abort, flash
+from mongoengine import DoesNotExist
 
 from scout.extensions import store
 
 
 def validate_user(current_user, institute_id):
   # abort with 404 error if case/institute doesn't exist
-  institute = store.institute(institute_id)
+  try:
+    institute = store.institute(institute_id)
+  except DoesNotExist as exception:
+    return abort(404)
 
   if institute not in current_user.institutes:
     flash('You do not have access to this institute.')
