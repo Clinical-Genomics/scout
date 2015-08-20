cf2f8257eedb4ae08126f97932ab4cdf01c21728
Robin Andeer <robin.andeer@gmail.com>
2015-08-20 15:51:01 +0200

    update change log + bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='scout',
-  version='1.1.0',
+  version='1.1.1',
   url='https://github.com/Clinical-Genomics/scout',
   description='Scout is a Flask template/bootstrap/boilerplate application.',
   author='Robin Andeer',
