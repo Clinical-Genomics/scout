2d821b23518d3707f6e6c5653faab1fc78f4a888
moonso <monsunas@gmail.com>
2015-11-27 15:20:50 +0100

    Updated __init__.py

--- scout/ext/backend/__init__.py
+++ scout/ext/backend/__init__.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import (print_function, absolute_import, unicode_literals)
 
+from .load_mongo import load_mongo_db
 from .event_handler import EventHandler
 from .variant_handler import VariantHandler
-from .fixture import FixtureAdapter
-from .config_parser import ConfigParser
 from .mongo import MongoAdapter
-from .load_mongo import load_mongo_db
