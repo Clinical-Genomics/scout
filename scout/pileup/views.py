2f8a23d7329a232e413c195fdd9b20fb86dbf6a7
Robin Andeer <robin.andeer@gmail.com>
2015-11-29 13:15:09 +0100

    send explicit bai paths and sample names to pileup

--- scout/pileup/views.py
+++ scout/pileup/views.py
@@ -9,6 +9,11 @@ pileup_bp = Blueprint('pileup', __name__, template_folder='templates',
 def viewer():
     """Visualize BAM alignments."""
     bam_files = request.args.getlist('bam')
+    bai_files = request.args.getlist('bai')
+    samples = request.args.getlist('sample')
+    alignments = [{'bam': bam, 'bai': bai, 'sample': sample}
+                  for bam, bai, sample in zip(bam_files, bai_files, samples)]
+
     vcf_file = request.args['vcf']
     position = {
         'contig': request.args['contig'],
@@ -16,5 +21,5 @@ def viewer():
         'stop': request.args['stop']
     }
 
-    return render_template('pileup.html', bam_files=bam_files,
+    return render_template('pileup.html', alignments=alignments,
                            vcf_file=vcf_file, position=position)
