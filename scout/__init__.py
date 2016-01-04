d22a568ffc879f7eae9a2a66ffd504e2d5ccf712
Robin Andeer <robin.andeer@gmail.com>
2016-01-04 10:10:31 +0100

    __package__ is not the same as dist name :P

--- scout/__init__.py
+++ scout/__init__.py
@@ -4,6 +4,6 @@ import logging
 import scout
 from pkg_resources import get_distribution
 
-__version__ = get_distribution(__package__).version
+__version__ = get_distribution('scout-browser').version
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(scout.__file__), '..'))
 logger = logging.getLogger()
