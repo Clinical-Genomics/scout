1d2b4373da54a8dc4cfad2b2f67493daf0c31a22
moonso <monsunas@gmail.com>
2016-06-08 13:16:39 +0200

    Adds export command

--- scout/commands/export.py
+++ scout/commands/export.py
@@ -17,24 +17,28 @@ import click
 logger = logging.getLogger(__name__)
 
 @click.command()
-@click.option('-i', '--institute', default=None)
 @click.option('-t', '--variant-type', default='causative')
 @click.option('-f', '--format', default='vcf')
-@click.option('-c', '--collaborator')
+@click.option('-c', '--collaborator', default=None)
 @click.pass_context
 def export(ctx, institute, variant_type, collaborator, format):
     """
     Export variants from the mongo database.
     """
     logger.info("Running scout export")
-
+    
+    # Store variants in a dictionary
+    causative_variants = {}
+    
     adapter = ctx.obj['adapter']
     cases = adapter.cases(
-        collaborator=collaborator
+        collaborator=collaborator,
+        has_causatives=True
     )
 
-    for case in cases:
-        if case.causatives:
-            logger.debug("Found causatives for case {0}".format(case.case_id))
-            for variant in case.causatives:
-                print(variant.variant_id)
+    for case_obj in cases:
+        for variant_obj in case_obj.causatives:
+            logger.debug("Found causative {0}".format(variant_obj.display_name))
+            causative_variants[variant_obj.variant_id] == variant_obj
+    
+    
