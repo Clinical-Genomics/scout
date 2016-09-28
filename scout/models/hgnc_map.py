5803fdaeda409596a7d6452165b23bd5c3420f15
moonso <monsunas@gmail.com>
2016-09-28 13:20:18 +0200

    Add model for mapping hgnc aliases, adresses #315

---
+++ scout/models/hgnc_map.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from mongoengine import (Document, StringField, ListField)
+
+class HgncAlias(Document):
+    #This works like a dictionary where hgnc_symbol is the correct id and
+    #values are all aliases
+    hgnc_symbol = ListField(StringField())
+    
