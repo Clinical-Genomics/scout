5d85949cc9335d297d216211399191159bb74883
Robin Andeer <robin.andeer@gmail.com>
2016-10-28 15:24:32 +0200

    store rank result, not category

--- scout/build/variant.py
+++ scout/build/variant.py
@@ -1,44 +1,44 @@
 from scout.models import Variant
 
 from . import (build_genotype, build_compound, build_gene)
-        
+
 def build_variant(variant, institute):
     """Build a mongoengine Variant based on parsed information
-    
+
         Args:
             variant(dict)
             institute(Institute): A mongoengine institute
-    
+
         Returns:
             variant_obj(Variant)
     """
     variant_obj = Variant(
-        document_id = variant['ids']['document_id'],
-        variant_id = variant['ids']['variant_id'],
-        display_name = variant['ids']['display_name'],
-        variant_type = variant['variant_type'],
-        case_id = variant['case_id'],
-        chromosome = variant['chromosome'],
-        position = variant['position'],
-        reference = variant['reference'],
-        alternative = variant['alternative'],
-        rank_score = variant['rank_score'],
-        institute = institute,
+        document_id=variant['ids']['document_id'],
+        variant_id=variant['ids']['variant_id'],
+        display_name=variant['ids']['display_name'],
+        variant_type=variant['variant_type'],
+        case_id=variant['case_id'],
+        chromosome=variant['chromosome'],
+        position=variant['position'],
+        reference=variant['reference'],
+        alternative=variant['alternative'],
+        rank_score=variant['rank_score'],
+        institute=institute,
     )
     variant_obj['simple_id'] = variant['ids'].get('simple_id')
-    
+
     variant_obj['quality'] = variant['quality']
     variant_obj['filters'] = variant['filters']
 
     variant_obj['end'] = variant.get('end')
     variant_obj['length'] = variant.get('length')
     variant_obj['db_snp_ids'] = variant.get('db_snp_ids')
-    
+
     variant_obj['category'] = variant.get('category')
     variant_obj['sub_category'] = variant.get('sub_category')
 
     variant_obj['mate_id'] = variant.get('mate_id')
-    
+
     gt_types = []
     for sample in variant.get('samples', []):
         gt_call = build_genotype(sample)
@@ -64,30 +64,29 @@ def build_variant(variant, institute):
 
     variant_obj['hgnc_symbols'] = variant.get('hgnc_symbols')
     variant_obj['ensembl_gene_ids'] = variant.get('ensembl_gene_ids')
-    
+
     # Add the callers
     call_info = variant.get('callers', {})
     variant_obj['gatk'] = call_info.get('gatk')
     variant_obj['samtools'] = call_info.get('samtools')
     variant_obj['freebayes'] = call_info.get('freebayes')
-    
+
     # Add the conservation
     conservation_info = variant.get('conservation', {})
-    variant_obj['phast_conservation'] = conservation_info.get('phast',[])
-    variant_obj['gerp_conservation'] = conservation_info.get('gerp',[])
-    variant_obj['phylop_conservation'] = conservation_info.get('phylop',[])
-    
+    variant_obj['phast_conservation'] = conservation_info.get('phast', [])
+    variant_obj['gerp_conservation'] = conservation_info.get('gerp', [])
+    variant_obj['phylop_conservation'] = conservation_info.get('phylop', [])
+
     variant_obj['gene_lists'] = variant.get('gene_lists')
     variant_obj['expected_inheritance'] = variant.get('expected_inheritance')
-    
+
     rank_results = []
-    for category in variant.get('rank_result',[]):
+    for category in variant.get('rank_result', []):
         rank_result = {
             'Category': category,
             'Score': variant['rank_result'][category]
         }
-        rank_results.append(category)
+        rank_results.append(rank_result)
     variant_obj['rank_score_results'] = rank_results
-        
-    
+
     return variant_obj
