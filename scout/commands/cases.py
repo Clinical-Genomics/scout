de3b1fb96f8e1c37e7d4cc322f8a8bf00a128e30
Robin Andeer <robin.andeer@gmail.com>
2016-11-10 13:06:06 +0100

    add help to CLI

--- scout/commands/cases.py
+++ scout/commands/cases.py
@@ -7,13 +7,14 @@ log = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option('-i', '--institute')
-@click.option('-r', '--reruns', is_flag=True)
-@click.option('-f', '--finished', is_flag=True, help="archived or solved")
+@click.option('-i', '--institute', help='institute id of related cases')
+@click.option('-r', '--reruns', is_flag=True, help='requested to be rerun')
+@click.option('-f', '--finished', is_flag=True, help='archived or solved')
 @click.pass_context
 def cases(context, institute, reruns, finished):
     """Show information about cases."""
     adapter = context.obj['adapter']
-    models = adapter.cases(institute, reruns=reruns, finished=finished)
+    models = adapter.cases(collaborator=institute, reruns=reruns,
+                           finished=finished)
     for model in models:
         click.echo(model.owner_case_id)
