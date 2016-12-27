086dc79c352b02a2b7117a15ceba66d9a860e0f2
Robin Andeer <robin.andeer@gmail.com>
2016-12-27 15:26:08 +0100

    fix default genes property

--- scout/models/case/case.py
+++ scout/models/case/case.py
@@ -82,8 +82,7 @@ class Case(Document):
         """Combine all gene ids for default gene panels."""
         distinct_genes = set()
         for panel in self.default_panels:
-            for gene_id in panel.gene_objects.keys():
-                distinct_genes.update(gene_id)
+            distinct_genes.update(panel.gene_objects.keys())
         return distinct_genes
 
     @property
