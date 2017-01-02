ea983608d67d64e68e8154a95c910c5ba678ffaf
Robin Andeer <robin.andeer@gmail.com>
2017-01-02 14:02:23 +0100

    enable research command

--- scout/commands/load_database.py
+++ scout/commands/load_database.py
@@ -10,79 +10,62 @@ Copyright (c) 2015 __MoonsoInc__. All rights reserved.
 
 """
 import logging
-import datetime
 
 import click
-import yaml
 
-from scout.exceptions import (IntegrityError, ConfigError)
-
-from scout.models import (User, Whitelist)
-
+from scout.models import User, Whitelist
 from scout.commands.case import case as case_command
 from scout.commands.load_institute import institute as institute_command
 from scout.commands.load_hpo import hpo as hpo_command
 from scout.commands.load_genes import genes as genes_command
 from scout.commands.load_panel import panel as panel_command
+from scout.commands.load_research import research as research_command
 
 from scout.load.all import load_region
 
-logger = logging.getLogger(__name__)
-    
+log = logging.getLogger(__name__)
+
 
 @click.command('region', short_help='Load variants from region')
-@click.option('--hgnc-id',
-    type=int,
-    help="Use a existing hgnc id to define the region",
-)
-@click.option('--institute-name',
-    required = True,
-    help = "Specify the institute that the case belongs to"
-)
-@click.option('--case-name',
-    required = True,
-    help = "Name of case"
-)
-@click.option('-c','--chromosome')
-@click.option('-s','--start', type=int)
-@click.option('-e','--end', type=int)
+@click.option('--hgnc-id', type=int,
+              help="Use a existing hgnc id to define the region",)
+@click.option('--institute-name', required=True,
+              help="Specify the institute that the case belongs to")
+@click.option('--case-name', required=True, help="Name of case")
+@click.option('-c', '--chromosome')
+@click.option('-s', '--start', type=int)
+@click.option('-e', '--end', type=int)
 @click.pass_context
 def region(context, hgnc_id, case_name, institute_name, chromosome, start, end):
     """Load all variants in a region to a existing case"""
     adapter = context.obj['adapter']
-    
     try:
         load_region(
-            adapter=adapter, 
-            case_id=case_name, 
-            owner=institute_name, 
-            hgnc_id=hgnc_id, 
-            chrom=chromosome, 
-            start=start, 
+            adapter=adapter,
+            case_id=case_name,
+            owner=institute_name,
+            hgnc_id=hgnc_id,
+            chrom=chromosome,
+            start=start,
             end=end)
-    except Error as err:
-        logger.warning(err)
+    except Exception as err:
+        log.warning(err)
         context.abort()
 
+
 @click.command('user', short_help='Load a user')
-@click.option('-i', '--institute-name',
-    required = True,
-)
-@click.option('-u', '--user-name',
-    default = 'Clark Kent',
-)
-@click.option('-m', '--user-mail',
-    default = 'clark.kent@mail.com',
-)
+@click.option('-i', '--institute-name', required=True,)
+@click.option('-u', '--user-name', default='Clark Kent')
+@click.option('-m', '--user-mail', default='clark.kent@mail.com')
 @click.pass_context
 def user(context, institute_name, user_name, user_mail):
     """Add a user to the database"""
     adapter = context.obj['adapter']
-    
+
     institute = adapter.institute(institute_id=institute_name)
-    
+
     if not institute:
-        logger.info("Institute {0} does not exist".format(institute_name))
+        log.info("Institute {0} does not exist".format(institute_name))
         context.abort()
 
     Whitelist(email=user_mail).save()
@@ -91,14 +74,15 @@ def user(context, institute_name, user_name, user_mail):
                 roles=['admin'],
                 institutes=[institute])
     user.save()
-    
+
 
 @click.group()
 @click.pass_context
 def load(context):
     """Load the Scout database."""
     pass
-    
+
+
 load.add_command(case_command)
 load.add_command(institute_command)
 load.add_command(genes_command)
@@ -106,3 +90,4 @@ load.add_command(region)
 load.add_command(hpo_command)
 load.add_command(panel_command)
 load.add_command(user)
+load.add_command(research_command)
--- scout/commands/load_research.py
+++ scout/commands/load_research.py
@@ -4,66 +4,57 @@ import click
 
 from scout.load.variant import load_variants
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
-@click.command('research', short_help='Upload research variants')
-@click.option('--case-id',
-                  default=None
-)
-@click.option('-i', '--institute', 
-              help='institute id of related cases'
-)
+
+@click.command(short_help='Upload research variants')
+@click.option('-c', '--case-id', help='family or case id')
+@click.option('-i', '--institute', help='institute id of related cases')
 @click.pass_context
 def research(context, case_id, institute):
     """Upload research variants to cases
-    
-        If a case is specified, all variants found for that case will be 
+
+        If a case is specified, all variants found for that case will be
         uploaded.
-    
+
         If no cases are specified then all cases that have 'research_requested'
         will have there research variants uploaded
     """
-    logger.info("Running scout load research")
+    log.info("Running scout load research")
     adapter = context.obj['adapter']
-    
-    if (case_id or institute):
+
+    if case_id:
         if not institute:
-            logger.info("Please specify a institute")
+            # Assume institute-case combo
+            institute, case_id = case_id.split('-', 1)
+        case_obj = adapter.case(institute_id=institute, case_id=case_id)
+        if case_obj is None:
+            log.info("No matching case found")
             context.abort()
-        if not case_id:
-            logger.info("Please specify a case id")
-            context.abort()
-        case_objs = case(
-            institute_id=institute, 
-            case_id=case_id
-        )
-    #Fetch all cases that have requested research
+        else:
+            case_objs = [case_obj]
     else:
-        case_objs = adapter.cases(research_requested=research_requested)
-    
-    if not case_objs:
-        logger.info("No matching cases could be found")
-        context.abort()
-    
+        # Fetch all cases that have requested research
+        case_objs = adapter.cases(research_requested=True)
+
     for case_obj in case_objs:
         if case_obj.research_requested:
-            logger.info("Loading research snvs for case {}".format(case_obj.case_id))
+            log.info("Load research SNV for: %s", case_obj.case_id)
             load_variants(
-                adapter=adapter, 
-                variant_file=case_obj.vcf_files['vcf_snv_research'], 
-                case_obj=case_obj, 
+                adapter=adapter,
+                variant_file=case_obj.vcf_files['vcf_snv_research'],
+                case_obj=case_obj,
                 variant_type='research',
-                category='snv', 
-                rank_treshold=case_obj.rank_score_treshold, 
+                category='snv',
+                rank_treshold=case_obj.rank_score_treshold,
             )
             if case_obj.vcf_files.get('vcf_sv_research'):
-                logger.info("Loading research svs for case {}".format(case_obj.case_id))
+                log.info("Load research SV for: %s", case_obj.case_id)
                 load_variants(
-                    adapter=adapter, 
-                    variant_file=case_obj.vcf_files['vcf_sv_research'], 
-                    case_obj=case_obj, 
+                    adapter=adapter,
+                    variant_file=case_obj.vcf_files['vcf_sv_research'],
+                    case_obj=case_obj,
                     variant_type='research',
-                    category='sv', 
-                    rank_treshold=case_obj.rank_score_treshold, 
+                    category='sv',
+                    rank_treshold=case_obj.rank_score_treshold,
                 )
-        click.echo('\t'.join([panel_obj.panel_name, str(panel_obj.version)]))
