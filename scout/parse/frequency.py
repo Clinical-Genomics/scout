87dd5547511d0cdf72c1d90b6da9c0b41a5caf8d
Robin Andeer <robin.andeer@gmail.com>
2017-01-11 16:05:34 +0100

    fix missing equal sign lol

--- scout/parse/frequency.py
+++ scout/parse/frequency.py
@@ -10,8 +10,8 @@ def parse_frequencies(variant):
     """
     frequencies = {}
     frequencies['thousand_g'] = parse_frequency(variant, '1000GAF')
-    frequencies['thousand_g_left'] parse_frequency(variant, 'left_1000GAF')
-    frequencies['thousand_g_right'] parse_frequency(variant, 'right_1000GAF')
+    frequencies['thousand_g_left'] = parse_frequency(variant, 'left_1000GAF')
+    frequencies['thousand_g_right'] = parse_frequency(variant, 'right_1000GAF')
     frequencies['thousand_g_max'] = parse_frequency(variant, '1000G_MAX_AF')
     frequencies['exac'] = parse_frequency(variant, 'EXACAF')
     frequencies['exac_max'] = parse_frequency(variant, 'ExAC_MAX_AF')
