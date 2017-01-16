7f23621f45dcfbdc322d35ec081fea718e071119
Robin Andeer <robin.andeer@gmail.com>
2017-01-16 15:45:34 +0100

    bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ def parse_reqs(req_path='./requirements.txt'):
 
 setup(
     name='scout-browser',
-    version='2.1.0',
+    version='2.1.1',
     url='https://github.com/Clinical-Genomics/scout',
     description='Clinical DNA variant visualizer and browser.',
     author='Robin Andeer',
