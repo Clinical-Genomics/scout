e2956c05af06f304214faff2c9841eb440cc3f16
Robin Andeer <robin.andeer@gmail.com>
2017-01-18 14:37:01 +0100

    use existing panel info

--- scout/commands/load_panel.py
+++ scout/commands/load_panel.py
@@ -7,31 +7,18 @@ from scout.load import load_panel
 from scout.utils.date import get_date
 from scout.utils.handle import get_file_handle
 
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
+
 
 @click.command('panel', short_help='Load a gene panel')
-@click.argument('path',
-    type=click.Path(exists=True)
-)
-@click.option('-d', '--date', 
-    help='date of gene panel. Default is today.',
-)
-@click.option('-n', '--name', 
-    help='display name for the panel'
-)
-@click.option('-v', '--version', 
-    help='panel version',
-    show_default=True,
-    default=1.0
-)
-@click.option('-t', '--panel-type', 
-    default='clinical',
-    show_default=True,
-)
-@click.option('--panel-id',
-)
-@click.option('--institute',
-)
+@click.argument('path', type=click.Path(exists=True))
+@click.option('-d', '--date', help='date of gene panel. Default is today.')
+@click.option('-n', '--name', help='display name for the panel')
+@click.option('-v', '--version', help='panel version', show_default=True,
+              default=1.0)
+@click.option('-t', '--panel-type', default='clinical', show_default=True)
+@click.option('--panel-id')
+@click.option('--institute')
 @click.pass_context
 def panel(context, date, name, version, panel_type, panel_id, path, institute):
     """Add a gene panel to the database."""
@@ -52,14 +39,19 @@ def panel(context, date, name, version, panel_type, panel_id, path, institute):
                 date = value
             elif name == 'display_name':
                 name = value
-                
+
     try:
         date = get_date(date)
     except ValueError as error:
-        logger.warning(error)
+        log.warning(error)
         context.abort()
-    
+
     adapter = context.obj['adapter']
+    existing_panel = adapter.gene_panel(panel_id).first()
+    if existing_panel:
+        log.debug("found existing panel")
+        name = name or existing_panel.display_name or panel_id
+        institute = institute or existing_panel.institute
     info = {
         'file': path,
         'institute': institute,
@@ -67,10 +59,7 @@ def panel(context, date, name, version, panel_type, panel_id, path, institute):
         'date': date,
         'version': version,
         'name': panel_id,
-        'full_name': name or panel_id
+        'full_name': name,
     }
-    
-    load_panel(
-        adapter=adapter,
-        panel_info=info
-    )
+
+    load_panel(adapter=adapter, panel_info=info)
