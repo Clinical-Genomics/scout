6cbf60b5071c32c3bd3d7e0cf30a6ed7f21257dc
Robin Andeer <robin.andeer@gmail.com>
2017-02-23 16:13:39 +0100

    fix export of gene panel

--- scout/export/panel.py
+++ scout/export/panel.py
@@ -10,13 +10,13 @@ def export_panels(adapter, panels):
     header_string = ("##gene_panel={0},version={1},updated_at={2},display_name={3}")
     contig_string = ("##contig={0}")
     bed_string = ("{0}\t{1}\t{2}\t{3}\t{4}")
-    
+
     panel_geneids = set()
     chromosomes_found = set()
     hgnc_geneobjs = []
-    
+
     for panel_id in panels:
-        panel_obj = adapter.gene_panel(panel_id).first()
+        panel_obj = adapter.gene_panel(panel_id)
         headers.append(header_string.format(
             panel_obj.panel_name,
             panel_obj.version,
@@ -25,13 +25,13 @@ def export_panels(adapter, panels):
         ))
         for gene_obj in panel_obj.genes:
             panel_geneids.add(gene_obj.hgnc_id)
-            
+
     for hgnc_id in panel_geneids:
         hgnc_geneobj = adapter.hgnc_gene(hgnc_id)
         hgnc_geneobjs.append(hgnc_geneobj)
         chromosomes_found.add(hgnc_geneobj.chromosome)
-        
-    
+
+
     for chrom in CHROMOSOMES:
         if chrom in chromosomes_found:
             headers.append(contig_string.format(chrom))
