59f74bb58868404637a100f371cd43890c41b61c
Robin Andeer <robin.andeer@gmail.com>
2017-03-03 16:04:07 +0100

    udpate python 3 compatability

--- tests/server/test_server_public.py
+++ tests/server/test_server_public.py
@@ -10,4 +10,4 @@ def test_index(client):
     resp = client.get(url_for('public.index'))
     # THEN the response should be ok and display version information
     assert resp.status_code == 200
-    assert __version__ in resp.data
+    assert __version__.encode() in resp.data
