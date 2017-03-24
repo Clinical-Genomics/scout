004d8327110f51733229ed28c19f42bf29c52dfa
Robin Andeer <robin.andeer@gmail.com>
2017-03-24 08:33:50 +0100

    add research request function

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -301,3 +301,13 @@ def rerun(institute_id, case_name):
     controllers.rerun(store, mail, current_user, institute_id, case_name, sender,
                       recipient)
     return redirect(request.referrer)
+
+
+@cases_bp.route('/<institute_id>/<case_name>/research', methods=['POST'])
+def research(institute_id, case_name):
+    """Open the research list for a case."""
+    institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
+    user_obj = store.user(current_user.email)
+    link = url_for('.case', institute_id=institute_id, case_name=case_name)
+    store.open_research(institute_obj, case_obj, user_obj, link)
+    return redirect(request.referrer)
