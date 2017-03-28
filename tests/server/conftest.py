0cec3d077f0d09566c9fa2cce74e039ae61c33b1
Robin Andeer <robin.andeer@gmail.com>
2017-03-28 09:12:04 +0200

    Add fixtures for testing Flask

--- tests/server/conftest.py
+++ tests/server/conftest.py
@@ -1,3 +1,57 @@
 # -*- coding: utf-8 -*-
-import pytest
+import logging
 
+import pytest
+import pymongo
+
+from scout.server.app import create_app
+from scout.adapter import PymongoAdapter
+from scout.load.hgnc_gene import load_hgnc_genes
+from scout.load.hpo import load_hpo
+from scout.load.panel import load_panel
+
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixtures
+def database_name(request):
+    db_name = 'realtestdb'
+    return db_name
+
+
+@pytest.fixture
+def app(database_name):
+    app = create_app(config=dict(DEBUG_TB_ENABLED=False, MONGO_DBNAME=database_name))
+    return app
+
+
+@pytest.yield_fixture(scope='session')
+def real_database(database_name, institute_obj, user_obj, genes, gene_database,
+                  hpo_terms_handle, genemap_handle, panel_info, case_obj, variant_objs):
+    """Setup a real database with populated data"""
+    mongo_client = pymongo.MongoClient()
+
+    database = mongo_client[database_name]
+    adapter = PymongoAdapter(database)
+
+    # Populate the database
+    adapter.add_institute(institute_obj)
+    adapter.add_user(user_obj)
+
+    load_hgnc_genes(adapter, genes)
+
+    log.info("Creating index on hgnc collection")
+    adapter.hgnc_collection.create_index([('build', pymongo.ASCENDING),
+                                          ('hgnc_symbol', pymongo.ASCENDING)])
+
+    load_hpo(adapter=gene_database, hpo_lines=hpo_terms_handle, disease_lines=genemap_handle)
+    load_panel(adapter=adapter, panel_info=panel_info)
+    adapter.add_case(case_obj)
+
+    for variant in variant_objs:
+        adapter.load_variant(variant)
+
+    yield adapter
+
+    mongo_client.drop_database(database_name)
--- tests/server/test_server_genes.py
+++ tests/server/test_server_genes.py
@@ -1,36 +1,36 @@
 # -*- coding: utf-8 -*-
-# from flask import url_for
+from flask import url_for
 
 
-# def test_genes(client):
-#     # GIVEN an initialized database
-#     # WHEN accessing the genes overview page
-#     resp = client.get(url_for('genes.genes'))
-#     # THEN it should return 20 random genes and a search box
-#     assert resp.status_code == 200
-#
-#
-# def test_genes_query(client):
-#     # GIVEN an initialized database
-#     # WHEN accessing genes overview with a gene query
-#     hgnc_id = 10249
-#     resp = client.get(url_for('genes.genes', query='10249 | ROBO1'))
-#     # THEN it should redirect (302) to the specific gene page
-#     assert resp.status_code == 302
-#     assert resp.location.endswith(url_for('genes.gene', hgnc_id=hgnc_id))
-#
-#
-# def test_gene(client):
-#     # GIVEN an initialized database
-#     # WHEN accessing a specific gene
-#     hgnc_id = 10249
-#     resp = client.get(url_for('genes.gene', hgnc_id=hgnc_id))
-#     # THEN it should display information about the gene
-#     assert resp.status_code == 200
-#
-#     # GIVEN a non-existent HGNC id
-#     hgnc_id = 2132333
-#     # WHEN accessing the gene view
-#     resp = client.get(url_for('genes.gene', hgnc_id=hgnc_id))
-#     # THEN it should return a 404 page
-#     assert resp.status_code == 404
+def test_genes(client):
+    # GIVEN an initialized database
+    # WHEN accessing the genes overview page
+    resp = client.get(url_for('genes.genes'))
+    # THEN it should return 20 random genes and a search box
+    assert resp.status_code == 200
+
+
+def test_genes_query(client):
+    # GIVEN an initialized database
+    # WHEN accessing genes overview with a gene query
+    hgnc_id = 10249
+    resp = client.get(url_for('genes.genes', query='10249 | ROBO1'))
+    # THEN it should redirect (302) to the specific gene page
+    assert resp.status_code == 302
+    assert resp.location.endswith(url_for('genes.gene', hgnc_id=hgnc_id))
+
+
+def test_gene(client):
+    # GIVEN an initialized database
+    # WHEN accessing a specific gene
+    hgnc_id = 10249
+    resp = client.get(url_for('genes.gene', hgnc_id=hgnc_id))
+    # THEN it should display information about the gene
+    assert resp.status_code == 200
+
+    # GIVEN a non-existent HGNC id
+    hgnc_id = 2132333
+    # WHEN accessing the gene view
+    resp = client.get(url_for('genes.gene', hgnc_id=hgnc_id))
+    # THEN it should return a 404 page
+    assert resp.status_code == 404
