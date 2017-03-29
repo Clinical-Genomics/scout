acf9f21a64c4db302ac1609192a6df51054b9526
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-03-29 13:30:50 +0200

    Adds  and adds the necessary files

---
+++ scout/demo/resources/__init__.py
@@ -0,0 +1,31 @@
+import pkg_resources
+
+###### Files ######
+
+# Gene sources:
+hgnc_file = 'demo/resources/hgnc_reduced_set.txt'
+exac_file = 'demo/resources/forweb_cleaned_exac_r03_march16_z_data_pLI_reduced.txt'
+transcripts37_file = 'demo/resources/ensembl_transcripts_reduced.txt'
+
+# OMIM resources:
+mim2gene_file = 'demo/resources/mim2gene_reduced.txt'
+genemap2_file = 'demo/resources/genemap2_reduced.txt'
+
+# Hpo resources
+hpogenes_file = 'demo/resources/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype_reduced.txt'
+hpoterms_file = 'demo/resources/ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes_reduced.txt'
+
+###### Paths ######
+
+# Gene paths
+hgnc_reduced_path = pkg_resources.resource_filename('scout', hgnc_file)
+exac_reduced_path = pkg_resources.resource_filename('scout', exac_file)
+transcripts37_reduced_path = pkg_resources.resource_filename('scout', transcripts37_file)
+
+# OMIM paths
+mim2gene_reduced_path = pkg_resources.resource_filename('scout', mim2gene_file)
+genemap2_reduced_path = pkg_resources.resource_filename('scout', genemap2_file)
+
+# Hpo paths
+hpogenes_reduced_path = pkg_resources.resource_filename('scout', hpogenes_file)
+hpoterms_reduced_path = pkg_resources.resource_filename('scout', hpoterms_file)
