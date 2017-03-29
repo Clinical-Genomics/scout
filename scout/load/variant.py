98f0f66eb4adf6021648b65bec788b4abc18c5bb
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-03-29 09:19:08 +0200

    Small change

--- scout/load/variant.py
+++ scout/load/variant.py
@@ -93,61 +93,61 @@ def load_variants(adapter, variant_file, case_obj, variant_type='clinical',
             'start': start,
             'end': end
         }
-
-    for nr_variants, variant in enumerate(variants):
-        rank_score = parse_rank_score(variant, case_obj['display_name'])
-        variant_obj = None
-        add_variant = False
-
-        if chrom or (rank_score > rank_threshold):
-            parsed_variant = parse_variant(
-                variant_dict=variant,
-                case=case_obj,
-                variant_type=variant_type,
-                rank_results_header=rank_results_header
-            )
-            add_variant = True
-            # If there are coordinates the variant should be loaded
-            if coordinates:
-                if not check_coordinates(parsed_variant, coordinates):
-                    add_variant = False
-
-            if add_variant:
-                variant_obj = build_variant(
-                    variant=parsed_variant,
-                    institute=institute_obj,
-                    gene_to_panels=gene_to_panels,
-                    hgncid_to_gene=hgncid_to_gene,
+    try:
+        for nr_variants, variant in enumerate(variants):
+            rank_score = parse_rank_score(variant, case_obj['display_name'])
+            variant_obj = None
+            add_variant = False
+        
+            if chrom or (rank_score > rank_threshold):
+                parsed_variant = parse_variant(
+                    variant_dict=variant,
+                    case=case_obj,
+                    variant_type=variant_type,
+                    rank_results_header=rank_results_header
                 )
                 add_variant = True
                 # If there are coordinates the variant should be loaded
                 if coordinates:
                     if not check_coordinates(parsed_variant, coordinates):
                         add_variant = False
-
+        
                 if add_variant:
                     variant_obj = build_variant(
                         variant=parsed_variant,
-                        institute_id=institute_obj['internal_id'],
+                        institute=institute_obj,
                         gene_to_panels=gene_to_panels,
                         hgncid_to_gene=hgncid_to_gene,
                     )
-
-                    try:
-                        load_variant(adapter, variant_obj)
-                        nr_inserted += 1
-                    except IntegrityError as error:
-                        pass
-
-            if (nr_variants != 0 and nr_variants % 5000 == 0):
-                logger.info("%s variants parsed" % str(nr_variants))
-                logger.info("Time to parse variants: {} ".format(
-                            datetime.now() - start_five_thousand))
-                start_five_thousand = datetime.now()
-
-            if (nr_inserted != 0 and (nr_inserted * inserted) % (1000 * inserted) == 0):
-                logger.info("%s variants inserted" % nr_inserted)
-                inserted += 1
+                    add_variant = True
+                    # If there are coordinates the variant should be loaded
+                    if coordinates:
+                        if not check_coordinates(parsed_variant, coordinates):
+                            add_variant = False
+        
+                    if add_variant:
+                        variant_obj = build_variant(
+                            variant=parsed_variant,
+                            institute_id=institute_obj['internal_id'],
+                            gene_to_panels=gene_to_panels,
+                            hgncid_to_gene=hgncid_to_gene,
+                        )
+        
+                        try:
+                            load_variant(adapter, variant_obj)
+                            nr_inserted += 1
+                        except IntegrityError as error:
+                            pass
+        
+                if (nr_variants != 0 and nr_variants % 5000 == 0):
+                    logger.info("%s variants parsed" % str(nr_variants))
+                    logger.info("Time to parse variants: {} ".format(
+                                datetime.now() - start_five_thousand))
+                    start_five_thousand = datetime.now()
+        
+                if (nr_inserted != 0 and (nr_inserted * inserted) % (1000 * inserted) == 0):
+                    logger.info("%s variants inserted" % nr_inserted)
+                    inserted += 1
 
     except Exception as error:
         logger.warning("Deleting inserted variants")
