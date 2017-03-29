c4dbe389f01b729c231a6f0ce8bd3f249defc59a
Robin Andeer <robin.andeer@gmail.com>
2017-03-29 13:59:14 +0200

    fix redirect when user is logged out

--- scout/server/app.py
+++ scout/server/app.py
@@ -46,7 +46,9 @@ def create_app(config_file=None, config=None):
             relevant_endpoint = not (static_endpoint or public_endpoint)
             # if endpoint requires auth, check if user is authenticated
             if relevant_endpoint and not current_user.is_authenticated:
-                login_url = url_for('login.login', next=url_for(request.endpoint))
+                # combine visited URL (convert byte string query string to unicode!)
+                next_url = "{}?{}".format(request.path, request.query_string.decode())
+                login_url = url_for('login.login', next=next_url)
                 return redirect(login_url)
 
     return app
