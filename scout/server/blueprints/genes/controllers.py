209743978ea803ec7e3f2a306fe3759109121e14
Robin Andeer <robin.andeer@gmail.com>
2017-03-31 14:47:39 +0200

    fix autocomplete for gene search

--- scout/server/blueprints/genes/controllers.py
+++ scout/server/blueprints/genes/controllers.py
@@ -9,6 +9,7 @@ def gene(record):
 def genes_to_json(store, query):
     """Fetch matching genes and convert to JSON."""
     gene_query = store.hgnc_genes(query, search=True)
-    json_terms = [{'name': "{} | {}".format(gene['hgnc_id'], gene['hgnc_symbol']),
+    json_terms = [{'name': "{} | {} ({})".format(gene['hgnc_id'], gene['hgnc_symbol'],
+                                                 ', '.join(gene['aliases'])),
                    'id': gene['hgnc_id']} for gene in gene_query]
     return json_terms
