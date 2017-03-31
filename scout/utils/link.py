ce9fe36b436443c63a9cc0c65fc8d6cf275ba904
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-03-31 14:54:48 +0200

    Removes ghost code

--- scout/utils/link.py
+++ scout/utils/link.py
@@ -12,14 +12,6 @@ from scout.parse.omim import get_mim_genes
 log = logging.getLogger(__name__)
 
 
-def add_gene_coordinates(gene, transcript):
-    """Add the gene coordinated"""
-    gene['ensembl_gene_id'] = transcript['ensembl_gene_id']
-    gene['chromosome'] = transcript['chrom']
-    gene['start'] = transcript['gene_start']
-    gene['end'] = transcript['gene_end']
-
-
 def genes_by_alias(hgnc_genes):
     """Return a dictionary with hgnc symbols as keys
     
@@ -60,48 +52,6 @@ def genes_by_alias(hgnc_genes):
                 }
 
     return alias_genes
-    
-
-def add_transcript(gene, parsed_transcript):
-    """Add a transcript to a gene if it is not already there
-
-    If the transcript already exists(determined by the ensembl transcript id)
-    and it has a new refseq symbol we add that to the transcript
-
-    Args:
-        gene(dict): Hgnc gene information
-        parsed_transcript(dict): Ensembl transcript information
-    """
-    transcript = {}
-    if parsed_transcript:
-        # This is the refseq identifier found in ensembl
-        enstid = parsed_transcript['enst_id']
-        refseq_identifyer = parsed_transcript['refseq']
-        is_primary = False
-        # If there is a refseq identifier check if it is the same as
-        # specified as primary by HGNC
-        if refseq_identifyer:
-            if refseq_identifyer in gene['ref_seq']:
-                is_primary = True
-        # If the transcript is already added
-        if enstid in gene['transcripts']:
-            transcript_obj = gene['transcripts'][enstid]
-            # We check if the current transcript is one of the identifiers
-            if refseq_identifyer:
-            # Check if the refseq identifier is in the list of primary
-            # transcripts
-                if not refseq_identifyer in transcript_obj['refseq']:
-                    transcript_obj['refseq'].append(refseq_identifyer)
-                    if is_primary:
-                        transcript_obj['is_primary'] = True
-        else:
-            parsed_transcript['is_primary'] = is_primary
-            if refseq_identifyer:
-                parsed_transcript['refseq'] = [refseq_identifyer]
-            else:
-                parsed_transcript['refseq'] = []
-
-            gene['transcripts'][enstid] = parsed_transcript
 
 def add_ensembl_info(gene_info, ensembl_transcripts):
     """Add gene coordinates and transcripts to gene
