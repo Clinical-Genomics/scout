c61cc40d122817cefdc5453869f97cbec2fb6bdb
Robin Andeer <robin.andeer@gmail.com>
2017-03-31 14:41:32 +0200

    fix display of inheritance models on HGNC gene

--- scout/server/blueprints/genes/controllers.py
+++ scout/server/blueprints/genes/controllers.py
@@ -3,13 +3,7 @@
 
 def gene(record):
     """Parse information about a gene."""
-    record['position'] = ("{this[chromosome]}:{this[start]}-{this[end]}"
-                          .format(this=record))
-
-    record['inheritance'] = [('AR', record.get('ar')), ('AD', record.get('ad')),
-                             ('MT', record.get('mt')), ('XR', record.get('xr')),
-                             ('XD', record.get('xd')), ('X', record.get('x')),
-                             ('Y', record.get('y'))]
+    record['position'] = ("{this[chromosome]}:{this[start]}-{this[end]}".format(this=record))
 
 
 def genes_to_json(store, query):
