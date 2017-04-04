6219f7cd733d7dd2b9e673de8c86518655332b86
Robin Andeer <robin.andeer@gmail.com>
2017-04-04 14:38:54 +0200

    add docs publish command to invoke file

--- tasks.py
+++ tasks.py
@@ -183,3 +183,14 @@ def add_user(context, email, name='Paul Anderson'):
         institutes=[institute]
     )
     user.save()
+
+
+@task
+def docs(context):
+    """Publish docs to the GitHub pages branch."""
+    context.run("git checkout gh-pages")
+    context.run("cp -R docs/_book/* .")
+    context.run("git add .")
+    context.run("git commit -a -m 'Update docs'")
+    context.run("git push origin gh-pages")
+    context.run("git checkout master")
