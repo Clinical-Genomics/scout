a07b54a4f423940411e375524b569e3985bb22c2
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-05-02 14:23:26 +0200

    Adapts how to parse rank score for cyvcf2

--- scout/parse/variant/rank_score.py
+++ scout/parse/variant/rank_score.py
@@ -3,13 +3,20 @@ def parse_rank_score(variant, case_id):
     """Parse the rank score
     
         Args:
-            variant(dict): A variant dictionary
+            variant(cyvcf2.Variant): A cyvcf2 variant
             case_id(str)
         
         Returns:
             rank_score(float)
     """
-    rank_score = float(variant.get('rank_scores', {}).get(case_id, 0.0))
+    rank_score = None
+    rank_score_entry = variant.INFO.get('RankScore')
+    
+    if rank_score_entry:
+        for family_info in rank_score_entry.split(','):
+            splitted_info = family_info.split(':')
+            if case_id == splitted_info[0]:
+                rank_score = float(splitted_info[1])
     
     return rank_score
     
--- tests/parse/test_parse_rank_score.py
+++ tests/parse/test_parse_rank_score.py
@@ -1,19 +1,20 @@
 from scout.parse.variant.rank_score import parse_rank_score
 
-def test_parse_rank_score(one_variant, parsed_case):
-    """docstring for test_parse_rank_score"""
-    rank_scores_dict = one_variant['rank_scores']
+def test_parse_rank_score(cyvcf2_variant):
+    rank_scores_info = cyvcf2_variant.INFO['RankScore']
+    variant_score = rank_scores_info.split(':')[1]
+    family_id = rank_scores_info.split(':')[0]
     
-    rank_score = rank_scores_dict[parsed_case['display_name']]
+    rank_score = parse_rank_score(cyvcf2_variant, family_id)
     
-    assert float(rank_score) == parse_rank_score(one_variant, parsed_case['display_name'])
+    assert float(variant_score) == rank_score
 
-def test_parse_rank_scores(variants, parsed_case):
-    """docstring for test_parse_rank_score"""
-    case_id = parsed_case['display_name']
-    for variant in variants:
-        rank_scores_dict = variant['rank_scores']
-        
-        rank_score = rank_scores_dict[case_id]
-    
-        assert float(rank_score) == parse_rank_score(variant, case_id)
+# def test_parse_rank_scores(variants, parsed_case):
+#     """docstring for test_parse_rank_score"""
+#     case_id = parsed_case['display_name']
+#     for variant in variants:
+#         rank_scores_dict = variant['rank_scores']
+#
+#         rank_score = rank_scores_dict[case_id]
+#
+#         assert float(rank_score) == parse_rank_score(variant, case_id)
