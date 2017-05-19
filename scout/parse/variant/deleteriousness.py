26de23ae8975f26afd3b731e11f045cd960aaac4
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-05-19 17:40:38 +0200

    Adds parse deleteriousness

---
+++ scout/parse/variant/deleteriousness.py
@@ -0,0 +1,17 @@
+
+def parse_cadd(variant, transcripts):
+    """Check if the cadd phred score is annotated"""
+    cadd = 0
+    cadd_keys = ['CADD', 'CADD_PHRED']
+    for key in cadd_keys:
+        cadd = variant.INFO.get(key, 0)
+        if cadd:
+            return float(cadd)
+    
+    for transcript in transcripts:
+        cadd_entry = transcript.get('cadd')
+        if (cadd_entry and cadd_entry > cadd):
+            cadd = cadd_entry
+    
+    return cadd
+    
