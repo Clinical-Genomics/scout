0b9e83537aa4fc9a286d9ef1a8648674fe40b3ea
Robin Andeer <robin.andeer@gmail.com>
2017-05-29 14:05:06 +0200

    add proxy fix to auto start module

--- scout/server/auto.py
+++ scout/server/auto.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
 
+from werkzeug.contrib.fixers import ProxyFix
+
 from .app import create_app
 
 app = create_app(os.environ['SCOUT_CONFIG'])
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
