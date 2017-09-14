b875084e74ee03c6b251a79f04f0db340bb356b8
Måns Magnusson <mans.magnusson@scilifelab.se>
2017-09-14 10:53:12 +0200

    Fix #604

--- scout/constants/indexes.py
+++ scout/constants/indexes.py
@@ -1,11 +1,37 @@
 from pymongo import (IndexModel, ASCENDING, DESCENDING)
 
 INDEXES = {
-    'hgnc_collection': [IndexModel(
-        [('build', ASCENDING), ('chromosome', ASCENDING)], name="build_chromosome"),
+    'hgnc_collection': [
+        IndexModel([
+            ('build', ASCENDING), 
+            ('chromosome', ASCENDING)], 
+            name="build_chromosome"),
     ],
     'variant_collection': [
-        IndexModel([('case_id', ASCENDING),('rank_score', DESCENDING)], name="caseid_rankscore"),
-        IndexModel([('case_id', ASCENDING),('variant_rank', ASCENDING)], name="caseid_variantrank")
-    ]
+        IndexModel([
+            ('case_id', ASCENDING),
+            ('rank_score', DESCENDING)], 
+            name="caseid_rankscore"),
+        IndexModel([
+            ('case_id', ASCENDING),
+            ('variant_rank', ASCENDING)], 
+            name="caseid_variantrank"),
+        IndexModel([
+            ('case_id', ASCENDING), 
+            ('category', ASCENDING), 
+            ('variant_type', ASCENDING), 
+            ('rank_score', DESCENDING)],
+            name="caseid_category_varianttype_rankscore"),
+        IndexModel([
+            ('case_id', ASCENDING), 
+            ('variant_id', ASCENDING)],
+            name="caseid_variantid"),
+        IndexModel([
+            ('case_id', ASCENDING), 
+            ('variant_type', ASCENDING), 
+            ('variant_rank', ASCENDING), 
+            ('panels', ASCENDING), 
+            ('thousand_genomes_frequency', ASCENDING)],
+            name="caseid_varianttype_variantrank_panels_thousandg")
+    ],
 }
