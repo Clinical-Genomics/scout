49e764c7d06804f1023624e4470f747f2138f798
Robin Andeer <robin.andeer@gmail.com>
2017-09-25 08:49:21 +0200

    support authenticating to a database other than the main database

--- scout/commands/base.py
+++ scout/commands/base.py
@@ -8,7 +8,7 @@ import yaml
 # Adapter stuff
 from scout.adapter.mongo import MongoAdapter
 from scout.adapter.client import get_connection
-from pymongo.errors import (ConnectionFailure, ServerSelectionTimeoutError)
+from pymongo.errors import ConnectionFailure
 
 # General, logging
 from scout import __version__
@@ -33,83 +33,76 @@ log = logging.getLogger(__name__)
 
 
 @click.group()
-@click.option('-l', '--logfile',
-    type=click.Path(exists=False),
-    help="Path to log file. If none logging is printed to stderr.",
-)
-@click.option('--loglevel',
-    default='INFO',
-    type=click.Choice(LOG_LEVELS),
-    help="Set the level of log output.",
-    show_default=True,
-)
-@click.option('-db', '--mongodb', help='Name of mongo database')
+@click.option('--loglevel', default='INFO', type=click.Choice(LOG_LEVELS),
+              help="Set the level of log output.", show_default=True)
+@click.option('-db', '--mongodb', help='Name of mongo database [scout]')
 @click.option('-u', '--username')
 @click.option('-p', '--password')
+@click.option('-a', '--authdb', help='database to use for authentication')
 @click.option('-port', '--port', help="Specify on what port to listen for the mongod")
 @click.option('-h', '--host', help="Specify the host for the mongo database.")
-@click.option('-c', '--config',
-    type=click.Path(exists=True),
-    help="Specify the path to a config file with database info.",
-)
+@click.option('-c', '--config', type=click.Path(exists=True),
+              help="Specify the path to a config file with database info.")
 @click.option('--demo', is_flag=True, help="If the demo database should be used")
 @click.version_option(__version__)
 @click.pass_context
-def cli(context, mongodb, username, password, host, port, logfile, loglevel,
-        config, demo):
+def cli(context, mongodb, username, password, authdb, host, port, loglevel, config, demo):
     """scout: manage interactions with a scout instance."""
     coloredlogs.install(level=loglevel)
     log.info("Running scout version %s", __version__)
     log.debug("Debug logging enabled.")
 
-    mongo_configs = {}
-    configs = {}
+    mongo_config = {}
     if config:
-        log.debug("Use config file {0}".format(config))
+        log.debug("Use config file %s", config)
         with open(config, 'r') as in_handle:
-            configs = yaml.load(in_handle)
+            cli_config = yaml.load(in_handle)
+    else:
+        cli_config = {}
 
-    mongo_configs['mongodb'] = (mongodb or configs.get('mongodb') or 'scout')
+    mongo_config['mongodb'] = (mongodb or cli_config.get('mongodb') or 'scout')
     if demo:
-        mongo_configs['mongodb'] = 'scout-demo'
+        mongo_config['mongodb'] = 'scout-demo'
 
-    mongo_configs['host'] = (host or configs.get('host') or 'localhost')
-    mongo_configs['port'] = (port or configs.get('port') or 27017)
-    mongo_configs['username'] = username or configs.get('username')
-    mongo_configs['password'] = password or configs.get('password')
+    mongo_config['host'] = (host or cli_config.get('host') or 'localhost')
+    mongo_config['port'] = (port or cli_config.get('port') or 27017)
+    mongo_config['username'] = username or cli_config.get('username')
+    mongo_config['password'] = password or cli_config.get('password')
+    mongo_config['authdb'] = authdb or cli_config.get('authdb') or mongo_config['mongodb']
     # mongo uri looks like:
     # mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
 
     if context.invoked_subcommand in ('setup', 'serve'):
-        mongo_configs['adapter'] = None
+        mongo_config['adapter'] = None
     else:
-        log.info("Setting database name to %s", mongo_configs['mongodb'])
-        log.debug("Setting host to {0}".format(mongo_configs['host']))
-        log.debug("Setting port to {0}".format(mongo_configs['port']))
-        
+        log.info("Setting database name to %s", mongo_config['mongodb'])
+        log.debug("Setting host to %s", mongo_config['host'])
+        log.debug("Setting port to %s", mongo_config['port'])
+
         valid_connection = check_connection(
-            host=mongo_configs['host'], 
-            port=mongo_configs['port'], 
-            username=mongo_configs['username'], 
-            password=mongo_configs['password'], )
-    
+            host=mongo_config['host'],
+            port=mongo_config['port'],
+            username=mongo_config['username'],
+            password=mongo_config['password'],
+            authdb=mongo_config['authdb'],
+        )
+
         log.info("Test if mongod is running")
         if not valid_connection:
             log.warning("Connection could not be established")
             context.abort()
 
         try:
-            client = get_connection(**mongo_configs)
+            client = get_connection(**mongo_config)
         except ConnectionFailure:
             context.abort()
 
-        database = client[mongo_configs['mongodb']]
+        database = client[mongo_config['mongodb']]
 
         log.info("Setting up a mongo adapter")
-        mongo_adapter = MongoAdapter(database)
-        mongo_configs['adapter'] = mongo_adapter
+        mongo_config['adapter'] = MongoAdapter(database)
 
-    context.obj = mongo_configs
+    context.obj = mongo_config
 
 
 cli.add_command(load_command)
