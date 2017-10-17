162b464d5a92a30c6b90e92935e94353b06fe6a3
Robin Andeer <robin.andeer@gmail.com>
2017-10-17 15:49:58 +0200

    use CALLERS constant in parse code

--- scout/parse/variant/callers.py
+++ scout/parse/variant/callers.py
@@ -1,28 +1,18 @@
+from scout.constants import CALLERS
+
 
 def parse_callers(variant):
     """Parse how the different variant callers have performed
-    
+
         Args:
             variant(dict): A variant dictionary
-    
+
         Returns:
-            callers(dict): A dictionary on the form 
+            callers(dict): A dictionary on the form
             {'gatk': <filter>,'freebayes': <filter>,'samtools': <filter>}
     """
-    callers = {
-        'gatk': None,
-        'freebayes': None,
-        'samtools': None,
-        'mutect': None,
-        'pindel': None,
-    }
-    if variant.var_type == 'sv':
-        callers = {
-            'cnvnator': None,
-            'delly':None,
-            'tiddit':None,
-            'manta':None,
-        }
+    relevant_callers = CALLERS['sv' if variant.var_type == 'sv' else 'snv']
+    callers = {caller['id']: None for caller in relevant_callers}
     raw_info = variant.INFO.get('set')
     if raw_info:
         info = raw_info.split('-')
@@ -37,7 +27,7 @@ def parse_callers(variant):
                 for caller in callers:
                     if caller in call:
                         callers[caller] = 'Filtered'
-                
+
             elif call in set(callers.keys()):
                 callers[call] = 'Pass'
     # The following is parsing of a custom made merge
