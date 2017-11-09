d8fe4a69cf3fcf8c11836a38acac9f558310a757
Robin Andeer <robin.andeer@gmail.com>
2017-11-09 16:54:04 +0100

    display "n.d." (not determined) instead of -1 for SV variant length, fix #654

--- scout/server/blueprints/variants/controllers.py
+++ scout/server/blueprints/variants/controllers.py
@@ -118,6 +118,10 @@ def parse_variant(store, institute_obj, case_obj, variant_obj, update=False):
         acmg_code = ACMG_MAP[variant_obj['acmg_classification']]
         variant_obj['acmg_classification'] = ACMG_COMPLETE_MAP[acmg_code]
 
+    # convert length for SV variants
+    variant_length = variant_obj.get('length')
+    variant_obj['length'] = {100000000000: 'inf', -1: 'n.d.'}.get(variant_length, variant_length)
+
     return variant_obj
 
 
