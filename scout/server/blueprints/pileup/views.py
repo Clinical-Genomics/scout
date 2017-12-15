d445e7b9549d6d9a8f76785c92eb3947b7b606ba
Robin Andeer <robin.andeer@gmail.com>
2017-12-15 12:05:49 +0100

    dynamically get bam files in igv view

--- scout/server/blueprints/pileup/views.py
+++ scout/server/blueprints/pileup/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import os.path
+
 from flask import abort, Blueprint, render_template, request, make_response
 
 from .partial import send_file_partial
 from scout.server.utils import public_endpoint
+from scout.server.extensions import store
 
 pileup_bp = Blueprint('pileup', __name__, template_folder='templates',
                       static_folder='static', static_url_path='/pileup/static')
@@ -44,22 +47,28 @@ def viewer():
                            position=position, vcf_file=vcf_file)
 
 
-@pileup_bp.route('/igv.xml')
+@pileup_bp.route('<variant_id>/igv.xml')
 @public_endpoint
-def igv():
-    """Start IGV browser with a BAM file."""
-    vcf_file = request.args.get('vcf')
-    bam_files = request.args.getlist('bam')
-    bai_files = request.args.getlist('bai')
-    samples = request.args.getlist('sample')
-    alignments = [{'bam': bam, 'bai': bai, 'sample': sample}
-                  for bam, bai, sample in zip(bam_files, bai_files, samples)]
+def igv(variant_id):
+    """Start IGV browser for a variant."""
+    variant_obj = store.variant(variant_id)
+    case_obj = store.case(variant_obj['case_id'])
+
+    alignments = []
+    for individual in case_obj['individuals']:
+        bam_path = individual.get('bam_file')
+        if bam_path and os.path.exists(bam_path):
+            alignments.append({
+                'sample': individual['display_name'],
+                'bam': individual['bam_file']
+            })
+        else:
+            log.debug("%s: no bam file found", individual['individual_id'])
 
     position = {
-        'contig': request.args['contig'],
-        'start': request.args['start'],
-        'stop': request.args['stop']
+        'contig': variant_obj['chromosome'],
+        'start': variant_obj['start'] - 50,
+        'stop': variant_obj['stop'] + 50,
     }
 
-    return render_template('pileup/igv.xml', alignments=alignments,
-                           position=position, vcf_file=vcf_file)
+    return render_template('pileup/igv.xml', alignments=alignments, position=position)
