2c089366527f1f9e97a6dde7bd2506b1026e8437
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-01-31 12:25:55 +0100

    Updates export of genes

--- scout/export/gene.py
+++ scout/export/gene.py
@@ -2,18 +2,18 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def export_genes(adapter):
+def export_genes(adapter, build='37'):
     """Export all genes from the database"""
     logger.info("Exporting all genes to .bed format")
     
-    for gene in adapter.all_genes():
+    for gene in adapter.all_genes(build=build):
         gene_string = ("{0}\t{1}\t{2}\t{3}\t{4}")
         
         yield gene_string.format(
-            gene.chromosome,
-            gene.start,
-            gene.end,
-            gene.hgnc_symbol,
-            gene.hgnc_id
+            gene['chromosome'],
+            gene['start'],
+            gene['end'],
+            gene['hgnc_symbol'],
+            gene['hgnc_id']
         )
     
