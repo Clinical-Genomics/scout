ec9b38069766f3cb4840e35cba16752dff4f23ca
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-04-13 13:31:32 +0200

    pep8 refactoring

--- scout/adapter/mongo/institute.py
+++ scout/adapter/mongo/institute.py
@@ -17,7 +17,7 @@ class InstituteHandler(object):
         """
         internal_id = institute_obj['internal_id']
         display_name = institute_obj['internal_id']
-        
+
         # Check if institute already exists
         if self.institute(institute_id=internal_id):
             raise IntegrityError("Institute {0} already exists in database"
@@ -41,27 +41,27 @@ class InstituteHandler(object):
                 coverage_cutoff(int): Update coverage cutoff
         """
         updates = {}
-        
+
         if sanger_recipient:
             logger.info("Updating sanger recipients for institute"\
                         " {0} with {1}".format(
                             internal_id, sanger_recipient))
             updates['$push'] = {'sanger_recipients':sanger_recipient}
-        
+
         if coverage_cutoff:
             logger.info("Updating coverage cutoff for institute"\
                         " {0} with {1}".format(
                             internal_id, coverage_cutoff))
             updates['$set'] = {'coverage_cutoff': coverage_cutoff}
-        
+
         if updates:
             if '$set' in updates:
                 updates['$set']['updated_at'] = datetime.now()
             else:
                 updates['$set'] = dict(updated_at=datetime.now())
-            
+
             ##TODO should this raise if institute not found?
-            
+
             self.institute_collection.update_one({'_id':internal_id}, updates)
 
     def institute(self, institute_id):
@@ -79,7 +79,7 @@ class InstituteHandler(object):
         })
         if institute_obj is None:
             logger.debug("Could not find institute {0}".format(institute_id))
-        
+
         return institute_obj
 
     def institutes(self):
