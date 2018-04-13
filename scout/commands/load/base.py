1a126c8877508a62e4f180a0bdd4a1255b9f2f7b
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-04-13 13:30:22 +0200

    Refactored commands into separate files

---
+++ scout/commands/load/base.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import logging
+
+import click
+
+from .case import case as case_command
+
+from .institute import institute as institute_command
+from .panel import panel as panel_command
+from .research import research as research_command
+from .variants import variants as variants_command
+from .region import region as region_command
+from .user import user as user_command
+
+LOG = logging.getLogger(__name__)
+
+
+
+
+
+@click.group()
+@click.pass_context
+def load(context):
+    """Load the Scout database."""
+    pass
+
+
+@load.command()
+@click.argument('case_id')
+@click.argument('report_path', type=click.Path(exists=True))
+@click.pass_context
+def report(context, case_id, report_path):
+    """Add delivery report to an existing case."""
+    adapter = context.obj['adapter']
+    customer, family = case_id.split('-', 1)
+    existing_case = adapter.case(customer, family)
+    if existing_case is None:
+        click.echo("ERROR: no case found!")
+        context.abort()
+    existing_case.delivery_report = report_path
+    existing_case.save()
+    click.echo("saved report to case!")
+
+
+load.add_command(case_command)
+load.add_command(institute_command)
+load.add_command(region_command)
+load.add_command(panel_command)
+load.add_command(user_command)
+load.add_command(research_command)
+load.add_command(variants_command)
