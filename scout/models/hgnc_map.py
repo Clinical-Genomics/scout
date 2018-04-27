a5a723d1989a929acc2cbe133d79cfe21c40c7cc
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-04-27 14:52:34 +0200

    Creates new model for transcripts

--- scout/models/hgnc_map.py
+++ scout/models/hgnc_map.py
@@ -2,14 +2,55 @@ from __future__ import unicode_literals
 
 from bson.objectid import ObjectId
 
-hgnc_transcript = {
-                'ensembl_transcript_id': str, # required
-                'refseq_id': str,
-                'start': int, # required
-                'end': int, # required
-                'is_primary': bool,
-                'build': str, # '37' or '38', defaults to '37', required
-    }
+class Exon(dict):
+    """Exon dictionary
+    
+        "exon_id": str, # str(chrom-start-end)
+        "chrom": str, 
+        "start": int, 
+        "end": int,     
+        "transcript": str, # ENST ID
+        "hgnc_id": int,      # HGNC_id
+        "rank": int, # Order of exon in transcript
+        "build": str, # Genome build
+    
+    """
+    def __init__(self, exon_id, chrom, start, end, transcript, hgnc_id, rank, build='37'):
+        super(Exon, self).__init__()
+        self['exon_id'] = exon_id
+        self['chrom'] = chrom
+        self['start'] = int(start)
+        self['end'] = int(end)
+        self['transcript'] = transcript
+        self['hgnc_id'] = hgnc_id
+        self['rank'] = int(rank)
+        self['build'] = build
+
+class HgncTranscript(dict):
+    """Transcript dictionary
+    
+        "transcript_id": str, # ensembl id
+        "hgnc_id": int,
+        "chrom": str, 
+        "start": int, 
+        "end": int, 
+        "is_primary": bool, 
+        "refseq_id": str, 
+        "build": str, # Genome build
+        "length": int
+    """
+    def __init__(self, transcript_id, hgnc_id, chrom, start, end, is_primary=False, 
+                 refseq_id=None, build='37'):
+        super(HgncTranscript, self).__init__()
+        self['transcript_id'] = transcript_id
+        self['hgnc_id'] = int(hgnc_id)
+        self['chrom'] = chrom
+        self['start'] = int(start)
+        self['end'] = int(end)
+        self['is_primary'] = is_primary
+        self['refseq_id'] = refseq_id
+        self['build'] = build
+        self['length'] = self['end'] - self['start']
 
 
 hgnc_gene = {
