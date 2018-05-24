ca6aefbf7d16c51c93c85de09a8d3e1e75c70a5e
northwestwitch <rasi.chiara@gmail.com>
2018-05-24 10:11:29 +0200

    removed cached tests

--- scout/constants/case_tags.py
+++ scout/constants/case_tags.py
@@ -1,6 +1,6 @@
 ANALYSIS_TYPES = ('wgs', 'wes', 'mixed', 'unknown')
 
-SEX_MAP = {1: 'male', 2: 'female', 'other': 'unknown', 0: 'unknown'}
+SEX_MAP = {1: 'male', 2: 'female', 'other': 'unknown', '0': 'unknown'}
 REV_SEX_MAP = {value: key for key, value in SEX_MAP.items() if key != 'other'}
 
 PHENOTYPE_MAP = {1: 'unaffected', 2: 'affected', 0: 'unknown', -9: 'unknown'}
