f1bc3b965e069621a0d0bf1f84d63de368e80d8a
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-06-12 14:03:10 +0200

    Adds instructions for new release
    - Add a file that describes how to release; RELEASE.md
    - Update setup.py to Clinical Genomics best practice
    - Move __version__ to own file to be more explicit

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import codecs
-from setuptools import setup, find_packages
 
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'scout-browser'
+DESCRIPTION = 'Clinical DNA variant visualizer and browser.'
+URL = 'https://github.com/Clinical-Genomics/scout'
+EMAIL = 'mans.magnusson@scilifelab.se'
+AUTHOR = 'Måns Magnusson'
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 def parse_reqs(req_path='./requirements.txt'):
     """Recursively parse requirements from nested pip files."""
     install_requires = []
-    with codecs.open(req_path, 'r') as handle:
+    with io.open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as handle:
         # remove comments and empty lines
         lines = (line.strip() for line in handle
                  if line.strip() and not line.startswith('#'))
@@ -23,18 +40,69 @@ def parse_reqs(req_path='./requirements.txt'):
 
     return install_requires
 
+# What packages are required for this module to be executed?
+REQUIRED = parse_reqs()
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
-    name='scout-browser',
-    version='3.5.2',
-    url='https://github.com/Clinical-Genomics/scout',
-    description='Clinical DNA variant visualizer and browser.',
-    author='Robin Andeer',
-    author_email='robin.andeer@gmail.com',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
     packages=find_packages(exclude=['tests/', 'scripts/']),
-    include_package_data=True,
     zip_safe=False,
-    install_requires=parse_reqs(),
+    install_requires=REQUIRED,
+    include_package_data=True,
     extras_require=dict(
         coverage=['chanjo-report'],
     ),
@@ -51,6 +119,12 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries'
-    ]
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3.6',
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+    
 )
