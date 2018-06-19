1e90b94edccad67d9bbf84235dfed4a540310b04
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-06-19 13:19:54 +0200

    Updates scout load report

--- scout/commands/load/report.py
+++ scout/commands/load/report.py
@@ -1,21 +1,27 @@
 import logging
 import click
 
-LOG = logging.getLogger(__name__)
+from scout.load.report import load_report
+from scout.exceptions import (IntegrityError, ValidationError)
 
+LOG = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('case_id')
 @click.argument('report_path', type=click.Path(exists=True))
+@click.option('-u', '--update', 
+    is_flag=True
+)
 @click.pass_context
-def report(context, case_id, report_path):
+def report(context, case_id, report_path, update):
     """Add delivery report to an existing case."""
     adapter = context.obj['adapter']
-    customer, family = case_id.split('-', 1)
-    existing_case = adapter.case(customer, family)
-    if existing_case is None:
-        LOG.warning("no case found")
+    try:
+        updated_case = load_report(adapter, case_id, report_path, update)
+    except IntegrityError as err:
+        LOG.warning(err)
         context.abort()
-    existing_case.delivery_report = report_path
-    existing_case.save()
-    LOG.info("saved report to case!")
+    except ValidationError as err:
+        LOG.warning(err)
+        LOG.info("Use flag --update if it should be overwritten")
+        context.abort()
