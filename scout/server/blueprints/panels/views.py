d763e55a0a203f873f1b14e0e31d8f94cc055140
northwestwitch <rasi.chiara@gmail.com>
2018-08-24 13:39:18 +0200

    simplest view working

--- scout/server/blueprints/panels/views.py
+++ scout/server/blueprints/panels/views.py
@@ -121,9 +121,8 @@ def panel_export(panel_id):
     panel_obj = store.panel(panel_id)
     data = controllers.panel(store, panel_obj)
     data['report_created_at'] = datetime.datetime.now().strftime("%Y-%m-%d")
-    #html_report = render_template('panels/panel_pdf_simple.html', **data)
-    #return render_pdf(HTML(string=html_report), download_filename=data['panel']['panel_name']+'_'+str(data['panel']['version'])+'_'+datetime.datetime.now().strftime("%Y-%m-%d")+'_scout.pdf')
-    return render_template('panels/panel_pdf_simple.html', **data)
+    html_report = render_template('panels/panel_pdf_simple.html', **data)
+    return render_pdf(HTML(string=html_report), download_filename=data['panel']['panel_name']+'_'+str(data['panel']['version'])+'_'+datetime.datetime.now().strftime("%Y-%m-%d")+'_scout.pdf')
 
 
 @panels_bp.route('/panels/<panel_id>/update/<int:hgnc_id>', methods=['GET', 'POST'])
