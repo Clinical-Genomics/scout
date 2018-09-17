fe08ff2761574a9d5ac10f7c12d5cf7e1cd9dd7a
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-09-17 16:37:45 +0200

    Udapts tests to new api for loading gene panels

--- tests/conftest.py
+++ tests/conftest.py
@@ -574,37 +574,26 @@ def real_hpo_database(request, real_gene_database, hpo_terms_handle, hpo_to_gene
 
 
 @pytest.fixture(scope='function')
-def panel_database(request, gene_database, panel_info):
+def panel_database(request, gene_database, parsed_panel):
     "Returns an adapter to a database populated with user, institute and case"
     adapter = gene_database
     logger.info("Adding panel to adapter")
 
     adapter.load_panel(
-        path=panel_info['file'],
-        institute=panel_info['institute'],
-        panel_id=panel_info['panel_name'],
-        date=panel_info['date'],
-        panel_type=panel_info['type'],
-        version=panel_info['version'],
-        display_name=panel_info['full_name']
+        parsed_panel=parsed_panel,
     )
 
     return adapter
 
 
 @pytest.fixture(scope='function')
-def real_panel_database(request, real_gene_database, panel_info):
+def real_panel_database(request, real_gene_database, parsed_panel):
     "Returns an adapter to a database populated with user, institute and case"
     adapter = real_gene_database
     logger.info("Adding panel to real adapter")
+
     adapter.load_panel(
-        path=panel_info['file'],
-        institute=panel_info['institute'],
-        panel_id=panel_info['panel_name'],
-        date=panel_info['date'],
-        panel_type=panel_info['type'],
-        version=panel_info['version'],
-        display_name=panel_info['full_name']
+        parsed_panel=parsed_panel,
     )
 
     return adapter
