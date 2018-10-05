7d09056f7cfb382c069d9a4e71e576ff045d8765
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-10-05 14:55:10 +0200

    Removes Cursor.count() from cases views

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -27,7 +27,7 @@ cases_bp = Blueprint('cases', __name__, template_folder='templates',
 def index():
     """Display a list of all user institutes."""
     institute_objs = user_institutes(store, current_user)
-    institutes_count = ((institute_obj, store.cases(collaborator=institute_obj['_id']).count())
+    institutes_count = ((institute_obj, sum(1 for i in store.cases(collaborator=institute_obj['_id'])))
                         for institute_obj in institute_objs if institute_obj)
     return dict(institutes=institutes_count)
 
