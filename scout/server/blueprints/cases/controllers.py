6ddd2eb693390ab4cbb37ae0fb7f75f8360ba85b
northwestwitch <rasi.chiara@gmail.com>
2018-10-19 12:31:55 +0200

    return whole response for debugging

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -264,7 +264,7 @@ def coverage_report_contents(store, institute_obj, case_obj, base_url):
     #extract body content
     coverage_data = ''.join(['%s' % x for x in soup.body.contents])
 
-    return coverage_data
+    return str(resp)
 
 
 def clinvar_submissions(store, user_id, institute_id):
