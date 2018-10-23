790d882ff5cbd65be096bbf0be9201c34fa27343
northwestwitch <rasi.chiara@gmail.com>
2018-10-23 15:35:07 +0200

    modified gene panel update controller

--- scout/server/blueprints/panels/controllers.py
+++ scout/server/blueprints/panels/controllers.py
@@ -27,47 +27,70 @@ def existing_gene(store, panel_obj, hgnc_id):
     return existing_genes.get(hgnc_id)
 
 
-def update_panel(store, panel_name, csv_lines):
-    """Update an existing gene panel with genes."""
+def update_panel(store, panel_name, csv_lines, option='update'):
+    """Update an existing gene panel with genes.
+
+    Args:
+        store(scout.adapter.MongoAdapter)
+        panel_name(str)
+        csv_lines(iterable(str)): Stream with genes
+        option(str): 'update' or 'replace'
+
+    Returns:
+        panel_obj(dict)
+    """
+
     panel_obj = store.gene_panel(panel_name)
-    if panel_obj is None:
-        return None
+    #if panel_obj is None:
+    #    return None
 
-    existing_genes = {gene['hgnc_id'] for gene in panel_obj['genes']}
-    try:
-        new_genes = parse_genes(csv_lines)
-    except SyntaxError as error:
-        flash(error.args[0], 'danger')
-        return None
+    #existing_genes = {gene['hgnc_id'] for gene in panel_obj['genes']}
+    #try:
+    #    new_genes = parse_genes(csv_lines)
+    #except SyntaxError as error:
+    #    flash(error.args[0], 'danger')
+    #    return None
 
-    for new_gene in new_genes:
-        if not new_gene['hgnc_id']:
-            flash("gene missing hgnc id: {}".format(new_gene['hgnc_symbol']),
-                  'danger')
-            continue
+    #for new_gene in new_genes:
+    #    if not new_gene['hgnc_id']:
+    #        flash("gene missing hgnc id: {}".format(new_gene['hgnc_symbol']),
+    #              'danger')
+    #        continue
 
-        gene_obj = store.hgnc_gene(new_gene['hgnc_id'])
-        if gene_obj is None:
-            flash("gene not found: {} - {}".format(new_gene['hgnc_id'], new_gene['hgnc_symbol']),
-                  'danger')
-            continue
-        if new_gene['hgnc_symbol'] and gene_obj['hgnc_symbol'] != new_gene['hgnc_symbol']:
-            flash("symbol mis-match: {} | {}".format(gene_obj['hgnc_symbol'],
-                  new_gene['hgnc_symbol']), 'warning')
-        action = 'edit' if gene_obj['hgnc_id'] in existing_genes else 'add'
-        info_data = {
-            'disease_associated_transcripts': new_gene['transcripts'],
-            'reduced_penetrance': new_gene['reduced_penetrance'],
-            'mosaicism': new_gene['mosaicism'],
-            'inheritance_models': new_gene['inheritance_models'],
-            'database_entry_version': new_gene['database_entry_version'],
-        }
-        store.add_pending(panel_obj, gene_obj, action=action, info=info_data)
+    #    gene_obj = store.hgnc_gene(new_gene['hgnc_id'])
+    #    if gene_obj is None:
+    #        flash("gene not found: {} - {}".format(new_gene['hgnc_id'], new_gene['hgnc_symbol']),
+    #              'danger')
+    #        continue
+    #    if new_gene['hgnc_symbol'] and gene_obj['hgnc_symbol'] != new_gene['hgnc_symbol']:
+    #        flash("symbol mis-match: {} | {}".format(gene_obj['hgnc_symbol'],
+    #              new_gene['hgnc_symbol']), 'warning')
+    #    action = 'edit' if gene_obj['hgnc_id'] in existing_genes else 'add'
+    #    info_data = {
+    #        'disease_associated_transcripts': new_gene['transcripts'],
+    #        'reduced_penetrance': new_gene['reduced_penetrance'],
+    #        'mosaicism': new_gene['mosaicism'],
+    #        'inheritance_models': new_gene['inheritance_models'],
+    #        'database_entry_version': new_gene['database_entry_version'],
+    #    }
+    #    store.add_pending(panel_obj, gene_obj, action=action, info=info_data)
     return panel_obj
 
 
 def new_panel(store, institute_id, panel_name, display_name, csv_lines):
-    """Create a new gene panel."""
+    """Create a new gene panel.
+
+    Args:
+        store(scout.adapter.MongoAdapter)
+        institute_id(str)
+        panel_name(str)
+        display_name(str)
+        csv_lines(iterable(str)): Stream with genes
+
+    Returns:
+        panel_id: the ID of the new panel document created or None
+
+    """
     institute_obj = store.institute(institute_id)
     if institute_obj is None:
         flash("{}: institute not found".format(institute_id))
