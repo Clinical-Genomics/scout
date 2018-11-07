a8ba4f26fe6896e5e9b49650a3bc6e9ede2dfdff
Måns Magnusson <mans.magnusson@scilifelab.se>
2018-11-07 14:27:07 +0100

    Adds functionality to reupload svs from commandline

--- scout/commands/update/case.py
+++ scout/commands/update/case.py
@@ -29,9 +29,14 @@ LOG = logging.getLogger(__name__)
               help='path to research VCF with cancer variants to be added')
 @click.option('--peddy-ped', type=click.Path(exists=True),
               help='path to outfile .peddy.ped from peddy')
+@click.option('--reupload-sv', is_flag=True,
+              help='Remove all SVs and re upload from existing files')
+@click.option('--rankscore-treshold',
+              help='Set a new rank score treshold if desired')
 @click.pass_context
 def case(context, case_id, case_name, institute, collaborator, vcf, vcf_sv,
-         vcf_cancer, vcf_research, vcf_sv_research, vcf_cancer_research, peddy_ped):
+         vcf_cancer, vcf_research, vcf_sv_research, vcf_cancer_research, peddy_ped,
+         reupload_sv, rankscore_treshold):
     """
     Update a case in the database
     """
@@ -41,38 +46,57 @@ def case(context, case_id, case_name, institute, collaborator, vcf, vcf_sv,
             LOG.info("Please specify which case to update.")
             context.abort
         case_id = "{0}-{1}".format(institute, case_name)
-    # Chock if the case exists
+    # Check if the case exists
     case_obj = adapter.case(case_id)
     
     if not case_obj:
         LOG.warning("Case %s could not be found", case_id)
         context.abort()
     
+    case_changed = False
     if collaborator:
         if not adapter.institute(collaborator):
             LOG.warning("Institute %s could not be found", collaborator)
             context.abort()
         if not collaborator in case_obj['collaborators']:
+            case_changed = True
             case_obj['collaborators'].append(collaborator)
             LOG.info("Adding collaborator %s", collaborator)
     
     if vcf:
         LOG.info("Updating 'vcf_snv' to %s", vcf)
         case_obj['vcf_files']['vcf_snv'] = vcf
+        case_changed = True
     if vcf_sv:
         LOG.info("Updating 'vcf_sv' to %s", vcf_sv)
         case_obj['vcf_files']['vcf_sv'] = vcf_sv
+        case_changed = True
     if vcf_cancer:
         LOG.info("Updating 'vcf_cancer' to %s", vcf_cancer)
         case_obj['vcf_files']['vcf_cancer'] = vcf_cancer
+        case_changed = True
     if vcf_research:
         LOG.info("Updating 'vcf_research' to %s", vcf_research)
         case_obj['vcf_files']['vcf_research'] = vcf_research
+        case_changed = True
     if vcf_sv_research:
         LOG.info("Updating 'vcf_sv_research' to %s", vcf_sv_research)
         case_obj['vcf_files']['vcf_sv_research'] = vcf_sv_research
+        case_changed = True
     if vcf_cancer_research:
         LOG.info("Updating 'vcf_cancer_research' to %s", vcf_cancer_research)
         case_obj['vcf_files']['vcf_cancer_research'] = vcf_cancer_research
-
-    adapter.update_case(case_obj)
+        case_changed = True
+    
+    if case_changed:
+        adapter.update_case(case_obj)
+    
+    if reupload_sv:
+        rankscore_treshold = rankscore_treshold or case_obj.get("rank_score_threshold", 5)
+        sv_files = ['vcf_sv_research', 'vcf_sv']
+        adapter.delete_variants(case_id, variant_type='clinical', category='sv')
+        adapter.delete_variants(case_id, variant_type='research', category='sv')
+        if case_obj['vcf_files'].get('vcf_sv'):
+            adapter.load_variants(case_obj, variant_type='clinical', category='sv', rank_threshold=rank_score_threshold)
+        if case_obj['vcf_files'].get('vcf_sv_research'):
+            adapter.load_variants(case_obj, variant_type='research', category='sv', rank_threshold=rank_score_threshold)
