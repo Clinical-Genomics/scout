bada84553b88151839375372f0e45db9b99d06b7
northwestwitch <rasi.chiara@gmail.com>
2018-12-04 17:15:46 +0100

    removed api validation parte because it is handled by matchbox

--- scout/resources/__init__.py
+++ scout/resources/__init__.py
@@ -2,16 +2,10 @@ import pkg_resources
 
 ###### Files ######
 
-# MatchMaker Exchange json schema, as defined in (defined in https://github.com/ga4gh/mme-apis)
-mme_json_schema = 'resources/mme_api.json'
-
 # Cytoband
 cytobands_file = 'resources/cytoBand.txt.gz'
 
 ###### Paths ######
-
-# MatchMaker Exchange
-mme_schema_path = pkg_resources.resource_filename('scout', mme_json_schema)
 
 # Cytoband path
 cytobands_path = pkg_resources.resource_filename('scout', cytobands_file)
