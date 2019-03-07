7b5f71edfa69fcca602d51e1952cd025d129c580
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-03-07 14:19:38 +0100

    Add more query tests.

---
+++ tests/server/blueprints/cases/test_server_cases.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from flask import url_for
+
+def test_cases(app, user_obj, institute_obj):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for('auto_login'))
+        assert resp.status_code == 200
+
+        # WHEN accessing the cases page
+        resp = client.get(url_for('cases.cases',
+                                  institute_id=institute_obj['internal_id']))
+
+        # THEN it should return a page
+        assert resp.status_code == 200
+
+def test_cases_query(app, user_obj, case_obj, institute_obj):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    slice_query = case_obj['display_name']
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for('auto_login'))
+        assert resp.status_code == 200
+
+        # WHEN accessing the cases page with a query
+        resp = client.get(url_for('cases.cases',
+                                  query=slice_query,
+                                  institute_id=institute_obj['internal_id']))
+
+        # THEN it should return a page
+        assert resp.status_code == 200
+
+def test_cases_panel_query(app, user_obj, case_obj, parsed_panel, institute_obj):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    slice_query = parsed_panel['panel_id']
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for('auto_login'))
+        assert resp.status_code == 200
+
+        # WHEN accessing the cases page with a query
+        resp = client.get(url_for('cases.cases',
+                                  query=slice_query,
+                                  institute_id=institute_obj['internal_id']))
+
+        # THEN it should return a page
+        assert resp.status_code == 200
--- tests/server/conftest.py
+++ tests/server/conftest.py
@@ -30,6 +30,7 @@ def app(real_database_name, real_variant_database, user_obj):
 
     return app
 
+
 @pytest.fixture
 def institute_info():
     _institute_info = dict(internal_id='cust000', display_name='test_institute')
@@ -41,34 +42,3 @@ def user_info(institute_info):
     _user_info = dict(email='john@doe.com', name='John Doe', roles=['admin'],
                       institutes=[institute_info['internal_id']])
     return _user_info
-
-
-#@pytest.fixture
-#def real_database(request, database_name, institute_obj, user_obj, genes, parsed_panel):
-#    """Setup a real database with populated data"""
-#    mongo_client = pymongo.MongoClient()
-#    mongo_client.drop_database(database_name)
-#
-#    database = mongo_client[database_name]
-#    adapter = MongoAdapter(database)
-#
-#    # Populate the database
-#    adapter.add_institute(institute_obj)
-#    adapter.add_user(user_obj)
-#
-#    load_hgnc_genes(adapter, genes)
-#    adapter.hgnc_collection.create_index([('build', pymongo.ASCENDING),
-#                                          ('hgnc_symbol', pymongo.ASCENDING)])
-#    adapter.load_panel(parsed_panel=parsed_panel)
-
-#    load_hpo(adapter=gene_database, hpo_lines=hpo_terms_handle, disease_lines=genemap_handle)
-#    adapter.add_case(case_obj)
-
-    # for variant in variant_objs:
-    #     adapter.load_variant(variant)
-
-#    def teardown():
-##
-#    request.addfinalizer(teardown)
-#
-#    return adapter
