eb6649380dc114bc69745b471363c0ca2a7abad2
northwestwitch <rasi.chiara@gmail.com>
2019-03-11 14:50:20 +0100

    superugly check in views

--- scout/server/blueprints/dashboard/views.py
+++ scout/server/blueprints/dashboard/views.py
@@ -43,8 +43,13 @@ def index():
     #1) Their default institute when the page is first loaded
     #2) if they ask for an institute that they don't belong to
     #3) if they want perform a query on all institutes
-    if not institute_id or (institute_id not in accessible_institutes) or ( slice_query and institute_id=='None' ) :
+    if not institute_id:
         institute_id = accessible_institutes[0]
+    elif not institute_id =='None' and not institute_id in accessible_institutes:
+        institute_id = accessible_insstitutes[0]
+    elif slice_query and institute_id=='None' and not current_user.is_admin:
+        institute_id = accessible_institutes[0]
+
 
     LOG.info("Fetch all cases with institute: %s", institute_id)
 
