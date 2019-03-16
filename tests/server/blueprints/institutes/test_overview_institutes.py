3902b45e30989e1f9bf894b655bcf0cdbb2ef78b
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-03-16 15:03:25 +0100

    Test institutes overview.

---
+++ tests/server/blueprints/institutes/test_overview_institutes.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from flask import url_for
+
+def test_overview(app, user_obj, institute_obj):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for('auto_login'))
+        assert resp.status_code == 200
+
+        # WHEN accessing the cases page
+        resp = client.get(url_for('overview.institutes'))
+
+        # THEN it should return a page
+        assert resp.status_code == 200
