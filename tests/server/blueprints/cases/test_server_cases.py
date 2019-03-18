5f73f2f4e464b7cf2c92b9f9ee6dc17180d7fcb7
northwestwitch <rasi.chiara@gmail.com>
2019-03-18 11:45:26 +0100

    started adding tests for mme views

--- tests/server/blueprints/cases/test_server_cases.py
+++ tests/server/blueprints/cases/test_server_cases.py
@@ -54,3 +54,23 @@ def test_cases_panel_query(app, user_obj, case_obj, parsed_panel, institute_obj)
 
         # THEN it should return a page
         assert resp.status_code == 200
+
+
+def test_matchmaker_matches(app,case_obj,institute_obj, ):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for('auto_login'))
+        assert resp.status_code == 200
+
+        # WHEN accessing the cases page with a query
+        resp = client.get(url_for('cases.matchmaker_matches',
+                                  institute_id=institute_obj['internal_id'],
+                                  case_name=case_obj['display_name']
+                                  ))
+
+        # The page should redirect because default user is not allowed
+        # to save or view cases in MatchMaker
+        assert resp.status_code == 302
