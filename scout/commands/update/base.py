4f2e87b8d42fdc2e2138c9624a1529fc3f9bb5a8
northwestwitch <rasi.chiara@gmail.com>
2019-04-02 16:39:42 +0200

    modified cli to update compounds and created test

--- scout/commands/update/base.py
+++ scout/commands/update/base.py
@@ -34,7 +34,7 @@ update.add_command(user_command) #done!
 update.add_command(panel_command) #done!
 update.add_command(case_command) #done!
 update.add_command(omim_command) #done!
-update.add_command(compound_command)
+update.add_command(compound_command) #done!
 update.add_command(hpo_command)
 update.add_command(gene_command)
 update.add_command(disease_command)
