fefcc84c89c4d4004b854c276c4257f3cea44099
northwestwitch <rasi.chiara@gmail.com>
2019-04-26 10:03:33 +0200

    do not update case if there are no updates to make

--- scripts/update_files_path.py
+++ scripts/update_files_path.py
@@ -66,7 +66,7 @@ def do_replace(db_uri, old_path, new_path, test):
             click.echo('fixing case {0}/{1} [{2},{3}] --> updated {4} paths'.format(i+1, n_cases, case['owner'], case['display_name'], updates))
 
             # update case object in database
-            if test is False:
+            if updates and test is False:
                 match_condition = {'_id' : case['_id']}
                 updated_case = db.case.find_one_and_update(match_condition,
                     {'$set':set_command}, return_document=pymongo.ReturnDocument.AFTER)
