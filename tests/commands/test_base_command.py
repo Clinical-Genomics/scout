112b7cce5d80572f12ab06e2891fdee536235fe5
northwestwitch <rasi.chiara@gmail.com>
2019-05-09 11:56:22 +0200

    simpler scout -v test

--- tests/commands/test_base_command.py
+++ tests/commands/test_base_command.py
@@ -1,23 +1,19 @@
 import pytest
 
 from scout.commands import app_cli
+from scout import __version__
 from flask.cli import current_app
 
 #Sanity check for cli
-def test_version(mock_app):
+def test_base_cmd(mock_app):
 
     runner = mock_app.test_cli_runner()
     assert runner
 
-    # Test CLI base, no arguments provided
-    result =  runner.invoke(app_cli, [])
+    # test scout -v command
+    result =  runner.invoke(app_cli, ['--loglevel', 'DEBUG', '-v'])
     assert result.exit_code == 0
-    assert 'Entry point of Scout CLI' in result.output
-
-    # Test changing log level using loglevel param
-    result =  runner.invoke(app_cli, ['--loglevel', 'DEBUG', '--help'])
-    assert result.exit_code == 0
-    assert 'Running scout version' not in result.output
+    assert 'Running scout version {}'.format(__version__) in result.output
 
     # test setting the database to scout-demo using the CLI
     result = runner.invoke(app_cli, ['--demo', 'view', 'institutes'])
