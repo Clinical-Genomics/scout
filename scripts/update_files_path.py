ce1d32e2d3437b640ca0b488e86197faa2435d4e
northwestwitch <rasi.chiara@gmail.com>
2019-05-16 10:26:36 +0200

    better printout

--- scripts/update_files_path.py
+++ scripts/update_files_path.py
@@ -56,10 +56,10 @@ def do_replace(db_uri, old_path, new_path, test, discover):
             # fix delivery report path
             d_report = case.get('delivery_report')
             if d_report and old_path in d_report:
-                replace_fields.append(['case[delivery_report]',d_report,'*'])
+                replace_fields.append(['case[delivery_report]',d_report])
                 set_command['delivery_report'] = d_report.replace(old_path, new_path)
             elif d_report:
-                fields.append(['case[delivery_report]',d_report,''])
+                fields.append(['case[delivery_report]',d_report])
 
             # fix delivery report when there are analysis-specific reports
             analyses = case.get('analyses')
@@ -68,11 +68,11 @@ def do_replace(db_uri, old_path, new_path, test, discover):
                 for n, analysis in enumerate(analyses):
                     d_report = analysis.get('delivery_report')
                     if d_report and old_path in d_report:
-                        replace_fields.append(['case[analyses][{}][delivery_report]'.format(n),d_report,'*'])
+                        replace_fields.append(['case[analyses][{}][delivery_report]'.format(n),d_report])
                         analyses[n]['delivery_report'] = d_report.replace(old_path, new_path)
                         update=True
                     elif d_report:
-                        fields.append(['case[analyses][{}][delivery_report]'.format(n),d_report,''])
+                        fields.append(['case[analyses][{}][delivery_report]'.format(n),d_report])
 
             if update:
                 set_command['analyses'] =  analyses
@@ -80,10 +80,10 @@ def do_replace(db_uri, old_path, new_path, test, discover):
             # fix delivery report path when 'delivery_path' key exists in case object:
             d_path = case.get('delivery_path')
             if d_path and old_path in d_path:
-                replace_fields.append(['case[delivery_path]',d_path,'*'])
+                replace_fields.append(['case[delivery_path]',d_path])
                 set_command['delivery_path'] = d_path.replace(old_path, new_path)
             elif d_path:
-                fields.append(['case[delivery_path]',d_path,''])
+                fields.append(['case[delivery_path]',d_path])
 
             # fix links to VCF files:
             update=False
@@ -91,11 +91,11 @@ def do_replace(db_uri, old_path, new_path, test, discover):
                 for vcf_type in VCF_FILES:
                     path_to_vcf_type = case['vcf_files'].get(vcf_type)
                     if path_to_vcf_type and old_path in path_to_vcf_type:
-                        replace_fields.append(['case[vcf_files][{}]'.format(vcf_type),path_to_vcf_type,'*'])
+                        replace_fields.append(['case[vcf_files][{}]'.format(vcf_type),path_to_vcf_type])
                         case['vcf_files'][vcf_type] = path_to_vcf_type.replace(old_path, new_path)
                         update=True
                     elif path_to_vcf_type:
-                        fields.append(['case[vcf_files][{}]'.format(vcf_type),path_to_vcf_type,''])
+                        fields.append(['case[vcf_files][{}]'.format(vcf_type),path_to_vcf_type])
             if update:
                 set_command['vcf_files'] = case['vcf_files']
 
@@ -109,9 +109,9 @@ def do_replace(db_uri, old_path, new_path, test, discover):
                         if ind_file_path and old_path in ind_file_path:
                             update = True
                             ind_obj[ind_file] = ind_file_path.replace(old_path, new_path)
-                            replace_fields.append(['case[individuals][{}][{}]'.format(z,ind_file),ind_file_path,'*'])
+                            replace_fields.append(['case[individuals][{}][{}]'.format(z,ind_file),ind_file_path])
                         elif ind_file_path:
-                            fields.append(['case[individuals][{}][{}]'.format(z,ind_file),ind_file_path,''])
+                            fields.append(['case[individuals][{}][{}]'.format(z,ind_file),ind_file_path])
 
                     case['individuals'][z] = ind_obj
             if update:
@@ -120,9 +120,7 @@ def do_replace(db_uri, old_path, new_path, test, discover):
             click.echo('####### fixing case {0}/{1} [{2},{3}] ########'.format(i+1, n_cases, case['owner'], case['display_name']))
 
             click.echo('Replace n={} fields with new path.'.format(len(replace_fields)))
-            print(tabulate(replace_fields, ['key','path','replace'], tablefmt="grid"))
-            click.echo('other paths:')
-            print(tabulate(fields, ['key','path','replace'], tablefmt="grid"))
+            print(tabulate(replace_fields, ['key','path'], tablefmt="grid"))
 
             # update case object in database
             if replace_fields and test is False:
@@ -133,7 +131,11 @@ def do_replace(db_uri, old_path, new_path, test, discover):
                 if updated_case:
                     click.echo('---> case updated!')
 
-            click.echo('#'*100+'\n')
+            click.echo('other paths:')
+            for field in fields:
+                print(field)
+
+            click.echo('#'*100+'\n\n')
 
     except Exception as err:
         click.echo('Error {}'.format(err))
