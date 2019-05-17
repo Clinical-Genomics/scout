d2b7ef6b9c052e71e3ff58faaa7557586316b10c
northwestwitch <rasi.chiara@gmail.com>
2019-05-17 12:06:35 +0200

    better test the cli base

--- tests/commands/test_base_command.py
+++ tests/commands/test_base_command.py
@@ -1,20 +1,35 @@
 import pytest
 
+from click.testing import CliRunner
 from scout.commands import cli
 from scout import __version__
 from flask.cli import current_app
 
 #Sanity check for cli
-def test_base_cmd(mock_app):
+def test_base_cmd():
 
-    runner = mock_app.test_cli_runner()
+    # Create a test CLI runner
+    runner = CliRunner()
     assert runner
 
-    # test scout -v command
-    result =  runner.invoke(cli, ['--loglevel', 'DEBUG', '-v'])
+    # test the CLI base, no arguments provided
+    result =  runner.invoke(cli)
     assert result.exit_code == 0
-    assert 'Running scout version {}'.format(__version__) in result.output
+    # debug message should be printed
+    assert 'Debug logging enabled.' in result.output
 
-    # test setting the database to scout-demo using the CLI
-    result = runner.invoke(cli, ['--demo', 'view', 'institutes'])
+    # test the cli with a different loglevel that DEBUG
+    result =  runner.invoke(cli, ['--loglevel', 'WARNING'])
     assert result.exit_code == 0
+    # debug message should NOT be printed
+    assert 'Debug logging enabled.' not in result.output
+
+    # test the cli with -v (version) option
+    result =  runner.invoke(cli, ['-v'])
+    assert result.exit_code == 0
+    assert 'INFO Running scout version {}'.format( __version__) in result.output
+
+    # test the cli with --demo option
+    result =  runner.invoke(cli, ['--demo'])
+    assert result.exit_code == 0
+    assert 'connecting to database:' in result.output and 'scout-demo' in result.output
