33adc5b7e959d63774677ad440a19e54ecc2439a
northwestwitch <rasi.chiara@gmail.com>
2019-06-24 17:10:26 +0200

    double sorting because some vars are missing keys

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -326,7 +326,7 @@ def causatives(institute_id):
         except ValueError:
             flash('Provided gene info could not be parsed!', 'warning')
 
-    variants = store.check_causatives(institute_obj=institute_obj,limit_genes=hgnc_id).sort("hgnc_symbols", pymongo.ASCENDING)
+    variants = store.check_causatives(institute_obj=institute_obj,limit_genes=hgnc_id).sort("hgnc_symbol", pymongo.ASCENDING).sort("genes.hgnc_symbol", pymongo.ASCENDING)
     all_variants = {}
     all_cases = {}
     for variant_obj in variants:
