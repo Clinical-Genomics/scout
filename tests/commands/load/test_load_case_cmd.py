253f87d14ea7b86dd54585e6e65b6ba8c1ca01fb
mikaell <mikael.laaksonen@scilifelab.se>
2019-08-28 15:29:45 +0200

    Add Test for KeyError in LOAD
    Add a test for testing KeyError in the LOAD command.

--- tests/commands/load/test_load_case_cmd.py
+++ tests/commands/load/test_load_case_cmd.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import pytest
 from scout.demo import load_path
 
 from scout.commands import cli
@@ -21,6 +22,34 @@ def test_load_case(mock_app, institute_obj, case_obj):
     assert os.path.exists(load_path)
 
     # Test command to upload case using demo resources:
-    result =  runner.invoke(cli, ['load', 'case', load_path ])
+    result = runner.invoke(cli, ['load', 'case', load_path ])
     assert result.exit_code == 0
     assert store.case_collection.find().count() == 1
+
+
+
+
+def test_load_case_KeyError(mock_app, institute_obj, case_obj):
+    # GIVEN a config setup with 'sample_id' missing
+    runner = mock_app.test_cli_runner()
+    assert runner
+
+    # remove case from real populated database using adapter
+    store.delete_case(case_id=case_obj['_id'])
+    assert store.case_collection.find().count() == 0
+    assert store.institute_collection.find({'_id':'cust000'}).count()==1
+
+    # Make sure the scout config file is available
+    assert os.path.exists(load_path)
+    sed_change = "sed -i -e 's/sample_id/SAMPLE_ID/g' "
+    os.system(sed_change + load_path)    
+
+    # Test command to upload case using demo resources:
+    # WHEN: config is loaded
+    result = runner.invoke(cli, ['load', 'case', load_path ])
+    # THEN KeyError is caught and exit value is non-zero
+    assert result.exit_code != 0
+
+    sed_restore = "sed -i -e 's/SAMPLE_ID/sample_id/g' "    # restore config
+    os.system(sed_restore + load_path)    
+    
