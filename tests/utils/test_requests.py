69229d80a6f9ea4ed5ef56f27742b35a8a5cb1e1
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-09-09 13:44:55 +0200

    Adds TRAVIS to test

--- tests/utils/test_requests.py
+++ tests/utils/test_requests.py
@@ -3,6 +3,8 @@
 import pytest
 from scout.utils.requests import fetch_refseq_version, get_request
 
+TRAVIS = os.getenv('TRAVIS')
+
 def test_get_request_bad_url():
     """Test functions that accepts an url and returns decoded data from it"""
 
