3f759149464ffd9f10f74a2c5a097998cb71cd80
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-10-24 13:59:11 +0200

    Adds current user

--- scout/server/blueprints/variant/controllers.py
+++ scout/server/blueprints/variant/controllers.py
@@ -1,3 +1,5 @@
+from flask_login import current_user
+
 from scout.server.utils import (institute_and_case, variant_case, user_institutes)
 from scout.constants import (CALLERS, MANUAL_RANK_OPTIONS, DISMISS_VARIANT_OPTIONS, VERBS_MAP, 
                              ACMG_MAP, MOSAICISM_OPTIONS, ACMG_OPTIONS, ACMG_COMPLETE_MAP)
