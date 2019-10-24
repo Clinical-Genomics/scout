931ab2f5ede5dd288eee0af9662f48902acf7aec
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-10-24 13:07:30 +0200

    Adds functionality to export a single gene

--- scout/commands/export/gene.py
+++ scout/commands/export/gene.py
@@ -15,14 +15,25 @@ LOG = logging.getLogger(__name__)
 @click.command('genes', short_help='Export genes')
 @build_option
 @json_option
+@click.option('-i', '--hgnc-id',
+    type=int,
+    help="Specify what gene to fetch"
+)
 @with_appcontext
-def genes(build, json):
-    """Export all genes from a build"""
+def genes(build, json, hgnc_id):
+    """Export genes from Scout"""
     LOG.info("Running scout export genes")
     adapter = store
-    result = adapter.all_genes(build=build)
-    gene_list = list(result)
-    if build == 'GRCh38':
+    if hgnc_id:
+        gene_obj = adapter.hgnc_gene(hgnc_id, build=build)
+        if not gene_obj:
+            LOG.info("No result found for %s", hgnc_id)
+            return
+        gene_list = [gene_obj]
+    else:
+        result = adapter.all_genes(build=build)
+        gene_list = list(result)
+    if '38' in build:
         for gene_obj in gene_list:
             if gene_obj['chromosome'] == 'MT':
                 gene_obj['chromosome'] = 'M'
