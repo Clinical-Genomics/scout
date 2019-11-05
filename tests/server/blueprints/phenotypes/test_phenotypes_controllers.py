7d7999b383bebad0bad9b815fcb9f372677faa5d
northwestwitch <rasi.chiara@gmail.com>
2019-11-05 15:42:06 +0100

    added a test

---
+++ tests/server/blueprints/phenotypes/test_phenotypes_controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from scout.server.blueprints.phenotypes import controllers
+
+def test_hpo_terms(real_adapter):
+
+    adapter = real_adapter
+
+    ## GIVEN a adapter with one hpo term
+    hpo_term = dict(
+        _id = 'HP1', # Same as hpo_id
+       hpo_id = 'HP1', # Required
+       description = 'First term',
+       genes = [1], # List with integers that are hgnc_ids
+    )
+    adapter.load_hpo_term(hpo_term)
+
+    # The phenotypes controller should be able to return that term
+    results = controllers.hpo_terms(adapter)
+    assert results['phenotypes'] == [hpo_term]
