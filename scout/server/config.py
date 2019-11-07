938a32d29ec4ffc59695a873a359302b77b6936e
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-11-07 15:36:09 +0100

    Adds loquspath to config. Fix #1422

--- scout/server/config.py
+++ scout/server/config.py
@@ -18,7 +18,7 @@ MAIL_USE_SSL = False
 
 # connection details for LoqusDB MongoDB database
 LOQUSDB_SETTINGS = dict(
-    # binary='/Users/mansmagnusson/miniconda3/envs/loqusdb/bin/loqusdb',
+    binary='/Users/mansmagnusson/miniconda3/envs/loqusdb/bin/loqusdb',
     # config_path=<path/to/loqus/config>
     
 )
