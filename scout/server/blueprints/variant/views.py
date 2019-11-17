daaf7c389c7727f58c9d2c6ea7ac35215fd14e1f
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-11-17 11:13:11 +0100

    Fix #1515 - don't generate 'Select a tag' and catch them if we do.

--- scout/server/blueprints/variant/views.py
+++ scout/server/blueprints/variant/views.py
@@ -91,7 +91,13 @@ def variant_update(institute_id, case_name, variant_id):
 
     manual_rank = request.form.get('manual_rank')
     if manual_rank:
-        new_manual_rank = int(manual_rank) if manual_rank != '-1' else None
+        try:
+            new_manual_rank = int(manual_rank) if manual_rank != '-1' else None
+        except:
+            LOG.warning("Attempt to update manual rank with invalid value {}".format(manual_rank))
+            manual_rank = '-1'
+            new_manual_rank = -1
+
         store.update_manual_rank(institute_obj, case_obj, user_obj, link, variant_obj,
                                  new_manual_rank)
         if new_manual_rank:
