a30be8e4699d0098b3c4a0bc3a61c24228f5a0f1
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-11-28 17:37:47 +0100

    And a test for the SV controller.

--- tests/server/blueprints/variants/test_variants_controllers.py
+++ tests/server/blueprints/variants/test_variants_controllers.py
@@ -2,7 +2,8 @@ import copy
 import logging
 
 from scout.server.blueprints.variants.controllers import (variants_export_header,
-                                                          variant_export_lines, variants)
+                                                          variant_export_lines,
+                                                          variants, sv_variants)
 
 LOG = logging.getLogger(__name__)
 
@@ -71,6 +72,51 @@ def test_variants_research_shadow_clinical_assessments(real_variant_database, in
 
     # THEN previous annotations are reported back for the reseach case.
     assert any([variant.get('clinical_assessments') for variant in res_variants])
+
+def test_sv_variants_research_shadow_clinical_assessments(real_variant_database, institute_obj, case_obj):
+    # GIVEN a db with variants,
+    adapter = real_variant_database
+    case_id = case_obj['_id']
+
+    # GIVEN a clinical variant from one case
+    variant_clinical = adapter.variant_collection.find_one({'case_id': case_id, 'variant_type': 'clinical'})
+    # GIVEN the variant is an SV
+    adapter.variant_collection.update_one({'_id': variant_clinical['_id']},
+        {'$set' : {'category': 'sv', 'sub_category': 'dup'}})
+
+    # GIVEN a copy of that variant marked research
+    variant_research = copy.deepcopy(variant_clinical)
+    variant_research['_id'] = 'research_version'
+    variant_research['variant_type'] = 'research'
+    variant_research['category'] = 'sv'
+    variant_research['sub_category']: 'dup'
+    adapter.variant_collection.insert_one(variant_research)
+
+    # WHEN updating the manual assessments of the clinical variant
+    adapter.variant_collection.update_one({'_id': variant_clinical['_id']},
+        {'$set' : {
+                    'manual_rank': 2,
+                    'mosaic_tags': [ "1" ],
+                    'dismiss_variant': [ "2", "3" ]
+        }})
+
+    # WHEN filtering for that variant in research
+    variants_query = {'variant_type': 'research'}
+    variants_query_res = adapter.variants(case_obj['_id'], query=variants_query,
+                                            category='sv')
+    assert variants_query_res
+
+    res = sv_variants(adapter, institute_obj, case_obj, variants_query_res)
+    res_variants = res['variants']
+
+    LOG.debug('Variants: {}'.format(res_variants))
+
+    # THEN it is returned
+    assert any([variant['_id'] == variant_research['_id'] for variant in res_variants])
+
+    # THEN previous annotations are reported back for the reseach case.
+    assert any([variant.get('clinical_assessments') for variant in res_variants])
+
 
 def test_variant_csv_export(real_variant_database, case_obj):
     adapter = real_variant_database
