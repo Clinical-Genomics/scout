51e1b1d3a14a10dbed53d0c9c61b6ed8597e8120
northwestwitch <rasi.chiara@gmail.com>
2020-01-14 09:01:01 +0100

    split conservation INFO parsing from CSQ parsing

--- scout/parse/variant/conservation.py
+++ scout/parse/variant/conservation.py
@@ -1,33 +1,48 @@
+import logging
+
 from scout.constants import CONSERVATION
 import numbers
 
-def parse_conservations(variant):
+logger = logging.getLogger(__name__)
+
+def parse_conservations(variant, vep_header=None):
     """Parse the conservation predictors
 
         Args:
             variant(dict): A variant dictionary
+            vep_header(list): CSQ field might contain conservation annotations
 
         Returns:
             conservations(dict): A dictionary with the conservations
     """
     conservations = {}
 
-    conservations['gerp'] = parse_conservation(
-                                            variant,
-                                            'dbNSFP_GERP___RS'
-                                        )
-    conservations['phast'] = parse_conservation(
-                                            variant,
-                                            'dbNSFP_phastCons100way_vertebrate'
-                                        )
-    conservations['phylop'] = parse_conservation(
-                                            variant,
-                                            'dbNSFP_phyloP100way_vertebrate'
-                                        )
+    conservation_keys = {
+        'info_keys' : {
+            'gerp' : 'dbNSFP_GERP___RS',
+            'phast' : 'dbNSFP_phastCons100way_vertebrate',
+            'phylop' : 'dbNSFP_phyloP100way_vertebrate'
+        },
+        'csq_keys' : {
+            'gerp' : 'GERP++_RS',
+            'phast' : 'phastCons100way_vertebrate',
+            'phylop' : 'phyloP100way_vertebrate'
+        }
+    }
+
+    if any(elem in list(conservation_keys['csq_keys'].values()) for elem in vep_header):
+        # if CSQ field contains any conservation key specified in conservation_keys['csq_keys']
+        logger.info('-------------------------->{}'.format(variant.INFO.get('CSQ')))
+
+    else: # parse conservation from the INFO field
+        for key,value in conservation_keys['info_keys'].items():
+            conservations[key] = parse_conservation_info(variant,value)
+
     return conservations
 
-def parse_conservation(variant, info_key):
-    """Get the conservation prediction
+
+def parse_conservation_info(variant, info_key):
+    """Get the conservation prediction from the INFO field
 
         Args:
             variant(dict): A variant dictionary
@@ -50,3 +65,16 @@ def parse_conservation(variant, info_key):
                 conservations.append('NotConserved')
 
     return conservations
+
+
+def parse_conservation_csq(variant, info_key):
+    """Get the conservation prediction from the CSQ field
+
+        Args:
+            variant(dict): A variant dictionary
+            info_key(str)
+
+        Returns:
+            conservations(list): List of censervation terms
+    """
+    vep_info = variant.INFO.get('CSQ')
