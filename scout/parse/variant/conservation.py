7f198247839343eb53ed9ddee704529c78d027a0
northwestwitch <rasi.chiara@gmail.com>
2020-01-14 14:47:26 +0100

    included test and fixed conservation parsing from CSQ field

--- scout/parse/variant/conservation.py
+++ scout/parse/variant/conservation.py
@@ -3,9 +3,9 @@ import logging
 from scout.constants import CONSERVATION
 import numbers
 
-logger = logging.getLogger(__name__)
+LOG = logging.getLogger(__name__)
 
-def parse_conservations(variant, vep_header=None):
+def parse_conservations(variant, vep_header=[]):
     """Parse the conservation predictors
 
         Args:
@@ -29,24 +29,33 @@ def parse_conservations(variant, vep_header=None):
             'phylop' : 'phyloP100way_vertebrate'
         }
     }
+    vep_info = variant.INFO.get('CSQ')
 
+    # if CSQ field contains any conservation key specified in conservation_keys['csq_keys']
     if any(elem in list(conservation_keys['csq_keys'].values()) for elem in vep_header):
-        # if CSQ field contains any conservation key specified in conservation_keys['csq_keys']
-        logger.info('-------------------------->{}'.format(variant.INFO.get('CSQ')))
+
+        raw_transcripts = list(dict(zip(vep_header, transcript_info.split('|')))
+                           for transcript_info in vep_info.split(','))
+
+        for key,value in conservation_keys['csq_keys'].items():
+            conservations[key] = parse_conservation_csq(raw_transcripts, value, key)
+
 
     else: # parse conservation from the INFO field
         for key,value in conservation_keys['info_keys'].items():
-            conservations[key] = parse_conservation_info(variant,value)
+            conservations[key] = parse_conservation_info(variant, value, key)
 
     return conservations
 
 
-def parse_conservation_info(variant, info_key):
+def parse_conservation_info(variant, info_key, field_key):
     """Get the conservation prediction from the INFO field
 
         Args:
             variant(dict): A variant dictionary
-            info_key(str)
+            info_key(str): 'dbNSFP_GERP___RS', 'dbNSFP_phastCons100way_vertebrate' or
+                'phyloP100way_vertebrate'
+            field_key(str): 'gerp', 'phast' or 'phylop'
 
         Returns:
             conservations(list): List of censervation terms
@@ -59,7 +68,7 @@ def parse_conservation_info(variant, info_key):
             raw_score = (raw_score,)
 
         for score in raw_score:
-            if score >= CONSERVATION[info_key]['conserved_min']:
+            if score >= CONSERVATION[field_key]['conserved_min']:
                 conservations.append('Conserved')
             else:
                 conservations.append('NotConserved')
@@ -67,14 +76,33 @@ def parse_conservation_info(variant, info_key):
     return conservations
 
 
-def parse_conservation_csq(variant, info_key):
+def parse_conservation_csq(raw_transcripts, csq_key, field_key):
     """Get the conservation prediction from the CSQ field
 
         Args:
-            variant(dict): A variant dictionary
-            info_key(str)
+            raw_transcripts(generator): raw transcripts from CSQ field
+            csq_key(str): 'GERP++_RS', 'phastCons100way_vertebrate' or 'phyloP100way_vertebrate'
+            field_key(str): 'gerp', 'phast' or 'phylop'
 
         Returns:
             conservations(list): List of censervation terms
     """
-    vep_info = variant.INFO.get('CSQ')
+    conservations = []
+    conservations_anno = set() # do not include duplicated values
+
+    for raw_transcript in raw_transcripts:
+        try:
+            scores = raw_transcript.get(csq_key)
+            for score in scores.split('&'): # fiels may consist of multiple numeric values
+                conservations_anno.add(float(score))
+        except ValueError :
+            LOG.warning('Error while parsing {} value:{} '.format(field_key,raw_transcript.get(csq_key)))
+            continue
+
+    for score in conservations_anno:
+        if score >= CONSERVATION[field_key]['conserved_min']:
+            conservations.append('Conserved')
+        else:
+            conservations.append('NotConserved')
+
+    return conservations
--- tests/parse/test_parse_conservation.py
+++ tests/parse/test_parse_conservation.py
@@ -1,4 +1,4 @@
-from scout.parse.variant.conservation import (parse_conservation, parse_conservations)
+from scout.parse.variant.conservation import (parse_conservation_info, parse_conservations)
 
 def test_parse_conservation(cyvcf2_variant):
     variant = cyvcf2_variant
@@ -6,7 +6,7 @@ def test_parse_conservation(cyvcf2_variant):
     variant.INFO['dbNSFP_GERP___RS'] = 3.7
     ## WHEN parsing conservation
     ## THEN assert that the field is parsed correct
-    assert parse_conservation(variant, 'dbNSFP_GERP___RS') == ['Conserved']
+    assert parse_conservation_info(variant, 'dbNSFP_GERP___RS', 'gerp') == ['Conserved']
 
 def test_parse_conservation_multiple_terms(cyvcf2_variant):
     variant = cyvcf2_variant
@@ -15,7 +15,7 @@ def test_parse_conservation_multiple_terms(cyvcf2_variant):
 
     ## WHEN parsing conservation
     ## THEN assert that all terms are returned
-    assert parse_conservation(variant, 'dbNSFP_GERP___RS') == ['Conserved', 'NotConserved']
+    assert parse_conservation_info(variant, 'dbNSFP_GERP___RS', 'gerp') == ['Conserved', 'NotConserved']
 
 def test_parse_conservations(cyvcf2_variant):
     variant = cyvcf2_variant
@@ -32,3 +32,27 @@ def test_parse_conservations(cyvcf2_variant):
     assert conservations['gerp'] == ['Conserved', 'NotConserved']
     assert conservations['phast'] == ['Conserved']
     assert conservations['phylop'] == ['NotConserved']
+
+
+def test_parse_conservation_csq(cyvcf2_variant):
+
+    ## GIVEN a variant with multiple conservation annotations
+    csq_entry="""0&4.6|0.8|2.4,0&4.6|0.8|2.4"""
+    vep_header="""GERP++_RS|phastCons100way_vertebrate|phyloP100way_vertebrate"""
+
+    cyvcf2_variant.INFO['CSQ'] = csq_entry
+
+    # variant consists of 2 transcrips, each one of them with gerp, phast and phylop values
+    raw_transcripts =  list((dict(zip(vep_header.split('|'), transcript_info.split('|')))
+                       for transcript_info in csq_entry.split(',')))
+
+    assert len(raw_transcripts) == 2
+
+    conservations = parse_conservations(cyvcf2_variant, vep_header.split('|'))
+
+    assert conservations['gerp'] == ['NotConserved','Conserved']
+    assert conservations['phast'] == ['Conserved']
+    assert conservations['phylop'] == ['NotConserved']
+
+
+    #assert len(conservations[0]) == len(vep_header.split('|'))
