661abbd17cbe24ddf73ad0454598c78a5cbaa022
northwestwitch <rasi.chiara@gmail.com>
2020-01-27 15:58:13 +0100

    typo

--- scout/adapter/mongo/case.py
+++ scout/adapter/mongo/case.py
@@ -583,7 +583,7 @@ class CaseHandler(object):
                     # collect user-entered info and save at the individual level in new case_obj
                     if ind.get('age') is None:
                         ind['age'] = old_ind.get('age')
-                    if ind.get('tissue_type') is None
+                    if ind.get('tissue_type') is None:
                         ind['tissue_type'] = old_ind.get('tissue_type')
 
         updated_case = self.case_collection.find_one_and_update(
