3263f6c0ce5983cecf67fce0003b6137c48c1a01
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-01-29 15:21:47 +0100

    Enables usage of static file when updating diseases

--- scout/commands/update/disease.py
+++ scout/commands/update/disease.py
@@ -12,42 +12,48 @@ Copyright (c) 2017 __MoonsoInc__. All rights reserved.
 import logging
 
 import click
-from flask.cli import with_appcontext, current_app
+from flask.cli import current_app, with_appcontext
 
 from scout.load.hpo import load_disease_terms
-from scout.utils.handle import get_file_handle
-
-from scout.utils.scout_requests import fetch_mim_files
 from scout.server.extensions import store
+from scout.utils.handle import get_file_handle
+from scout.utils.scout_requests import fetch_mim_files
 
 LOG = logging.getLogger(__name__)
 
 
 @click.command("diseases", short_help="Update disease terms")
 @click.option("--api-key", help="Specify the api key")
+@click.option("--genemap2", type=click.Path(exists=True), help="Path to genemap2 file")
 @with_appcontext
-def diseases(api_key):
+def diseases(api_key, genemap2):
     """
     Update disease terms in mongo database.
+
+    If no file is specified the file will be fetched from OMIM with the api-key
     """
     adapter = store
 
     # Fetch the omim information
     api_key = api_key or current_app.config.get("OMIM_API_KEY")
-    if not api_key:
-        LOG.warning("Please provide a omim api key to load the omim gene panel")
-        raise click.Abort()
+    if genemap2:
+        genemap_lines = get_file_handle(genemap2)
 
-    try:
-        mim_files = fetch_mim_files(api_key, genemap2=True)
-    except Exception as err:
-        LOG.warning(err)
-        raise click.Abort()
+    else:
+        if not api_key:
+            LOG.warning("Please provide a omim api key to load the omim gene panel")
+            raise click.Abort()
+
+        try:
+            mim_files = fetch_mim_files(api_key, genemap2=True)
+            genemap_lines = mim_files["genemap2"]
+        except Exception as err:
+            LOG.warning(err)
+            raise click.Abort()
 
     LOG.info("Dropping DiseaseTerms")
     adapter.disease_term_collection.drop()
     LOG.debug("DiseaseTerms dropped")
-
-    load_disease_terms(adapter=adapter, genemap_lines=mim_files["genemap2"])
+    load_disease_terms(adapter=adapter, genemap_lines=genemap_lines)
 
     LOG.info("Successfully loaded all disease terms")
