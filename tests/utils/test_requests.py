ab57982325d7e5149b5f8cab5220726a38386bf7
northwestwitch <rasi.chiara@gmail.com>
2020-02-03 15:19:27 +0100

    reintroduce all tests

--- tests/utils/test_requests.py
+++ tests/utils/test_requests.py
@@ -25,3 +25,23 @@ def test_get_request():
     url = "http://www.github.com"
     decoded_resp = get_request(url)
     assert "<!DOCTYPE html>" in decoded_resp
+
+
+def test_fetch_refseq_version():
+    """Test eutils service from entrez that retrieves refseq version"""
+
+    # fetch complete refseq version for accession that has version
+    refseq_acc = "NM_020533"
+    refseq_version = fetch_refseq_version(refseq_acc)
+
+    # entrez eutils might be down the very moment of the test
+    version_n = refseq_version.split(".")[1]
+    # make sure that contains version number
+    assert version_n.isdigit()
+
+    # fetch complete refseq version for accession without version
+    refseq_acc = "NM_000000"
+    refseq_version = fetch_refseq_version(refseq_acc)
+
+    # make sure that contains version number
+    assert refseq_version == refseq_acc
