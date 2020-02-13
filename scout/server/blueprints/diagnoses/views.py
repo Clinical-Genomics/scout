2a7425acb080556d2f1116df2baa44c6d40038db
northwestwitch <rasi.chiara@gmail.com>
2020-02-13 16:26:27 +0100

    renamed a template

--- scout/server/blueprints/diagnoses/views.py
+++ scout/server/blueprints/diagnoses/views.py
@@ -8,7 +8,7 @@ omim_bp = Blueprint("diagnoses", __name__, template_folder="templates")
 
 
 @omim_bp.route("/diagnoses/<omim_id>", methods=["GET"])
-@templated("diagnoses/omim_genes.html")
+@templated("diagnoses/omim_term.html")
 def omim_diagnosis(omim_id):
     """Display genes associated to a specific OMIM diagnosis"""
 
