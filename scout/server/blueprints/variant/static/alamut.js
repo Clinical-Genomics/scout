20e9e54d485d82d93386959d08a1b32ed38c73e1
northwestwitch <rasi.chiara@gmail.com>
2020-02-24 07:50:37 +0100

    work in progress

--- scout/server/blueprints/variant/static/alamut.js
+++ scout/server/blueprints/variant/static/alamut.js
@@ -1,11 +1,13 @@
-function httpGetLoadBam(hgvs, bamFile) {
+// Check here for documentation : https://www.interactive-biosoftware.com/doc/alamut-visual/2.7/programmatic-access.html
+
+function httpGetLoadBam(region, bamFile) {
 	var xmlHttp = null;
 	xmlHttp = new XMLHttpRequest();
 	var bamXmlHttp = null;
 	bamXmlHttp = new XMLHttpRequest();
 
 	if (xmlHttp) {
-		xmlHttp.open( "GET", "http://localhost:10000/show?request="+hgvs+"&synchronous=true", true );
+		xmlHttp.open( "GET", "http://localhost:10000/show?request="+region+"&synchronous=true", true );
 		xmlHttp.send(null);
 		xmlHttp.onreadystatechange=function() {
 			if (xmlHttp.readyState==4) {
