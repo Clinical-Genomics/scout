9aff6c52580ed8e38c02f566894e7fb3c41a3589
northwestwitch <rasi.chiara@gmail.com>
2020-03-02 10:23:01 +0100

    scout download hpo now works

--- scout/commands/download/hpo.py
+++ scout/commands/download/hpo.py
@@ -25,14 +25,14 @@ def print_hpo(out_dir):
 
     hpo_file_name = "genes_to_phenotype.txt"
     hpo_file_path = out_dir / hpo_file_name
-    LOG.info("Download HPO genes to %s", hpo_file_path)
+    LOG.info("Download HPO genes to phenotypes to diagnosis data to %s", hpo_file_path)
     with hpo_file_path.open("w", encoding="utf-8") as outfile:
         for line in fetch_genes_to_hpo_to_disease():
             outfile.write(line + "\n")
 
     hpo_file_name = "phenotype_to_genes.txt"
     hpo_file_path = out_dir / hpo_file_name
-    LOG.info("Download HPO TO genes to %s", hpo_file_path)
+    LOG.info("Download HPO to genes to diagnosis data to %s", hpo_file_path)
     with hpo_file_path.open("w", encoding="utf-8") as outfile:
         for line in fetch_hpo_to_genes_to_disease():
             outfile.write(line + "\n")
