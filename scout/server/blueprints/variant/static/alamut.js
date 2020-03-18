e971c0c89384e87bd9834fc6fbbe7d51951d6ede
northwestwitch <rasi.chiara@gmail.com>
2020-03-18 14:56:06 +0100

    almost working

--- scout/server/blueprints/variant/static/alamut.js
+++ scout/server/blueprints/variant/static/alamut.js
@@ -1,6 +1,10 @@
 // Check here for documentation : https://www.interactive-biosoftware.com/doc/alamut-visual/2.8/programmatic-access.html
 
 function httpGetLoadBam(region, bamFile) {
+
+	alert(region);
+	alert(bamFile);
+	/*
 	var xmlHttp = null;
 	xmlHttp = new XMLHttpRequest();
 	var bamXmlHttp = null;
@@ -16,4 +20,5 @@ function httpGetLoadBam(region, bamFile) {
 			}
 		}
 	}
+	*/
 }
