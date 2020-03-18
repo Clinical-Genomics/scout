88de6e39ed4c08ed6a9bb3a415fc3486c2308d2d
Nina <norgrennina85@gmail.com>
2020-03-18 13:03:05 +0100

    cleared files

--- docker/scout-import/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scout --port $MONGO_PORT --host $MONGO_HOST load case $YAML
--- docker/scout-remove/docker-entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-scout --port $MONGO_PORT --host $MONGO_HOST delete case -c $SAMPLEID -i $INSTID
