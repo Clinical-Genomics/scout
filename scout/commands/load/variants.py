cffd0d20fef33019d68e8576ad68b44b2625ffe5
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-03-23 17:31:10 +0100

    Black all

--- scout/commands/load/variants.py
+++ scout/commands/load/variants.py
@@ -151,7 +151,7 @@ def variants(
                 start=start,
                 end=end,
                 gene_obj=gene_obj,
-                build=case_obj["genome_build"]
+                build=case_obj["genome_build"],
             )
 
         except Exception as e:
