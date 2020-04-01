0e2f4c56e33fa16c616ff135b9deff835959af75
Nina <norgrennina85@gmail.com>
2020-04-01 15:26:13 +0200

    added genome build to function in blueprints/variant/utils.py

--- scout/server/blueprints/variant/utils.py
+++ scout/server/blueprints/variant/utils.py
@@ -181,7 +181,7 @@ def add_gene_info(store, variant_obj, gene_panels=None, genome_build=None):
         panel_info = add_panel_specific_gene_info(extra_info.get(hgnc_id, []))
         variant_gene.update(panel_info)
 
-        update_transcripts_information(variant_gene, hgnc_gene, variant_obj)
+        update_transcripts_information(variant_gene, hgnc_gene, variant_obj, genome_build)
 
         variant_gene["common"] = hgnc_gene
 
