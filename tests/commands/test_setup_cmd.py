7d41086178e4df515ac8943da611a733e77fd6b7
northwestwitch <rasi.chiara@gmail.com>
2020-05-23 20:24:53 +0200

    removed one extra param

--- tests/commands/test_setup_cmd.py
+++ tests/commands/test_setup_cmd.py
@@ -10,7 +10,9 @@ def test_setup_database_invalid_omim_key(mock_app):
     assert runner
 
     # test the CLI command with non-valid API key
-    result = runner.invoke(cli, ["setup", "database", "--api-key", "not_a_valid_key", "--yes"])
+    result = runner.invoke(
+        cli, ["setup", "database", "--api-key", "not_a_valid_key", "--yes"]
+    )
     # Make sure that setup enters in setup function correctly but stops because
     # there is no valid OMIM API KEY
     assert result.exit_code != 0
@@ -23,7 +25,8 @@ def test_setup_database(mock_app):
     assert runner
 
     # test the CLI command for seting up scout
-    result = runner.invoke(cli, ["setup", "demo"], input="y")
+    result = runner.invoke(cli, ["setup", "demo"])
+
     # Make sure that setup function works correctly
     assert result.exit_code == 0
     assert "Scout instance setup successful" in result.output
