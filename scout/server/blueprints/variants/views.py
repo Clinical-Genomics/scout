c6b66d0c30002e9619e33ee5855b313c924daac9
northwestwitch <rasi.chiara@gmail.com>
2020-05-29 09:09:21 +0200

    typo

--- scout/server/blueprints/variants/views.py
+++ scout/server/blueprints/variants/views.py
@@ -257,7 +257,7 @@ def cancer_variants(institute_id, case_name):
             # Flash a message with errors
             for field, err_list in form.errors.items():
                 for err in err_list:
-                    flash(f"Conteint of field '{field}' has not a valid format", "warning")
+                    flash(f"Content of field '{field}' has not a valid format", "warning")
             # And do not submit the form
             return redirect(
                 url_for(
