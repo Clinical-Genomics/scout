915c75c58ba722fc86a1b8f2d5bd62737735ee67
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2020-06-04 13:20:47 +0200

    ahem..

--- scout/server/blueprints/variants/controllers.py
+++ scout/server/blueprints/variants/controllers.py
@@ -74,12 +74,10 @@ def variants(store, institute_obj, case_obj, variants_query, page=1, per_page=50
             if evaluation_obj["case_id"] == case_obj["_id"]:
                 continue
 
-            evaluation_obj["classification"] = ACMG_COMPLETE_MAP.get(classification)
+            classification = evaluation_obj["classification"]
 
-            if is_research or evaluation_obj["classification"] not in [
-                "pathogenic",
-                "likely_pathogenic",
-            ]:
+            if is_research or classification not in ["pathogenic", "likely_pathogenic"]:
+                evaluation_obj["classification"] = ACMG_COMPLETE_MAP.get(classification)
                 evaluations.append(evaluation_obj)
 
         variant_obj["evaluations"] = evaluations
@@ -98,7 +96,12 @@ def variants(store, institute_obj, case_obj, variants_query, page=1, per_page=50
 
         variants.append(
             parse_variant(
-                store, institute_obj, case_obj, variant_obj, update=True, genome_build=genome_build,
+                store,
+                institute_obj,
+                case_obj,
+                variant_obj,
+                update=True,
+                genome_build=genome_build,
             )
         )
 
@@ -126,10 +129,14 @@ def sv_variants(store, institute_obj, case_obj, variants_query, page=1, per_page
                 variant_type="clinical",
             )
         if clinical_var_obj is not None:
-            variant_obj["clinical_assessments"] = get_manual_assessments(clinical_var_obj)
+            variant_obj["clinical_assessments"] = get_manual_assessments(
+                clinical_var_obj
+            )
 
         variants.append(
-            parse_variant(store, institute_obj, case_obj, variant_obj, genome_build=genome_build)
+            parse_variant(
+                store, institute_obj, case_obj, variant_obj, genome_build=genome_build
+            )
         )
 
     return {"variants": variants, "more_variants": more_variants}
@@ -167,7 +174,9 @@ def get_manual_assessments(variant_obj):
                     MANUAL_RANK_OPTIONS[manual_rank]["description"]
                 )
                 assessment["label"] = MANUAL_RANK_OPTIONS[manual_rank]["label"]
-                assessment["display_class"] = MANUAL_RANK_OPTIONS[manual_rank]["label_class"]
+                assessment["display_class"] = MANUAL_RANK_OPTIONS[manual_rank][
+                    "label_class"
+                ]
 
             if assessment_type == "cancer_tier":
                 cancer_tier = variant_obj[assessment_type]
@@ -175,7 +184,9 @@ def get_manual_assessments(variant_obj):
                     CANCER_TIER_OPTIONS[cancer_tier]["description"]
                 )
                 assessment["label"] = CANCER_TIER_OPTIONS[cancer_tier]["label"]
-                assessment["display_class"] = CANCER_TIER_OPTIONS[cancer_tier]["label_class"]
+                assessment["display_class"] = CANCER_TIER_OPTIONS[cancer_tier][
+                    "label_class"
+                ]
 
             if assessment_type == "acmg_classification":
                 classification = variant_obj[assessment_type]
@@ -193,7 +204,9 @@ def get_manual_assessments(variant_obj):
                 for reason in variant_obj[assessment_type]:
                     if not isinstance(reason, int):
                         reason = int(reason)
-                        assessment["title"] += "<strong>{}</strong> - {}<br><br>".format(
+                        assessment[
+                            "title"
+                        ] += "<strong>{}</strong> - {}<br><br>".format(
                             DISMISS_VARIANT_OPTIONS[reason]["label"],
                             DISMISS_VARIANT_OPTIONS[reason]["description"],
                         )
@@ -283,7 +296,10 @@ def parse_variant(
         variant_obj = store.update_variant(variant_obj)
 
     variant_obj["comments"] = store.events(
-        institute_obj, case=case_obj, variant_id=variant_obj["variant_id"], comments=True,
+        institute_obj,
+        case=case_obj,
+        variant_id=variant_obj["variant_id"],
+        comments=True,
     )
 
     if variant_genes:
@@ -301,7 +317,9 @@ def parse_variant(
 
     # convert length for SV variants
     variant_length = variant_obj.get("length")
-    variant_obj["length"] = {100000000000: "inf", -1: "n.d."}.get(variant_length, variant_length)
+    variant_obj["length"] = {100000000000: "inf", -1: "n.d."}.get(
+        variant_length, variant_length
+    )
     if not "end_chrom" in variant_obj:
         variant_obj["end_chrom"] = variant_obj["chromosome"]
     variant_obj["cosmic_link"] = cosmic_link(variant_obj)
@@ -337,7 +355,9 @@ def download_variants(store, case_obj, variant_objs):
     )
     # return a csv with the exported variants
     return Response(
-        generate(",".join(document_header), export_lines), mimetype="text/csv", headers=headers,
+        generate(",".join(document_header), export_lines),
+        mimetype="text/csv",
+        headers=headers,
     )
 
 
@@ -387,7 +407,9 @@ def variant_export_lines(store, case_obj, variants_query):
                         transcript_obj.get("is_canonical")
                         and transcript_obj.get("is_canonical") is True
                     ):
-                        hgvs_nucleotide = str(transcript_obj.get("coding_sequence_name"))
+                        hgvs_nucleotide = str(
+                            transcript_obj.get("coding_sequence_name")
+                        )
                 hgvs_c.append(hgvs_nucleotide)
 
             variant_line.append(";".join(str(x) for x in gene_ids))
@@ -399,7 +421,9 @@ def variant_export_lines(store, case_obj, variants_query):
                 variant_line.append("-")  # instead of gene ids
                 i = i + 1
 
-        variant_gts = variant["samples"]  # list of coverage and gt calls for case samples
+        variant_gts = variant[
+            "samples"
+        ]  # list of coverage and gt calls for case samples
         for individual in case_obj["individuals"]:
             for variant_gt in variant_gts:
                 if individual["individual_id"] == variant_gt["sample_id"]:
@@ -428,9 +452,15 @@ def variants_export_header(case_obj):
     # Add fields specific for case samples
     for individual in case_obj["individuals"]:
         display_name = str(individual["display_name"])
-        header.append("AD_reference_" + display_name)  # Add AD reference field for a sample
-        header.append("AD_alternate_" + display_name)  # Add AD alternate field for a sample
-        header.append("GT_quality_" + display_name)  # Add Genotype quality field for a sample
+        header.append(
+            "AD_reference_" + display_name
+        )  # Add AD reference field for a sample
+        header.append(
+            "AD_alternate_" + display_name
+        )  # Add AD alternate field for a sample
+        header.append(
+            "GT_quality_" + display_name
+        )  # Add Genotype quality field for a sample
     return header
 
 
@@ -498,7 +528,8 @@ def get_clinvar_submission(store, institute_id, case_name, variant_id, submissio
 
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     pinned = [
-        store.variant(variant_id) or variant_id for variant_id in case_obj.get("suspects", [])
+        store.variant(variant_id) or variant_id
+        for variant_id in case_obj.get("suspects", [])
     ]
     variant_obj = store.variant(variant_id)
     clinvar_submission_objs = store.clinvars(submission_id=submission_id)
@@ -516,7 +547,9 @@ def upload_panel(store, institute_id, case_name, stream):
     """Parse out HGNC symbols from a stream."""
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     raw_symbols = [
-        line.strip().split("\t")[0] for line in stream if line and not line.startswith("#")
+        line.strip().split("\t")[0]
+        for line in stream
+        if line and not line.startswith("#")
     ]
     # check if supplied gene symbols exist
     hgnc_symbols = []
@@ -528,7 +561,9 @@ def upload_panel(store, institute_id, case_name, stream):
     return hgnc_symbols
 
 
-def populate_filters_form(store, institute_obj, case_obj, user_obj, category, request_form):
+def populate_filters_form(
+    store, institute_obj, case_obj, user_obj, category, request_form
+):
     # Update filter settings if Clinical Filter was requested
     clinical_filter_panels = []
 
@@ -640,9 +675,13 @@ def populate_sv_filters_form(store, institute_obj, case_obj, category, request_o
     ]
 
     # populate available panel choices
-    available_panels = case_obj.get("panels", []) + [{"panel_name": "hpo", "display_name": "HPO"}]
+    available_panels = case_obj.get("panels", []) + [
+        {"panel_name": "hpo", "display_name": "HPO"}
+    ]
 
-    panel_choices = [(panel["panel_name"], panel["display_name"]) for panel in available_panels]
+    panel_choices = [
+        (panel["panel_name"], panel["display_name"]) for panel in available_panels
+    ]
 
     form.gene_panels.choices = panel_choices
 
@@ -671,10 +710,14 @@ def populate_sv_filters_form(store, institute_obj, case_obj, category, request_o
     if not_found_ids:
         flash("HGNC id not found: {}".format(", ".join(not_found_ids)), "warning")
     if not_found_symbols:
-        flash("HGNC symbol not found: {}".format(", ".join(not_found_symbols)), "warning")
+        flash(
+            "HGNC symbol not found: {}".format(", ".join(not_found_symbols)), "warning"
+        )
     if non_clinical_symbols:
         flash(
-            "Gene not included in clinical list: {}".format(", ".join(non_clinical_symbols)),
+            "Gene not included in clinical list: {}".format(
+                ", ".join(non_clinical_symbols)
+            ),
             "warning",
         )
     form.hgnc_symbols.data = hgnc_symbols
@@ -708,7 +751,9 @@ def verified_excel_file(store, institute_list, temp_excel_dir):
 
     for cust in institute_list:
         verif_vars = store.verified(institute_id=cust)
-        LOG.info("Found {} verified variants for customer {}".format(len(verif_vars), cust))
+        LOG.info(
+            "Found {} verified variants for customer {}".format(len(verif_vars), cust)
+        )
 
         if not verif_vars:
             continue
@@ -728,7 +773,9 @@ def verified_excel_file(store, institute_list, temp_excel_dir):
             Report_Sheet.write(row, col, field)
 
         # Write variant lines, after header (start at line 1)
-        for row, line in enumerate(cust_verified, 1):  # each line becomes a row in the document
+        for row, line in enumerate(
+            cust_verified, 1
+        ):  # each line becomes a row in the document
             for col, field in enumerate(line):  # each field in line becomes a cell
                 Report_Sheet.write(row, col, field)
         workbook.close()
@@ -755,6 +802,8 @@ def activate_case(store, institute_obj, case_obj, current_user):
 
         user_obj = store.user(current_user.email)
         case_link = url_for(
-            "cases.case", institute_id=institute_obj["_id"], case_name=case_obj["display_name"],
+            "cases.case",
+            institute_id=institute_obj["_id"],
+            case_name=case_obj["display_name"],
         )
         store.update_status(institute_obj, case_obj, user_obj, "active", case_link)
