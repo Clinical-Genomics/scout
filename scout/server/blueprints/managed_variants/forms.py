677defdfb42f80d0c33ffccb44c1237d3c2bc94e
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2020-06-16 16:21:32 +0200

    Register blueprint.

---
+++ scout/server/blueprints/managed_variants/forms.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from flask_wtf import FlaskForm
+from wtforms import (
+    IntegerField,
+    SelectField,
+    SelectMultipleField,
+    SubmitField,
+    TextField,
+    validators,
+)
+
+from scout.constants import (
+    SV_TYPES,
+    CHROMOSOMES,
+)
+
+LOG = logging.getLogger(__name__)
+CHROMOSOME_OPTIONS = [("", "All")] + [(chrom, chrom) for chrom in CHROMOSOMES]
+SV_TYPE_CHOICES = [(term, term.replace("_", " ").upper()) for term in SV_TYPES]
+CATEGORY_CHOICES = [
+    (term, term.replace("_", " ").upper()) for term in ["snv", "str", "sv", "cancer", "cancer_sv"]
+]
+
+
+class ManagedVariantsFilterForm(FlaskForm):
+    chrom = SelectField("Chromosome", [validators.Optional()], choices=[])
+    start = IntegerField("Start position", [validators.Optional()])
+    end = IntegerField("End position", [validators.Optional()])
+    cytoband_start = SelectField("Cytoband start", choices=[])
+    cytoband_end = SelectField("Cytoband end", choices=[])
+
+    category = SelectMultipleField("Category", choices=CATEGORY_CHOICES)
+    svtype = SelectMultipleField("SVType", choices=SV_TYPE_CHOICES)
+
+    description = TextField(label="Description")
+    filter_variants = SubmitField(label="Filter variants")
+    export = SubmitField(label="Filter and export")
