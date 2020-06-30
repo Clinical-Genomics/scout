9e47d47313bb1755111250417b24a28000520cb8
northwestwitch <rasi.chiara@gmail.com>
2020-06-30 10:21:48 +0200

    remove debug message

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -73,8 +73,6 @@ def cases(institute_id):
     institute_obj = institute_and_case(store, institute_id)
     name_query = request.args.get("search_term")
 
-    flash(type(request.args))
-
     limit = 100
     if request.args.get("search_limit"):
         limit = int(request.args.get("search_limit"))
