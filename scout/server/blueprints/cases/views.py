825c1c2e25d1c10dce67a779a0b30c6f04fc3304
Lint Action <lint-action@samuelmeuli.com>
2020-07-07 11:02:31 +0000

    Fix code style issues with Black

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -936,7 +936,7 @@ def vcf2cytosure(institute_id, case_name, individual_id):
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     outdir = os.path.abspath(os.path.dirname(vcf2cytosure))
     filename = os.path.basename(vcf2cytosure)
-    attachment_filename = display_name+"."+case_obj['_id']+".vcf2cytosure.cgh"
+    attachment_filename = display_name + "." + case_obj["_id"] + ".vcf2cytosure.cgh"
     LOG.debug("Attempt to deliver file {0} from dir {1}".format(attachment_filename, outdir))
     return send_from_directory(
         outdir, filename, attachment_filename=attachment_filename, as_attachment=True
