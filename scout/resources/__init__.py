ac413c47cddd3b9bc759dc5abe8e5cd2541a617f
Chiara Rasi <rasi.chiara@gmail.com>
2020-07-10 11:29:16 +0200

    Make variants cytoband genome-build aware (#1936)
    * variants are created with the cytoband according to genome build
    
    * Fix code style issues with Black
    
    * fix a comment
    
    * improve capture right genome build
    
    * updated changelog
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- scout/resources/__init__.py
+++ scout/resources/__init__.py
@@ -4,16 +4,12 @@ import pkg_resources
 ###### Files ######
 
 # Cytoband
-
-cytobands_file = "resources/cytoBand.txt.gz"
 cytoband_hg19_file = "resources/cytoBand_hg19.txt.gz"
 cytoband_hg38_file = "resources/cytoBand_hg38.txt.gz"
 
 ###### Paths ######
 
 # Cytoband path
-
-cytobands_path = pkg_resources.resource_filename("scout", cytobands_file)
 cytobands_37_path = pkg_resources.resource_filename("scout", cytoband_hg19_file)
 cytobands_38_path = pkg_resources.resource_filename("scout", cytoband_hg38_file)
 
