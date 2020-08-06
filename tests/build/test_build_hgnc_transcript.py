1a74224665b18c7329ba0d701944887db84392cb
mikaell <mikael.laaksonen@scilifelab.se>
2020-08-06 13:32:55 +0200

    Fix Formatting and Add Comments

--- tests/build/test_build_hgnc_transcript.py
+++ tests/build/test_build_hgnc_transcript.py
@@ -1,10 +1,23 @@
 from pprint import pprint as pp
 import logging
+
 LOG = logging.getLogger(__name__)
 import pytest
 from scout.build.genes.transcript import build_transcript
 
-{'chrom': '1', 'transcript_start': 1167629, 'transcript_end': 1170421, 'mrna': {'NM_080605'}, 'mrna_predicted': set(), 'nc_rna': set(), 'ensembl_gene_id': 'ENSG00000176022', 'ensembl_transcript_id': 'ENST00000379198', 'hgnc_id': 17978, 'primary_transcripts': {'NM_080605'}}
+{
+    "chrom": "1",
+    "transcript_start": 1167629,
+    "transcript_end": 1170421,
+    "mrna": {"NM_080605"},
+    "mrna_predicted": set(),
+    "nc_rna": set(),
+    "ensembl_gene_id": "ENSG00000176022",
+    "ensembl_transcript_id": "ENST00000379198",
+    "hgnc_id": 17978,
+    "primary_transcripts": {"NM_080605"},
+}
+
 
 def test_build_hgnc_transcripts(parsed_transcripts):
     # GIVEN iterable with parsed transcripts
@@ -19,49 +32,50 @@ def test_build_hgnc_transcripts(parsed_transcripts):
         assert tx_obj["hgnc_id"]
 
 
-
-
-
 def test_build_hgnc_transcripts_Exceptions(mocker, test_transcript):
-    # GIVEN a 
-    test_transcript['hgnc_id'] = None
-    # THEN calling build_transcript() will raise KeyError
+    # GIVEN a parsed transcript
+
+    # WHEN setting 'hgnc_id' to None
+    test_transcript["hgnc_id"] = None
+    # THEN calling build_transcript() will raise TypeError
     with pytest.raises(TypeError):
         build_transcript(test_transcript)
-
-    del test_transcript['hgnc_id'] 
+    # WHEN deleting 'hgnc_id'
+    del test_transcript["hgnc_id"]
+    # THEN calling build_transcript() will raise KeyError
     with pytest.raises(KeyError):
         build_transcript(test_transcript)
-        
 
-    test_transcript['transcript_end'] = None
-    # THEN calling build_transcript() will raise KeyError
+    # WHEN setting 'transcript_end' to None
+    test_transcript["transcript_end"] = None
+    # THEN calling build_transcript() will raise TypeError
     with pytest.raises(TypeError):
         build_transcript(test_transcript)
-
-    del test_transcript['transcript_end'] 
+    # WHEN deleting 'transcript_end'
+    del test_transcript["transcript_end"]
+    # THEN calling build_transcript() will raise KeyError
     with pytest.raises(KeyError):
         build_transcript(test_transcript)
 
-
-    test_transcript['transcript_start'] = None
-    # THEN calling build_transcript() will raise KeyError
+    # WHEN setting 'transcript_start' to None
+    test_transcript["transcript_start"] = None
+    # THEN calling build_transcript() will raise TypeError
     with pytest.raises(TypeError):
         build_transcript(test_transcript)
-
-    del test_transcript['transcript_start'] 
+    # WHEN deleting 'transcript_start'
+    del test_transcript["transcript_start"]
+    # THEN calling build_transcript() will raise KeyError
     with pytest.raises(KeyError):
         build_transcript(test_transcript)
 
-
-    del test_transcript['chrom'] 
+    # WHEN deleting 'chrom'
+    del test_transcript["chrom"]
+    # THEN calling build_transcript() will raise KeyError
     with pytest.raises(KeyError):
         build_transcript(test_transcript)
 
-
-    del test_transcript['ensembl_transcript_id'] 
+    # WHEN deleting 'ensembl_transcript_id'
+    del test_transcript["ensembl_transcript_id"]
+    # THEN calling build_transcript() will raise KeyError
     with pytest.raises(KeyError):
         build_transcript(test_transcript)
-
-
-        
