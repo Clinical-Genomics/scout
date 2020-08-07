4f6309ec67d2b9db3947c2f5b58d6a37c120b51f
Lint Action <lint-action@samuelmeuli.com>
2020-08-07 07:52:00 +0000

    Fix code style issues with Black

--- tests/export/test_export_exon.py
+++ tests/export/test_export_exon.py
@@ -3,11 +3,10 @@ import pytest
 from scout.export.exon import export_exons
 
 import logging
+
 LOG = logging.getLogger(__name__)
+
 
 def test_export_exons(adapter, build=37):
     result = export_exons(adapter)
     assert isinstance(result, types.GeneratorType)
-
-  
-    
