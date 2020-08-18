f58fe9c244b012cb386953b937b6bee866705fe4
elevu <eleonora.maccacaro@gmail.com>
2020-08-18 18:11:11 +0200

    Fix small animation

--- scout/server/static/scripts/HelloWorld.js
+++ scout/server/static/scripts/HelloWorld.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './style/HelloWorld.style.css';
 import {motion} from 'framer-motion';
-import pill from "./assets/test-image.png";
 
 class HelloWorld extends React.Component {
 	render() {
 		return (
 			<div className="loadingAnimation">
-				hi
-				<img src={pill}/>
+				Hi! I am a tiny React app
 				<motion.div
 					animate={{
 						rotate: 360,
