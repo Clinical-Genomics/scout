ff19c23b5fae89b54dc0a4e66b18a061882bd586
elevu <eleonora.maccacaro@gmail.com>
2020-08-24 10:48:56 +0200

    Remove trailing whitespaces from panel name

--- scout/adapter/mongo/panel.py
+++ scout/adapter/mongo/panel.py
@@ -161,7 +161,7 @@ class PanelHandler:
             Args:
                 panel_obj(dict)
         """
-        panel_name = panel_obj["panel_name"]
+        panel_name = panel_obj["panel_name"].strip()
         panel_version = panel_obj["version"]
         display_name = panel_obj.get("display_name", panel_name)
 
