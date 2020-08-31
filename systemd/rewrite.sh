ec0cdf15842865234895d29d3b863148331466c1
Erik Sjölund <erik.sjolund@scilifelab.se>
2020-08-31 21:51:35 +0200

    Try out an alternative
    Signed-off-by: Erik Sjölund <erik.sjolund@scilifelab.se>

--- systemd/rewrite.sh
+++ systemd/rewrite.sh
@@ -4,6 +4,7 @@ set -e
 cd systemd
 mkdir -p rewritten
 for i in scout*service; do
-    cat $i | sed 's/podman run /podman run --user core:core /g' > "rewritten/${i}"
+ #   cat $i | sed 's/podman run /podman run --user core:core /g' > "rewritten/${i}"
+   cat $i  > "rewritten/${i}"
 done
-cat scout-create-datadir.service | sed 's/# Activate-on-FedoraCoreOS //g' > rewritten/scout-create-datadir.service
+#cat scout-create-datadir.service | sed 's/# Activate-on-FedoraCoreOS //g' > rewritten/scout-create-datadir.service
