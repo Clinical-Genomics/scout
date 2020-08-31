2be009bedb892827bf18766e3e954445d869778b
Erik Sjölund <erik.sjolund@scilifelab.se>
2020-08-31 21:31:45 +0200

    Fix bug in rewrite.sh
    Signed-off-by: Erik Sjölund <erik.sjolund@scilifelab.se>

--- systemd/rewrite.sh
+++ systemd/rewrite.sh
@@ -6,4 +6,4 @@ mkdir -p rewritten
 for i in scout*service; do
     cat $i | sed 's/podman run /podman run --user core:core /g' > "rewritten/${i}"
 done
-sed -i 's/# Activate-on-FedoraCoreOS //g' scout-create-datadir.service
+cat scout-create-datadir.service | sed 's/# Activate-on-FedoraCoreOS //g' > rewritten/scout-create-datadir.service
