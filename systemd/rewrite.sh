175288cd23c36a4ae4d076b97a692416a6c52705
Erik Sjölund <erik.sjolund@scilifelab.se>
2020-08-31 21:22:09 +0200

    Add chown
    Signed-off-by: Erik Sjölund <erik.sjolund@scilifelab.se>

--- systemd/rewrite.sh
+++ systemd/rewrite.sh
@@ -4,5 +4,6 @@ set -e
 cd systemd
 mkdir -p rewritten
 for i in scout*service; do
-    cat $i | sed 's/\[Service\]/[Service]\nUser=core\nGroup=core/g' > "rewritten/${i}"
+    cat $i | sed 's/podman run /podman run --user core:core /g' > "rewritten/${i}"
 done
+sed -i 's/# Activate-on-FedoraCoreOS //g' scout-create-datadir.service
