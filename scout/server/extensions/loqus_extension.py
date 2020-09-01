e2c3656e38c9b1398fe86934062cb53e5f24030c
mikaell <mikael.laaksonen@scilifelab.se>
2020-09-01 14:20:32 +0200

    Remove Debug Print

--- scout/server/extensions/loqus_extension.py
+++ scout/server/extensions/loqus_extension.py
@@ -29,7 +29,6 @@ def execute_command(cmd):
         line (str): line of output from command
     """
     output = ""
-    LOG.error("cmd: {}".format(cmd))
     cmd = [x for x in cmd if x != []]  # remove empty lists
     cmd_string = " ".join(cmd)  # add spacing to create a command string
     LOG.info("Running: %s", cmd_string)
@@ -139,7 +138,7 @@ class LoqusDB:
                     "sv",
                     "-c",
                     variant_info["chrom"],
-                    "-s",
+p                    "-s",
                     str(variant_info["pos"]),
                     "-e",
                     str(variant_info["end"]),
