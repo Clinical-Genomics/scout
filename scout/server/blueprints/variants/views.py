3753c6584d9546ca848ce8f475c94d9c19ad2f4a
Lint Action <lint-action@samuelmeuli.com>
2020-09-10 12:03:03 +0000

    Fix code style issues with Black

--- scout/server/blueprints/variants/views.py
+++ scout/server/blueprints/variants/views.py
@@ -384,7 +384,7 @@ def cancer_sv_variants(institute_id, case_name):
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     if request.form.get("hpo_clinical_filter"):
         case_obj["hpo_clinical_filter"] = True
-        
+
     # update status of case if visited for the first time
     controllers.activate_case(store, institute_obj, case_obj, current_user)
 
