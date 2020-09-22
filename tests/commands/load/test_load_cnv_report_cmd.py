bdaf7b8f30b6a3a493cc5246dd908bdcdff69ab8
elevu <eleonora.maccacaro@gmail.com>
2020-09-22 14:49:33 +0200

    Increase test coverage

--- tests/commands/load/test_load_cnv_report_cmd.py
+++ tests/commands/load/test_load_cnv_report_cmd.py
@@ -15,10 +15,20 @@ def test_load_cnv_report(mock_app, case_obj):
     assert runner
 
     # Test CLI function
-    result = runner.invoke(
-        cli,
-        ["load", "cnv-report", case_obj["_id"], cnv_report_path, "-u"],
-    )
+    result = runner.invoke(cli, ["load", "cnv-report", case_obj["_id"], cnv_report_path, "-u"],)
 
     assert "saved report to case!" in result.output
     assert result.exit_code == 0
+
+
+def test_invalid_path_load_cnv_report(mock_app, case_obj):
+    """Testing the load delivery report cli command"""
+
+    runner = mock_app.test_cli_runner()
+    assert runner
+
+    # Test CLI function
+    result = runner.invoke(cli, ["load", "cnv-report", case_obj["_id"], "invalid-path", "-u"],)
+
+    assert 'Path "invalid-path" does not exist.' in result.output
+    assert result.exit_code == 2
