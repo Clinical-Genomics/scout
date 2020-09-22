e311bf377d198d437fcc1f1c1496ce0b349f1689
Lint Action <lint-action@samuelmeuli.com>
2020-09-22 06:04:25 +0000

    Fix code style issues with Black

--- docker/panel-handler.py
+++ docker/panel-handler.py
@@ -4,14 +4,28 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser = argparse.ArgumentParser(description = "Helper script to add or remove cases from database")
-parser.add_argument("--id",   metavar = "ID",  type = str,   required = False,    help = "Panel ID for panel")
-parser.add_argument("--instid",     metavar = "INSTID",    type = str,   required = False,    help = "Institute ID for sample to remove")
-parser.add_argument("--folder",     metavar = "FOLDER",    type = str,   required = False,    help = "Folder where the sample that should be imported is stored")
-parser.add_argument("--panel",       metavar = "PANEL",      type = str,   required = False,    help = "panel file")
+parser = argparse.ArgumentParser(description="Helper script to add or remove cases from database")
+parser.add_argument("--id", metavar="ID", type=str, required=False, help="Panel ID for panel")
+parser.add_argument(
+    "--instid", metavar="INSTID", type=str, required=False, help="Institute ID for sample to remove"
+)
+parser.add_argument(
+    "--folder",
+    metavar="FOLDER",
+    type=str,
+    required=False,
+    help="Folder where the sample that should be imported is stored",
+)
+parser.add_argument("--panel", metavar="PANEL", type=str, required=False, help="panel file")
 args = parser.parse_args()
 
-print("docker-compose run --rm -v {:}:/panels -e \"ID={:}\" -e \"INSTID={:}\" -e \"PANEL={:}\" scout-addpanel".format(os.path.abspath(args.folder), args.panel, args.instid, args.panel))
-os.system("docker-compose run --rm -e \"ID={:}\" -e \"INSTID={:}\" -e \"PANEL={:}\" scout-addpanel".format(args.id, args.instid, args.panel))
-
-
+print(
+    'docker-compose run --rm -v {:}:/panels -e "ID={:}" -e "INSTID={:}" -e "PANEL={:}" scout-addpanel'.format(
+        os.path.abspath(args.folder), args.panel, args.instid, args.panel
+    )
+)
+os.system(
+    'docker-compose run --rm -e "ID={:}" -e "INSTID={:}" -e "PANEL={:}" scout-addpanel'.format(
+        args.id, args.instid, args.panel
+    )
+)
--- docker/scout-run.py
+++ docker/scout-run.py
@@ -4,28 +4,30 @@ import sys
 import os
 
 args = sys.argv.copy()
-cmd  = " ".join(args[1:])
+cmd = " ".join(args[1:])
 
 # mounts sample folder into container if command is load case
-if cmd.startswith('load case'):
+if cmd.startswith("load case"):
     folder = os.path.abspath(os.path.dirname(sys.argv[-1]))
-    yaml   = os.path.basename(sys.argv[-1])
-    args[-1] = '/sample/'+yaml
+    yaml = os.path.basename(sys.argv[-1])
+    args[-1] = "/sample/" + yaml
 else:
     folder = ""
 
 cmd = " ".join(args[1:])
-os.system("docker-compose run --rm {:} -e \"CMD={:}\" scout-run".format('-v '+folder+':/sample', cmd))
-#os.system("docker-compose run --rm -v {:}:/sample -e \"CMD={:}\" scout-run".format(folder, cmd))
-os.system("docker-compose run --rm -e \"CMD={:}\" scout-run".format(cmd))
+os.system(
+    'docker-compose run --rm {:} -e "CMD={:}" scout-run'.format("-v " + folder + ":/sample", cmd)
+)
+# os.system("docker-compose run --rm -v {:}:/sample -e \"CMD={:}\" scout-run".format(folder, cmd))
+os.system('docker-compose run --rm -e "CMD={:}" scout-run'.format(cmd))
 
 # add sample
-#os.system("docker-compose run --rm -v {:}:/sample -e \"YAML={:}\" scout-import".format(os.path.abspath(args.folder), args.yaml))
+# os.system("docker-compose run --rm -v {:}:/sample -e \"YAML={:}\" scout-import".format(os.path.abspath(args.folder), args.yaml))
 # remove sample
-#os.system("docker-compose run --rm -e \"SAMPLEID={:}\" -e \"INSTID={:}\" scout-remove".format(args.sampleid, args.instid))
+# os.system("docker-compose run --rm -e \"SAMPLEID={:}\" -e \"INSTID={:}\" scout-remove".format(args.sampleid, args.instid))
 
 # add user
-#os.system("docker-compose run --rm -e \"INSTID={:}\" -e \"NAME={:}\" -e \"ADID={:}\" -e \"USERMAIL={:}\" -e \"ADMIN={:}\" scout-adduser".format(args.instid, args.name, args.adid, args.usermail, admin))
+# os.system("docker-compose run --rm -e \"INSTID={:}\" -e \"NAME={:}\" -e \"ADID={:}\" -e \"USERMAIL={:}\" -e \"ADMIN={:}\" scout-adduser".format(args.instid, args.name, args.adid, args.usermail, admin))
 
 # remove user
-#os.system("docker-compose run --rm -e \"USERMAIL={:}\" scout-removeuser".format(args.usermail))
+# os.system("docker-compose run --rm -e \"USERMAIL={:}\" scout-removeuser".format(args.usermail))
