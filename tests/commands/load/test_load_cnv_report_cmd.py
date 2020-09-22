e1d61d945300dde9cb5ac07228b7892b224a984c
Lint Action <lint-action@samuelmeuli.com>
2020-09-22 12:50:37 +0000

    Fix code style issues with Black

--- tests/commands/load/test_load_cnv_report_cmd.py
+++ tests/commands/load/test_load_cnv_report_cmd.py
@@ -15,7 +15,10 @@ def test_load_cnv_report(mock_app, case_obj):
     assert runner
 
     # Test CLI function
-    result = runner.invoke(cli, ["load", "cnv-report", case_obj["_id"], cnv_report_path, "-u"],)
+    result = runner.invoke(
+        cli,
+        ["load", "cnv-report", case_obj["_id"], cnv_report_path, "-u"],
+    )
 
     assert "saved report to case!" in result.output
     assert result.exit_code == 0
@@ -28,7 +31,10 @@ def test_invalid_path_load_cnv_report(mock_app, case_obj):
     assert runner
 
     # Test CLI function
-    result = runner.invoke(cli, ["load", "cnv-report", case_obj["_id"], "invalid-path", "-u"],)
+    result = runner.invoke(
+        cli,
+        ["load", "cnv-report", case_obj["_id"], "invalid-path", "-u"],
+    )
 
     assert 'Path "invalid-path" does not exist.' in result.output
     assert result.exit_code == 2
