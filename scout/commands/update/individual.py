dc2abd86ffd34c5b28bac931d1ed0e76a99bdc6f
northwestwitch <rasi.chiara@gmail.com>
2020-10-02 14:09:09 +0200

    work in progress

--- scout/commands/update/individual.py
+++ scout/commands/update/individual.py
@@ -10,15 +10,15 @@ LOG = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option("--case-id", "-c", required=True)
-@click.option("--ind-id", "-i", required=True)
+@click.option("--case-id", "-c", required=True, help="Case id")
+@click.option("--ind-name", "-n", required=True, help="Individual display name")
 @click.option(
     "--alignment-path",
     "-a",
     type=click.Path(exists=True),
     help="Replace alignment file",
 )
-def individual(case_id, ind_id, alignment_path):
+def individual(case_id, ind_name, alignment_path):
     """Update information on individual level in Scout"""
     adapter = store
     case_obj = adapter.case(case_id)
@@ -26,19 +26,21 @@ def individual(case_id, ind_id, alignment_path):
         LOG.warning("Could not find case %s", case_id)
         return
     individuals = {ind_info["individual_id"]: ind_info for ind_info in case_obj["individuals"]}
-    if ind_id not in individuals:
-        LOG.warning("Could not find individual %s in case %s", ind_id, case_id)
+    if ind_name not in individuals:
+        LOG.warning("Could not find individual %s in case %s", ind_name, case_id)
         return
 
     case_changed = False
     for ind_info in case_obj["individuals"]:
-        if not ind_info["individual_id"] == ind_id:
+        if not ind_info["display_name"] == ind_name:
             continue
+        """
         if alignment_path:
             alignment_path = pathlib.Path(alignment_path).resolve()
-            LOG.info("Updating alignment path for %s to %s", ind_id, alignment_path)
+            LOG.info("Updating alignment path for %s to %s", ind_name, alignment_path)
             ind_info["bam_file"] = str(alignment_path)
             case_changed = True
+        """
 
     if not case_changed:
         LOG.info("Nothing to update")
