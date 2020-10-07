fd70768d8fe495f02be5eff5ede851dcf554ee0c
mikaell <mikael.laaksonen@scilifelab.se>
2020-10-07 09:59:49 +0200

    Force Tests Run

--- scout/adapter/mongo/variant.py
+++ scout/adapter/mongo/variant.py
@@ -339,6 +339,7 @@ class VariantHandler(VariantLoader):
 
         return result
 
+    
     def count_gene_variants(
         self, query=None, category="snv", variant_type=["clinical"], institute_id=None
     ):
