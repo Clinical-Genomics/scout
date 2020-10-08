1ebbed22d25a41ebbf8260c3247d7cd213b7dfa4
northwestwitch <rasi.chiara@gmail.com>
2020-10-08 15:17:58 +0200

    fixed tests

--- tests/commands/update/test_update_individual_cmd.py
+++ tests/commands/update/test_update_individual_cmd.py
@@ -1,10 +1,7 @@
 """Tests for update individual command"""
 
 from click.testing import CliRunner
-
 from scout.commands.update.individual import individual as ind_cmd
-
-# from scout.server.extensions import store
 
 
 def test_update_individual_no_args():
@@ -18,41 +15,66 @@ def test_update_individual_no_args():
 
     # THEN assert it fails since its missing mandatory arguments
     assert result.exit_code == 2
+    assert "Missing option" in result.output
 
 
-def test_update_individual(empty_mock_app):
-    """Tests the CLI that updates a individual"""
+def test_update_individual_non_existing_case(empty_mock_app):
+    """Tests the CLI that updates an individual from a case that doesn't exist"""
 
-    # GIVEN a CLI object
-    runner = empty_mock_app.test_cli_runner()
-
-    # WHEN updating a individual without case and ind
-    result = runner.invoke(ind_cmd, ["--case-id", "acase", "--ind-id", "anind"])
-
-    # THEN assert it exits without problems
-    assert result.exit_code == 0
-
-
-def test_update_individual_existing(empty_mock_app, real_adapter):
-    """Tests the CLI that updates a individual"""
-
-    # GIVEN a CLI object
-    runner = empty_mock_app.test_cli_runner()
-    # GIVEN a database with a case_obj
     case_id = "acase"
     ind_id = "anind"
-    case_obj = {"case_id": case_id, "individuals": [{"individual_id": ind_id}]}
-    real_adapter.case_collection.insert_one(case_obj)
+    # GIVEN a CLI object
+    runner = empty_mock_app.test_cli_runner()
 
-    # WHEN updating a individual without case and ind
-    result = runner.invoke(ind_cmd, ["--case-id", case_id, "--ind-id", ind_id])
+    # WHEN updating a individual without a valid case
+    result = runner.invoke(ind_cmd, ["--case-id", case_id, "--ind", ind_id])
 
-    # THEN assert it exits without problems since nothing happened
+    # THEN assert it exits
     assert result.exit_code == 0
+    # With a pertinent message
+    assert "Could not find case" in result.output
+
+
+def test_update_individual_no_individual(empty_mock_app, real_adapter, case_obj):
+    """Tests the CLI that updates a individual with case id and no ind name"""
+
+    # GIVEN a CLI object
+    runner = empty_mock_app.test_cli_runner()
+
+    real_adapter.case_collection.insert_one(case_obj)
+    assert real_adapter.case_collection.find_one()
+
+    # WHEN updating a individual providing the wrong individual name or no individual name
+    result = runner.invoke(ind_cmd, ["--case-id", case_obj["_id"]])
+
+    # THEN assert it exits
+    assert result.exit_code == 0
+    # With a pertinent message
+    assert "Please specify individual name" in result.output
+
+
+def test_update_individual_no_update_key(empty_mock_app, real_adapter, case_obj):
+    """Tests the CLI that updates a individual without providing a valid key"""
+
+    # GIVEN a CLI object
+    runner = empty_mock_app.test_cli_runner()
+
+    real_adapter.case_collection.insert_one(case_obj)
+    assert real_adapter.case_collection.find_one()
+
+    ind_name = case_obj["individuals"][0]["display_name"]
+
+    # WHEN updating a individual without a valid key
+    result = runner.invoke(ind_cmd, ["--case-id", case_obj["_id"], "--ind", ind_name])
+
+    # THEN assert it exits
+    assert result.exit_code == 0
+    # With a pertinent message
+    assert "Please specify a valid key" in result.output
 
 
 def test_update_alignment_path(mock_app, real_populated_database, bam_path):
-    """Tests the CLI that updates a individual"""
+    """Tests the CLI that updates a individual with an alignment path"""
 
     # GIVEN a CLI object
     runner = mock_app.test_cli_runner()
@@ -60,16 +82,16 @@ def test_update_alignment_path(mock_app, real_populated_database, bam_path):
     existing_case = real_populated_database.case_collection.find_one()
     case_id = existing_case["_id"]
     ind_info = existing_case["individuals"][0]
-    ind_id = ind_info["individual_id"]
+    ind_name = ind_info["display_name"]
 
     # WHEN updating a individual without case and ind
     result = runner.invoke(
         ind_cmd,
-        ["--case-id", case_id, "--ind-id", ind_id, "--alignment-path", str(bam_path)],
+        ["--case-id", case_id, "--ind", ind_name, "bam_file", str(bam_path)],
     )
 
     # THEN assert it exits without problems
     assert result.exit_code == 0
     # THEN assert that the new alignment path was added
     fetched_case = real_populated_database.case_collection.find_one()
-    assert fetched_case["individuals"][0]["bam_file"] == str(bam_path.resolve())
+    assert fetched_case["individuals"][0]["bam_file"] == str(bam_path)
