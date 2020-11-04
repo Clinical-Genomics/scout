3d5164290a135f2af60679ed19889db009de6741
northwestwitch <rasi.chiara@gmail.com>
2020-11-04 15:39:26 +0100

    skip not loaded variants when preparing clinvar submission data

--- scout/server/blueprints/variant/controllers.py
+++ scout/server/blueprints/variant/controllers.py
@@ -386,6 +386,9 @@ def clinvar_export(store, institute_id, case_name, variant_id):
 
     # gather missing transcript info from entrez (refseq id version)
     for pinned_var in pinned:
+        # Check if variant is not a previous variant that is not loaded in current case
+        if isinstance(pinned_var, str):
+            continue
         for gene in pinned_var.get("genes", []):
             for transcript in gene.get("transcripts"):
                 refseq_id = transcript.get("refseq_id")
