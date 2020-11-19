a94e039c87dc15f559f4123ba8f2a77991280e35
northwestwitch <rasi.chiara@gmail.com>
2020-11-19 11:19:43 +0100

    super small change

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -640,7 +640,6 @@ def phenotypes_genes(store, case_obj):
         }
     return hpo_genes
 
-
 def hpo_diseases(username, password, hpo_ids, p_value_treshold=1):
     """Return the list of HGNC symbols that match annotated HPO terms.
 
