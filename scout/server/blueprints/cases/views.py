5531d413df781cdb009139d66be3fb78f247d773
northwestwitch <rasi.chiara@gmail.com>
2020-12-07 17:34:09 +0100

    print only OMIM gene id

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -449,6 +449,7 @@ def case_diagnosis(institute_id, case_name):
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     user_obj = store.user(current_user.email)
     link = url_for(".case", institute_id=institute_id, case_name=case_name)
+
     omim_id = request.form["omim_term"].split("|")[0]
 
     if not "OMIM:" in omim_id:  # Could be an omim number provided by user
