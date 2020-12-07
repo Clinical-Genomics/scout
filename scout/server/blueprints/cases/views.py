6c11a9fa613685329801ffcf1013b2a0517aecd8
northwestwitch <rasi.chiara@gmail.com>
2020-12-07 16:39:32 +0100

    work in progress

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -445,10 +445,10 @@ def mt_report(institute_id, case_name):
 def case_diagnosis(institute_id, case_name):
     """Add or remove a diagnosis for a case."""
 
+    level = "phenotype" if "phenotype" in request.form else "gene"
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     user_obj = store.user(current_user.email)
     link = url_for(".case", institute_id=institute_id, case_name=case_name)
-    level = "phenotype" if "phenotype" in request.form else "gene"
     omim_id = request.form["omim_term"].split("|")[0]
 
     if not "OMIM:" in omim_id:  # Could be an omim number provided by user
