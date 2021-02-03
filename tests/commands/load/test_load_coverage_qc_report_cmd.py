5fbe4a8b18b636361f59aafbbd02a0ae331ff90b
hassanfa <hassan.foroughi@gmail.com>
2021-02-03 09:49:40 +0100

    tests

---
+++ tests/commands/load/test_load_coverage_qc_report_cmd.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+
+from scout.demo import coverage_qc_report 
+from scout.commands import cli
+
+
+def test_load_coverage_qc_report(mock_app, case_obj):
+    """Testing the load delivery report cli command"""
+
+    # Make sure the path to delivery report is a valid path
+    assert os.path.isfile(coverage_qc_report_path)
+
+    runner = mock_app.test_cli_runner()
+    assert runner
+
+    # Test CLI function
+    result = runner.invoke(
+        cli,
+        ["load", "coverage-qc-report", case_obj["_id"], coverage_qc_report_path, "-u"],
+    )
+
+    assert "saved report to case!" in result.output
+    assert result.exit_code == 0
+
+
+def test_invalid_path_load_coverage_qc_report(mock_app, case_obj):
+    """Testing the load delivery report cli command"""
+
+    runner = mock_app.test_cli_runner()
+    assert runner
+
+    # Test CLI function
+    result = runner.invoke(
+        cli,
+        ["load", "coverage-qc-report", case_obj["_id"], "invalid-path", "-u"],
+    )
+    assert "does not exist" in result.output
+    assert result.exit_code == 2
