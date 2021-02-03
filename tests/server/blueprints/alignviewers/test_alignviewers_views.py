39dffe1b1a15951fc794fddfe13797fc7a3de656
northwestwitch <rasi.chiara@gmail.com>
2021-02-03 16:13:39 +0100

    add comments

--- tests/server/blueprints/alignviewers/test_alignviewers_views.py
+++ tests/server/blueprints/alignviewers/test_alignviewers_views.py
@@ -15,7 +15,9 @@ def test_remote_cors(app):
         resp = client.get(url_for("auto_login"))
         assert resp.status_code == 200
 
+        # WHEN the remote cors endpoint is invoked with an url
         resp = client.get(url_for("alignviewers.remote_cors", remote_url=cloud_track_url))
+        # THEN it should return success response
         assert resp.status_code == 200
 
 
