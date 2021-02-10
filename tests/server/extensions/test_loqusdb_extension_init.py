d0c766259a9299397156042700425c6fb0eeb7e4
northwestwitch <rasi.chiara@gmail.com>
2021-02-10 15:32:30 +0100

    fixed another file with old tests

--- tests/server/extensions/test_loqusdb_extension_init.py
+++ tests/server/extensions/test_loqusdb_extension_init.py
@@ -8,7 +8,7 @@ from scout.exceptions.config import ConfigError
 from scout.server.extensions.loqus_extension import LoqusDB
 
 
-def test_init_loqusextension(loqus_exe):
+def test_init_loqus_exe_extension(loqus_exe):
     """Test a init a loqus extension object"""
     # GIVEN a loqusdb binary
     # WHEN initialising a loqusdb extension
@@ -16,12 +16,12 @@ def test_init_loqusextension(loqus_exe):
     # THEN assert that the binary is correct
     assert loqus_obj.get_bin_path() == loqus_exe
     # THEN assert that the version is 0
-    assert loqus_obj.version is None
+    assert loqus_obj.loqusdb_settings[0]["version"] is None
     # THEN assert that there is no config
     assert loqus_obj.get_config_path() is None
 
 
-def test_init_loqusextension_version(loqus_exe, loqus_version):
+def test_init_loqus_exe_extension_version(loqus_exe, loqus_version):
     """Test a init a loqus extension object with a specified version"""
     # GIVEN a loqusdb binary and a version
     # WHEN initialising a loqusdb extension
@@ -29,12 +29,12 @@ def test_init_loqusextension_version(loqus_exe, loqus_version):
     # THEN assert that the binary is correct
     assert loqus_obj.get_bin_path() == loqus_exe
     # THEN assert that the version is correct
-    assert loqus_obj.version == loqus_version
+    assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
     # THEN assert that there is no config
     assert loqus_obj.get_config_path() is None
 
 
-def test_init_loqusextension_config(loqus_exe, loqus_config, loqus_version):
+def test_init_loqus_exe_extension_config(loqus_exe, loqus_config, loqus_version):
     """Test a init a loqus extension object with a specified version"""
     # GIVEN a loqusdb binary, a version and a config
     # WHEN initialising a loqusdb extension
@@ -44,15 +44,20 @@ def test_init_loqusextension_config(loqus_exe, loqus_config, loqus_version):
     # THEN assert that the binary is correct
     assert loqus_obj.get_bin_path() == loqus_exe
     # THEN assert that the version is correct
-    assert loqus_obj.version == loqus_version
-    # THEN assert that there is no config
-    assert loqus_obj.loqusdb_settings == [
-        {"id": "default", "binary_path": loqus_exe, "config_path": None}
-    ]
+    assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
+    # THEN assert that there is no loqus exe config file
+    assert loqus_obj.loqusdb_settings[0]["config_path"] is None
 
 
-def test_init_loqusextension_init_app(loqus_exe, loqus_version):
-    """Test a init a loqus extension object with flask app with version"""
+def test_init_loqus_exe_extension_init_app(loqus_exe, loqus_version, monkeypatch):
+    """Test init a loqus exe extension object with flask app with version"""
+
+    # GIVEN a patched Loqus executable instance
+    def mockversion(*args):
+        return loqus_version
+
+    monkeypatch.setattr(LoqusDB, "get_instance_version", mockversion)
+
     # GIVEN a loqusdb binary
     configs = {"LOQUSDB_SETTINGS": {"binary_path": loqus_exe, "version": loqus_version}}
     # WHEN initialising a loqusdb extension with init app
@@ -64,13 +69,13 @@ def test_init_loqusextension_init_app(loqus_exe, loqus_version):
         # THEN assert that the binary is correct
         assert loqus_obj.get_bin_path() == loqus_exe
         # THEN assert that the version is correct
-        assert loqus_obj.version == loqus_version
+        assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
         # THEN assert that config.py has been read
-        assert loqus_obj.loqusdb_settings is not None
+        assert loqus_obj.loqusdb_settings[0] is not None
 
 
-def test_init_loqusextension_init_app_no_version(mocker, loqus_exe, loqus_version):
-    """Test a init a loqus extension object with flask app"""
+def test_init_loqus_exe_extension_init_app_no_version(mocker, loqus_exe, loqus_version):
+    """Test init a loqus extension object with flask app"""
     # GIVEN a loqusdb binary
     configs = {"LOQUSDB_SETTINGS": {"binary_path": loqus_exe}}
     mocker.patch.object(subprocess, "check_output")
@@ -83,14 +88,14 @@ def test_init_loqusextension_init_app_no_version(mocker, loqus_exe, loqus_versio
         loqus_obj.init_app(app)
         # THEN assert that the binary is correct
         assert loqus_obj.get_bin_path() == loqus_exe
-        assert loqus_obj.version == loqus_version
+        assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
         # THEN assert that there is no config
         # THEN assert that config.py has been read
-        assert loqus_obj.loqusdb_settings is not None
+        assert loqus_obj.loqusdb_settings[0] is not None
 
 
-def test_init_loqusextension_init_app_wrong_version(loqus_exe):
-    """Test a init a loqus extension object with flask app"""
+def test_init_loqus_exe_extension_init_app_wrong_version(loqus_exe):
+    """Test init a loqus exe extension that is too old for the flask app"""
     # GIVEN a loqusdb binary
     configs = {"LOQUSDB_SETTINGS": {"binary_path": loqus_exe, "version": 1.0}}
     # WHEN initialising a loqusdb extension with init app
@@ -102,8 +107,15 @@ def test_init_loqusextension_init_app_wrong_version(loqus_exe):
             loqus_obj.init_app(app)
 
 
-def test_init_loqusextension_init_app_with_config(loqus_exe, loqus_config):
-    """Test a init a loqus extension object with flask app with version and config"""
+def test_init_loqus_exe_extension_init_app_with_config(loqus_exe, loqus_config, monkeypatch):
+    """Test init a loqus exe extension object with flask app with version and config"""
+
+    # GIVEN a patched Loqus executable instance
+    def mockversion(*args):
+        return 2.5
+
+    monkeypatch.setattr(LoqusDB, "get_instance_version", mockversion)
+
     # GIVEN a loqusdb config as dict
     version = 2.5
     configs = {
@@ -122,20 +134,28 @@ def test_init_loqusextension_init_app_with_config(loqus_exe, loqus_config):
         # THEN assert that the binary is correct
         assert loqus_obj.get_bin_path() == loqus_exe
         # THEN assert that the version is correct
-        assert loqus_obj.version == version
+        assert loqus_obj.loqusdb_settings[0]["version"] == version
         # THEN assert that the config is correct
         assert loqus_obj.get_config_path() == loqus_config
 
 
-def test_init_loqusextension_init_app_with_config_multiple(loqus_exe, loqus_config):
-    """Test a init a loqus extension object with flask app with version and config"""
+def test_init_loqus_exe_extension_init_app_with_config_multiple(
+    loqus_exe, loqus_config, loqus_version, monkeypatch
+):
+    """ Test init a loqus exe extension object with flask app with version and config """
+
+    # GIVEN a patched Loqus executable instance
+    def mockversion(*args):
+        return loqus_version
+
+    monkeypatch.setattr(LoqusDB, "get_instance_version", mockversion)
+
     # GIVEN a loqusdb config as list
-    version = 2.5
     configs = {
         "LOQUSDB_SETTINGS": [
             {
                 "binary_path": loqus_exe,
-                "version": version,
+                "version": loqus_version,
                 "id": "default",
                 "config_path": loqus_config,
             }
@@ -165,32 +185,24 @@ def test_init_loqusextension_init_app_with_config_multiple(loqus_exe, loqus_conf
         with pytest.raises(ConfigError):
             assert loqus_obj.get_config_path("not configured id")
 
-        # THEN assert that the version is correct -with id
-        assert loqus_obj.get_configured_version("default") == version
-        # THEN assert that the version is correct -without id
-        assert loqus_obj.get_configured_version(None) == version
-        # THEN non-configured id raises
-        with pytest.raises(ConfigError):
-            assert loqus_obj.get_configured_version("not configured id")
-
         # THEN assert that the version is correct
-        assert loqus_obj.version == version
+        assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
         # THEN assert that the config is correct
 
 
-def test_init_loqusextension_init_app_get_version(loqus_exe, loqus_version):
-    """Test a init a loqus extension object with flask app with version and config"""
+def test_init_loqus_exe_extension_init_app_get_version(loqus_exe, loqus_version):
+    """Test init an exe loqus extension object with flask app with version and config"""
     # GIVEN a loqusdb binary
 
     # THEN initialising a loqusdb extension with init app
     loqus_obj = LoqusDB(version=loqus_version)
-    assert loqus_obj.get_version() == loqus_version
+    assert loqus_obj.loqusdb_settings[0]["version"] == loqus_version
 
 
-def test_init_loqusextension_init_app_get_version_CalledProcessError(
+def test_init_loquse_exe_extension_init_app_get_version_CalledProcessError(
     loqus_exe, loqus_config, mocker
 ):
-    """Test a init a loqus extension object with flask app with version and config"""
+    """Test init an exe loqus extension object with flask app with version and config"""
     # GIVEN mocking subprocess to raise CalledProcessError
     mocker.patch.object(
         subprocess, "check_output", side_effect=CalledProcessError(123, "case_count")
