3f09a1cc87a1fc11e5d74edde97a48a3ed4f0a67
northwestwitch <rasi.chiara@gmail.com>
2021-02-15 09:36:00 +0100

    increase coverage

--- tests/server/extensions/test_loqusdb_api_extension.py
+++ tests/server/extensions/test_loqusdb_api_extension.py
@@ -28,7 +28,22 @@ def test_loqusdb_api_snv_variant(loqus_api_app, monkeypatch, loqus_api_variant):
         assert var_info["observations"] == loqus_api_variant["observations"]
 
 
-def test_loqusdb_api_sn_variant(loqus_api_app, monkeypatch, loqus_api_variant):
+def test_loqus_api_snv_variant_not_found(loqus_api_app, monkeypatch, loqus_api_variant):
+    # GIVEN a mocked loqus API that doesn't return usable info
+    def mockapi(*args):
+        return {"details": "not found"}
+
+    monkeypatch.setattr(loqus_extension, "api_get", mockapi)
+
+    with loqus_api_app.app_context():
+        # WHEN fetching the variant info
+        var_info = loqusdb.get_variant({"_id": "a variant", "category": "snv"})
+
+        # THEN the loqus extensions should return an empty dictionary
+        assert var_info == {}
+
+
+def test_loqusdb_api_sv_variant(loqus_api_app, monkeypatch, loqus_api_variant):
     """Test fetching a SV variant info from loqusdb API"""
 
     # GIVEN a mocked loqus API
@@ -63,8 +78,24 @@ def test_loqusdb_api_cases(loqus_api_app, monkeypatch):
         return {"nr_cases_snvs": nr_snvs, "nr_cases_svs": nr_svs, "status_code": 200}
 
     monkeypatch.setattr(loqus_extension, "api_get", mockapi)
+
     with loqus_api_app.app_context():
         # WHEN fetching the nr_cases with SNV variants
         n_cases = loqusdb.case_count("snv", "default")
         # THEN the API should parse the returned info correctly
         assert n_cases == nr_snvs
+
+
+def test_loqusdb_api_cases_not_found(loqus_api_app, monkeypatch):
+    """Test fetching info on number of cases from loqusdb API when the API is not funct"""
+
+    def mockapi(*args):
+        return {"details": "not found"}
+
+    monkeypatch.setattr(loqus_extension, "api_get", mockapi)
+
+    with loqus_api_app.app_context():
+        # WHEN fetching the nr_cases with SNV variants
+        n_cases = loqusdb.case_count("snv", "default")
+        # THEN the if no cases are found, it should return 0 cases
+        assert n_cases == 0
