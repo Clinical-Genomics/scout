d62410f9d8866e99e797aecd53d6ea08c5291f98
northwestwitch <rasi.chiara@gmail.com>
2021-02-16 14:54:15 +0100

    increase coverage by testing directing the cli command call

---
+++ tests/commands/load/test_load_report.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+import click
+import tempfile
+
+from scout.commands import cli
+from scout.server.extensions import store
+
+
+def test_load_gene_fusion_report(mock_app):
+    """Test command line function that updated the gene fusion report for an existing case"""
+    # GIVEN a database with an existing case
+    case_obj = store.case_collection.find_one()
+
+    # GIVEN that this case has an old gene fusion report
+    old_report = case_obj.get("gene_fusion_report")
+
+    case_id = case_obj["_id"]
+    runner = mock_app.test_cli_runner()
+
+    # WHEN the update_gene_fusion command is executed provifing a new gene fusion report
+    with tempfile.NamedTemporaryFile(suffix=".tsv", prefix=os.path.basename(__file__)) as tf:
+
+        new_report_path = os.path.dirname(tf.name)
+        result = runner.invoke(
+            cli, ["load", "gene-fusion-report", case_id, new_report_path, "--update"]
+        )
+
+        # THEN the command should be succesful
+        assert result.exit_code == 0
+
+        # And the gene fusion report should have been updated
+        updated_case = store.case_collection.find_one()
+        assert updated_case["gene_fusion_report"] != old_report
--- tests/load/test_load_report.py
+++ tests/load/test_load_report.py
@@ -116,27 +116,3 @@ def test_load_coverage_qc_report_using_case_id_with_update_success(adapter, canc
     # THEN a report should have been added to that case
     updated_case_obj = adapter.case_collection.find_one()
     assert updated_case_obj["coverage_qc_report"] == report_path
-
-
-def test_load_gene_fusion_report_using_case_id_with_update_success(adapter, case_obj):
-
-    adapter.case_collection.insert_one(case_obj)
-    ## GIVEN a case exist, with a delivery report
-    case_obj = adapter.case_collection.find_one()
-    assert case_obj.get("gene_fusion_report")
-
-    ## WHEN trying to load a report for a case_id that does exist in the data base
-    case_id = case_obj["_id"]
-    report_path = "report_test_path"
-    update = True
-
-    load_gene_fusion_report(
-        adapter=adapter,
-        case_id=case_id,
-        report_path=report_path,
-        update=update,
-    )
-
-    # THEN a report should have been added to that case
-    updated_case_obj = adapter.case_collection.find_one()
-    assert updated_case_obj["gene_fusion_report"] == report_path
