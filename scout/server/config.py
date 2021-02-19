8ab3be28886033557ade357c4259ef2b7b0adf28
northwestwitch <rasi.chiara@gmail.com>
2021-02-19 10:40:19 +0100

    added a test and made a couple of fixes

--- scout/server/config.py
+++ scout/server/config.py
@@ -17,8 +17,8 @@ MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 
 # Configure gens service
-GENS_HOST = "127.0.0.1"
-GENS_PORT = 5000
+# GENS_HOST = "127.0.0.1"
+# GENS_PORT = 5000
 
 # connection details for LoqusDB MongoDB database
 #
