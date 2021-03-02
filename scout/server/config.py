93c4d22db9262a1455b41582157521b7b1f06838
northwestwitch <rasi.chiara@gmail.com>
2021-03-02 11:04:53 +0100

    revert config file

--- scout/server/config.py
+++ scout/server/config.py
@@ -16,13 +16,46 @@ MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
 
+# Configure gens service
+# GENS_HOST = "127.0.0.1"
+# GENS_PORT = 5000
 
-LOQUSDB_SETTINGS = [
-    {"binary_path": "/Users/chiararasi/miniconda3/envs/loqus/bin/loqusdb", "id": "default"},
-    {"binary_path": "/Users/chiararasi/miniconda3/envs/loqus/bin/loqusdb", "id": "hejhej"},
-    {"binary_path": "/Users/chiararasi/miniconda3/envs/loqus/bin/loqusdb", "id": "Mayo"},
-]
+# connection details for LoqusDB MongoDB database
+#
+# 1. One LoqusDB
+# LOQUSDB_SETTINGS = dict(
+#  binary_path="/miniconda3/envs/loqus2.5/bin/loqusdb")
+# # config_path=<path/to/loqus/config>
+#
+# 2. LoqusDB configurable per Institute
+# LOQUSDB_SETTINGS = [
+#     {"binary_path": "/miniconda3/envs/loqus2.5/bin/loqusdb", "id": "default"},
+#     {"binary_path": "/miniconda3/envs/loqus2.5/bin/loqusdb", "id": "hejhej"},
+#     {"binary_path": "/bin/yetanother/loqusdb", "id": "Mayo"},
+# ]
 
+# )
+# If not on localhost 27017 one needs to provide uri with
+# connection details for LoqusDB MongoDB database in the loqusdb config file
+#    uri=("mongodb://{}:{}@localhost:{}/loqusdb".format(MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT))
+
+# Cloud IGV tracks can be configured here to allow users to enable them on their IGV views.
+# CLOUD_IGV_TRACKS = [
+#    {
+#        "name": "custom_public_bucket",
+#        "access": "public",
+#        "tracks": [
+#            {
+#                "name": "dbVar Pathogenic or Likely Pathogenic",
+#                "type": "variant",
+#                "format": "vcf",
+#                "build": "37",
+#                "url": "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/25777457/GRCh37.variant_call.clinical.pathogenic_or_likely_pathogenic.vcf.gz",
+#                "indexURL": "https://s3-eu-west-1.amazonaws.com/pfigshare-u-files/25777460/GRCh37.variant_call.clinical.pathogenic_or_likely_pathogenic.vcf.gz.tbi",
+#            }
+#        ],
+#    },
+# ]
 
 # Chanjo-Report
 REPORT_LANGUAGE = "en"
