60f07d96f67f4531323afa7130c2e4e6f07e78e8
mikaell <mikael.laaksonen@mobilearts.com>
2021-04-01 11:54:49 +0200

    WiP: Pydantic

--- scout/parse/case.py
+++ scout/parse/case.py
@@ -18,6 +18,8 @@ from scout.parse.peddy import (
 from scout.parse.smn import parse_smn_file
 from scout.utils.date import get_date
 
+from scout.parse.config_base import ScoutLoadConfig, ScoutIndividual, VcfFiles
+
 LOG = logging.getLogger(__name__)
 
 
@@ -30,7 +32,11 @@ def get_correct_date(date_info):
     Returns:
         correct_date(datetime.datetime)
     """
+    LOG.debug(date_info)
+    if isinstance(date_info, str):
+        LOG.debug("STRINIGNIGGNIGNDIREKT")
     if isinstance(date_info, datetime.datetime):
+        LOG.debug("CORRECT DIREKT")
         return date_info
 
     if isinstance(date_info, str):
@@ -46,131 +52,92 @@ def get_correct_date(date_info):
     return datetime.datetime.now()
 
 
-def parse_case_data(
-    config=None,
-    ped=None,
-    owner=None,
-    vcf_snv=None,
-    vcf_sv=None,
-    vcf_cancer=None,
-    vcf_cancer_sv=None,
-    vcf_str=None,
-    smn_tsv=None,
-    peddy_ped=None,
-    peddy_sex=None,
-    peddy_check=None,
-    delivery_report=None,
-    multiqc=None,
-    cnv_report=None,
-    coverage_qc_report=None,
-    gene_fusion_report=None,
-    gene_fusion_report_research=None,
-):
+def parse_case_data(**kwargs):
     """Parse all data necessary for loading a case into scout
 
     This can be done either by providing a VCF file and other information
     on the command line. Or all the information can be specified in a config file.
     Please see Scout documentation for further instructions.
 
-    Args:
+    Possible keyword args:
+        cnv_report: Path to pdf file with CNV report
         config(dict): A yaml formatted config file
-        ped(iterable(str)): A ped formatted family file
+        coverage_qc_report: Path to html file with coverage and qc report
+        multiqc(str): Path to dir with multiqc information
         owner(str): The institute that owns a case
+        ped(iterable(str)): A ped formatted family file
+        peddy_ped(str): Path to a peddy ped
+        smn_tsv(str): Path to an SMN tsv file
+        vcf_cancer(str): Path to a vcf file
+        vcf_cancer_sv(str): Path to a vcf file
         vcf_snv(str): Path to a vcf file
         vcf_str(str): Path to a VCF file
         vcf_sv(str): Path to a vcf file
-        vcf_cancer(str): Path to a vcf file
-        vcf_cancer_sv(str): Path to a vcf file
-        smn_tsv(str): Path to an SMN tsv file
-        peddy_ped(str): Path to a peddy ped
-        multiqc(str): Path to dir with multiqc information
-        cnv_report: Path to pdf file with CNV report
-        coverage_qc_report: Path to html file with coverage and qc report
-        gene_fusion_report: Path to the gene fusions report
-        gene_fusion_report_research: Path to the gene fusions research report
 
     Returns:
         config_data(dict): Holds all the necessary information for loading
                            Scout
     """
-    config_data = copy.deepcopy(config) or {}
+    LOG.debug("***PARSE_CASE_DATA()***")
+    # If ped file  provided we need to parse that first
+    config = kwargs.pop("config", {})
+    if "ped" in kwargs and kwargs["ped"] is not None:
+        family_id, samples = parse_ped(kwargs["ped"])
+        config["family"] = family_id
+        config["samples"] = samples
+
+    # Give passed keyword arguments precedence over file configuration
+    # Except for 'owner', prededence config file over arguments
+    LOG.debug("KWARGS: {}".format(kwargs))
+    if "owner" in config:
+        print(kwargs)
+        kwargs.pop("owner", None)  # dont crash if 'owner' is missing
+    for key in kwargs:
+        if kwargs[key] is not None:
+            config[key] = kwargs[key]
+        else:
+            try:
+                config[key] = config[key]
+            except KeyError:
+                config[key] = None
+
+    # populate configuration according to Pydantic defined classes
+    LOG.debug("1st SCOUTLOADCONFIG")
+    config = ScoutLoadConfig(**config)
+
+    # convert to dict
+    config = config.dict()
+
+    synopsis = (
+        ". ".join(config["synopsis"])
+        if isinstance(config["synopsis"], list)
+        else config["synopsis"]
+    )
+
     # Default the analysis date to now if not specified in load config
-    config_data["analysis_date"] = get_correct_date(config_data.get("analysis_date"))
+    config["analysis_date"] = get_correct_date(config.get("analysis_date"))
 
-    # If the family information is in a ped file we nned to parse that
-    if ped:
-        family_id, samples = parse_ped(ped)
-        config_data["family"] = family_id
-        config_data["samples"] = samples
-
-    # Each case has to have a owner. If not provided in config file it needs to be given as a
-    # argument
-    if "owner" not in config_data:
-        if not owner:
-            raise SyntaxError("Case has no owner")
-
-        config_data["owner"] = owner
-
-    if "gene_panels" in config_data:
+    try:
         # handle whitespace in gene panel names
-        config_data["gene_panels"] = [panel.strip() for panel in config_data["gene_panels"]]
-        config_data["default_gene_panels"] = [
-            panel.strip() for panel in config_data["default_gene_panels"]
+        config["gene_panels"] = [panel.strip() for panel in config["gene_panels"]]
+        config["default_gene_panels"] = [
+            panel.strip() for panel in config["default_gene_panels"]
         ]
-
-    ##################### Add information from peddy if existing #####################
-    config_data["peddy_ped"] = peddy_ped or config_data.get("peddy_ped")
-    config_data["peddy_sex_check"] = peddy_sex or config_data.get("peddy_sex")
-    config_data["peddy_ped_check"] = peddy_check or config_data.get("peddy_check")
+    except:
+        pass
 
     # This will add information from peddy to the individuals
-    add_peddy_information(config_data)
+    add_peddy_information(config)
 
     ##################### Add multiqc information #####################
-    config_data["multiqc"] = multiqc or config_data.get("multiqc")
-
-    config_data["vcf_snv"] = vcf_snv if vcf_snv else config_data.get("vcf_snv")
-    config_data["vcf_sv"] = vcf_sv if vcf_sv else config_data.get("vcf_sv")
-    config_data["vcf_str"] = vcf_str if vcf_str else config_data.get("vcf_str")
-    config_data["smn_tsv"] = smn_tsv if smn_tsv else config_data.get("smn_tsv")
-
     LOG.debug("Checking for SMN TSV..")
-    if config_data["smn_tsv"]:
-        LOG.info("Adding SMN info from {}.".format(config_data["smn_tsv"]))
-        add_smn_info(config_data)
+    if config["smn_tsv"]:
+        LOG.info("Adding SMN info from {}.".format(config["smn_tsv"]))
+        add_smn_info(config)
 
-    config_data["vcf_cancer"] = vcf_cancer if vcf_cancer else config_data.get("vcf_cancer")
-    config_data["vcf_cancer_sv"] = (
-        vcf_cancer_sv if vcf_cancer_sv else config_data.get("vcf_cancer_sv")
-    )
+    LOG.debug("***PARSE_CASE_DATA return: {}***".format(removeNoneRecursive(config)))
 
-    config_data["delivery_report"] = (
-        delivery_report if delivery_report else config_data.get("delivery_report")
-    )
-
-    config_data["cnv_report"] = cnv_report if cnv_report else config_data.get("cnv_report")
-    config_data["coverage_qc_report"] = (
-        coverage_qc_report if coverage_qc_report else config_data.get("coverage_qc_report")
-    )
-    config_data["gene_fusion_report"] = (
-        gene_fusion_report if gene_fusion_report else config_data.get("gene_fusion_report")
-    )
-    config_data["gene_fusion_report_research"] = (
-        gene_fusion_report_research
-        if gene_fusion_report_research
-        else config_data.get("gene_fusion_report_research")
-    )
-
-    config_data["rank_model_version"] = str(config_data.get("rank_model_version", ""))
-    config_data["rank_score_threshold"] = config_data.get("rank_score_threshold", 0)
-
-    config_data["sv_rank_model_version"] = str(config_data.get("sv_rank_model_version", ""))
-
-    config_data["track"] = config_data.get("track", "rare")
-    if config_data["vcf_cancer"] or config_data["vcf_cancer_sv"]:
-        config_data["track"] = "cancer"
-
-    return config_data
+    return removeNoneRecursive(config)
 
 
 def add_smn_info(config_data):
@@ -202,7 +169,9 @@ def add_smn_info(config_data):
             ]:
                 ind[key] = smn_info[ind_id][key]
         except KeyError as e:
-            LOG.warning("Individual {} has no SMN info to update: {}.".format(ind_id, e))
+            LOG.warning(
+                "Individual {} has no SMN info to update: {}.".format(ind_id, e)
+            )
 
 
 def add_smn_info_case(case_data):
@@ -234,7 +203,9 @@ def add_smn_info_case(case_data):
             ]:
                 ind[key] = smn_info[ind_id][key]
         except KeyError as e:
-            LOG.warning("Individual {} has no SMN info to update: {}.".format(ind_id, e))
+            LOG.warning(
+                "Individual {} has no SMN info to update: {}.".format(ind_id, e)
+            )
 
 
 def add_peddy_information(config_data):
@@ -275,7 +246,9 @@ def add_peddy_information(config_data):
         ind = analysis_inds[ind_id]
         # Check if peddy has inferred the ancestry
         if ind_id in ped_info:
-            ind["predicted_ancestry"] = ped_info[ind_id].get("ancestry-prediction", "UNKNOWN")
+            ind["predicted_ancestry"] = ped_info[ind_id].get(
+                "ancestry-prediction", "UNKNOWN"
+            )
         # Check if peddy has inferred the sex
         if ind_id in sex_check:
             if sex_check[ind_id]["error"]:
@@ -405,7 +378,9 @@ def parse_individual(sample):
     ind_info["tiddit_coverage_wig"] = sample.get(
         "tiddit_coverage_wig", sample.get("tiddit_coverage_wig")
     )
-    ind_info["upd_regions_bed"] = sample.get("upd_regions_bed", sample.get("upd_regions_bed"))
+    ind_info["upd_regions_bed"] = sample.get(
+        "upd_regions_bed", sample.get("upd_regions_bed")
+    )
     ind_info["upd_sites_bed"] = sample.get("upd_sites_bed", sample.get("upd_sites_bed"))
     ind_info["mt_bam"] = sample.get("mt_bam")
     ind_info["analysis_type"] = sample.get("analysis_type")
@@ -421,7 +396,9 @@ def parse_individual(sample):
     ind_info["smn2delta78_cn"] = sample.get("smn2delta78_cn", None)
     ind_info["smn_27134_cn"] = sample.get("smn_27134_cn", None)
 
-    ind_info["capture_kits"] = [sample.get("capture_kit")] if "capture_kit" in sample else []
+    ind_info["capture_kits"] = (
+        [sample.get("capture_kit")] if "capture_kit" in sample else []
+    )
 
     # Cancer specific values
     ind_info["tumor_type"] = sample.get("tumor_type")
@@ -439,7 +416,7 @@ def parse_individual(sample):
     ind_info["chromograph_images"] = sample.get("chromograph_images")
 
     # Remove key-value pairs from ind_info where key==None and return
-    return removeNoneValues(ind_info)
+    return removeNoneRecursive(ind_info)
 
 
 def parse_individuals(samples):
@@ -486,62 +463,12 @@ def parse_case(config):
     Returns:
         dict: parsed case data
     """
-    if "owner" not in config:
-        raise ConfigError("A case has to have a owner")
 
-    if "family" not in config:
-        raise ConfigError("A case has to have a 'family'")
-
-    individuals = parse_individuals(config["samples"])
-    synopsis = None
-    if config.get("synopsis"):
-        synopsis = (
-            ". ".join(config["synopsis"])
-            if isinstance(config["synopsis"], list)
-            else config["synopsis"]
-        )
-
-    case_data = {
-        "owner": config["owner"],
-        "collaborators": [config["owner"]],
-        "case_id": config["family"],
-        "display_name": config.get("family_name", config["family"]),
-        "synopsis": synopsis,
-        "phenotype_terms": config.get("phenotype_terms"),
-        "cohorts": config.get("cohorts"),
-        "genome_build": config.get("human_genome_build"),
-        "lims_id": config.get("lims_id"),
-        "rank_model_version": str(config.get("rank_model_version", "")),
-        "rank_score_threshold": config.get("rank_score_threshold", 0),
-        "sv_rank_model_version": str(config.get("sv_rank_model_version", "")),
-        "analysis_date": config.get("analysis_date"),
-        "individuals": individuals,
-        "vcf_files": {
-            "vcf_snv": config.get("vcf_snv"),
-            "vcf_sv": config.get("vcf_sv"),
-            "vcf_str": config.get("vcf_str"),
-            "vcf_cancer": config.get("vcf_cancer"),
-            "vcf_cancer_sv": config.get("vcf_cancer_sv"),
-            "vcf_snv_research": config.get("vcf_snv_research"),
-            "vcf_sv_research": config.get("vcf_sv_research"),
-            "vcf_cancer_research": config.get("vcf_cancer_research"),
-            "vcf_cancer_sv_research": config.get("vcf_cancer_sv_research"),
-        },
-        "smn_tsv": config.get("smn_tsv"),
-        "default_panels": config.get("default_gene_panels", []),
-        "gene_panels": config.get("gene_panels", []),
-        "assignee": config.get("assignee"),
-        "peddy_ped": config.get("peddy_ped"),
-        "peddy_sex": config.get("peddy_sex"),
-        "peddy_check": config.get("peddy_check"),
-        "delivery_report": config.get("delivery_report"),
-        "cnv_report": config.get("cnv_report"),
-        "coverage_qc_report": config.get("coverage_qc_report"),
-        "gene_fusion_report": config.get("gene_fusion_report"),
-        "gene_fusion_report_research": config.get("gene_fusion_report_research"),
-        "multiqc": config.get("multiqc"),
-        "track": config.get("track", "rare"),
-    }
+    LOG.debug("***PARSE_CASE()***")
+    configObj = ScoutLoadConfig(**config)  # create a config object based on pydantic
+    vcf_files = VcfFiles(**config)  # vcf_files ...
+    configObj.vcf_files = vcf_files
+    case_data = configObj.dict()  # translate object to legacy dict
 
     # add SMN info
     LOG.debug("Checking for SMN TSV..")
@@ -549,25 +476,11 @@ def parse_case(config):
         LOG.info("Adding SMN info from {}.".format(case_data["smn_tsv"]))
         add_smn_info_case(case_data)
 
-    # add the pedigree figure, this is a xml file which is dumped in the db
-    if "madeline" in config:
-        mad_path = Path(config["madeline"])
-        if not mad_path.exists():
-            raise ValueError("madeline path not found: {}".format(mad_path))
-        with mad_path.open("r") as in_handle:
-            case_data["madeline_info"] = in_handle.read()
-
-    if (
-        case_data["vcf_files"]["vcf_cancer"]
-        or case_data["vcf_files"]["vcf_cancer_research"]
-        or case_data["vcf_files"]["vcf_cancer_sv"]
-        or case_data["vcf_files"]["vcf_cancer_sv_research"]
-    ):
-        case_data["track"] = "cancer"
-
-    case_data["analysis_date"] = get_correct_date(case_data.get("analysis_date"))
-
-    return case_data
+    # TODO: parse_ped()
+    LOG.debug(
+        "***PARSE_CASE Return: {}***".format(removeNoneRecursive(configObj.dict()))
+    )
+    return removeNoneRecursive(configObj.dict())
 
 
 def parse_ped(ped_stream, family_type="ped"):
@@ -600,6 +513,7 @@ def parse_ped(ped_stream, family_type="ped"):
         for ind_id, individual in family.individuals.items()
     ]
 
+    LOG.debug("return (parse_ped): {}".format(family_id))
     return family_id, samples
 
 
@@ -614,3 +528,24 @@ def removeNoneValues(dictionary):
     """
 
     return {k: v for k, v in dictionary.items() if v is not None}
+
+
+def removeNoneRecursive(dictionary):
+    """Recusivly remove None from dictionary"""
+    return removeNoneRecursive_aux(dictionary, {})
+
+
+def removeNoneRecursive_aux(dictionary, new_dict):
+    LOG.debug("diction: {}".format(dictionary))
+    for key, value in dictionary.items():
+        if value is not None:
+            new_dict.update({key: value})
+        if (
+            isinstance(value, list)
+            and len(value) > 0
+                and key not in ["collaborators", "cohorts", "default_panels", "gene_panels", "capture_kits", "phenotype_terms"]
+        ):
+            new_list = [removeNoneRecursive_aux(item, {}) for item in value]
+            new_dict.update({key: new_list})
+
+    return new_dict
