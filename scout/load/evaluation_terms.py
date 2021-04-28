576c241c5571276f8667c29b44752d1bf1983d6a
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-04-28 10:51:54 +0200

    Load multiple eval terms with file in json format

--- scout/load/evaluation_terms.py
+++ scout/load/evaluation_terms.py
@@ -4,12 +4,18 @@
 import logging
 
 import datetime
+import pymongo
 
 LOG = logging.getLogger(__name__)
 
 
-def load_evaluation_term(adapter, internal_id, label, description, term_categroy, institute=None, **kwargs):
+def load_evaluation_term(adapter, internal_id, label, description, term_categroy, institute=None, rank=None, **kwargs):
     """Load a evaluation term into the database."""
+
+    if rank is None:
+        query = {'institute': institute if institute else 'all', 'sort': [('rank', pymongo.DESCENDING)]}
+        last_rank = adapter.evaluation_terms_collection.find_one(query)['rank']
+        rank += last_rank
 
     evaluation_term_obj = dict(
         internal_id=internal_id,
@@ -17,6 +23,7 @@ def load_evaluation_term(adapter, internal_id, label, description, term_categroy
         institute=institute,
         description=description,
         type=term_categroy,
+        rank=rank,
         last_modified=datetime.datetime.utcnow(),
         **kwargs
     )
