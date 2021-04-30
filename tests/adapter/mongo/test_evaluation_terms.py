ce999979e12a1a0351c8168d43914a79fa67712b
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-04-30 12:58:22 +0200

    Added tests of evaluation terms

---
+++ tests/adapter/mongo/test_evaluation_terms.py
@@ -0,0 +1,56 @@
+import pytest
+
+
+def test_insert_evaluation_term(adapter):
+    ##GIVEN a empty adapter
+    assert sum(1 for i in adapter.evaluation_terms()) == 0
+
+    ##WHEN inserting a transcript
+    evaluation_term_obj = {
+        "internal_id" : "test-a",
+        "name" : "test a",
+        "description" : "a test term",
+        "term_category" : "dismissal_term",
+        "rank" : 0,
+        "institute" : "all",
+        "analysis_type" : "all",
+        "label" : "test a",
+        "evidence" : [ ]
+    }
+    obj_id = adapter.add_evaluation_term(evaluation_term_obj)
+
+    ##THEN assert that the transcript is there
+    assert sum(1 for i in adapter.evaluation_terms()) == 1
+
+
+@pytest.mark.parametrize('param_name', ['institute', 'analysis_type'])
+def test_get_institute_exclusive_evaluation_term(adapter, param_name):
+    ##GIVEN a empty adapter
+    assert sum(1 for i in adapter.evaluation_terms()) == 0
+
+    base_term = {
+        "internal_id" : "test-a",
+        "name" : "test a",
+        "description" : "a test term",
+        "term_category" : "dismissal_term",
+        "rank" : 0,
+        "institute" : "all",
+        "analysis_type" : "all",
+        "label" : "test a",
+        "evidence" : [ ]
+    }
+    ## override an exclusive param
+    generic_term = base_term.copy()
+    obj_id = adapter.add_evaluation_term(generic_term)
+
+    ## institute
+    exclusive_term_obj = base_term.copy()
+    exclusive_term_obj[param_name] = 'foobar'
+    obj_id = adapter.add_evaluation_term(exclusive_term_obj)
+
+    ##THEN assert that the generic transcript is there
+    assert sum(1 for i in adapter.evaluation_terms()) == 1
+
+    ##THEN assert that the exclusive transcript is there
+    query = {param_name: 'foobar'}
+    assert sum(1 for i in adapter.evaluation_terms(query)) == 2
---
+++ tests/load/test_load_evaluation_term.py
@@ -0,0 +1,67 @@
+from scout.load import load_evaluation_term
+
+
+def test_load_default_evaluation_term(adapter):
+    # Assert empty database
+    assert sum(1 for i in adapter.evaluation_terms()) == 0
+
+    test_entry = {
+        'internal_id': 'test-entry',
+        'name': 'test entry',
+        'description': 'an entry to test the import',
+        'term_category': 'dismissal_term',
+    }
+    load_evaluation_term(adapter, **test_entry)
+    # test if loaded
+    assert sum(1 for i in adapter.evaluation_terms()) == 1
+    # test assignment of data
+    entry = adapter.get_evaluation_term('dismissal_term', term_id='test-entry')
+    assert 'last_modified' in entry and 'rank' in entry
+    assert all(test_key in entry.keys() for test_key in test_entry)
+    # assert assignment of rank
+    assert entry['rank'] == 1
+
+
+def test_load_evaluation_terms_kwargs(adapter):
+    # Assert empty database
+    assert sum(1 for i in adapter.evaluation_terms()) == 0
+
+    test_entry = {
+        'internal_id': 'test-entry',
+        'name': 'test entry',
+        'description': 'an entry to test the import',
+        'term_category': 'dismissal_term',
+        'custom_term_one': 'foo',
+        'custom_term_two': 'bar',
+    }
+    load_evaluation_term(adapter, **test_entry)
+    # test if loaded
+    assert sum(1 for i in adapter.evaluation_terms()) == 1
+    # test assignment of data
+    entry = adapter.get_evaluation_term('dismissal_term', term_id='test-entry')
+    assert 'last_modified' in entry
+    assert all(test_key in entry.keys() for test_key in test_entry)
+
+
+def test_load_evaluation_terms_rank_increment(adapter):
+    # Assert empty database
+    assert sum(1 for i in adapter.evaluation_terms()) == 0
+
+    test_entry_one = {
+        'internal_id': 'test-entry-one',
+        'name': 'test entry',
+        'description': 'an entry to test the import',
+        'term_category': 'dismissal_term',
+        'rank': 0,
+    }
+    test_entry_two = {
+        'internal_id': 'test-entry-two',
+        'name': 'test entry',
+        'description': 'an entry to test the import',
+        'term_category': 'dismissal_term',
+    }
+    load_evaluation_term(adapter, **test_entry_one)
+    load_evaluation_term(adapter, **test_entry_two)
+    # test assignment of data
+    entry = adapter.get_evaluation_term('dismissal_term', term_id='test-entry-two')
+    assert entry['rank'] == 1
