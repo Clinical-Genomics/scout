270e41b175c2c69cb4b911ed56e94c675a706011
Chiara Rasi <rasi.chiara@gmail.com>
2021-05-28 10:47:15 +0200

    Fix total vars count and update the count any time new variants are loaded (#2669)
    * stricter check in count variants
    
    * update variants stats if events trigger the upload of variants
    
    * fix a typo and codefactor thing
    
    * updated changelog
    
    * fix code
    
    * rever CSS style
    
    * revert variants controllers
    
    * Update scout/load/all.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/update/case.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/load/variants.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/load/research.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Fix code style issues with Black
    
    * Update scout/adapter/mongo/variant.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Fix code style issues with Black
    
    * renamed a variant according to review
    
    * revert to safer code
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- scout/server/blueprints/variants/views.py
+++ scout/server/blueprints/variants/views.py
@@ -49,7 +49,7 @@ def variants(institute_id, case_name):
     category = "snv"
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     variant_type = request.args.get("variant_type", "clinical")
-    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, False)
+    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, variant_type, False)
 
     if request.form.get("hpo_clinical_filter"):
         case_obj["hpo_clinical_filter"] = True
@@ -152,7 +152,7 @@ def str_variants(institute_id, case_name):
     category = "str"
 
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
-    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, False)
+    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, variant_type, False)
 
     user_obj = store.user(current_user.email)
 
@@ -230,7 +230,7 @@ def sv_variants(institute_id, case_name):
     category = "sv"
     # Define case and institute objects
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
-    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, False)
+    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, variant_type, False)
 
     if request.form.get("hpo_clinical_filter"):
         case_obj["hpo_clinical_filter"] = True
@@ -300,7 +300,7 @@ def cancer_variants(institute_id, case_name):
     category = "cancer"
     variant_type = request.args.get("variant_type", "clinical")
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
-    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, False)
+    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, variant_type, False)
 
     user_obj = store.user(current_user.email)
     if request.method == "POST":
@@ -405,7 +405,7 @@ def cancer_sv_variants(institute_id, case_name):
     category = "cancer_sv"
     # Define case and institute objects
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
-    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, False)
+    variants_stats = store.case_variants_count(case_obj["_id"], institute_id, variant_type, False)
 
     if request.form.get("hpo_clinical_filter"):
         case_obj["hpo_clinical_filter"] = True
