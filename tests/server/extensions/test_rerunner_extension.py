7ae900ac6cf715b657f57cb352abe38d4eb43e42
northwestwitch <rasi.chiara@gmail.com>
2021-06-01 08:29:36 +0200

    move create_app to fixtures

--- tests/server/extensions/test_rerunner_extension.py
+++ tests/server/extensions/test_rerunner_extension.py
@@ -1,27 +1,14 @@
 """Tests for the Case Rerunner extension"""
 
-from scout.server.app import create_app
 from scout.server.extensions import rerunner
 
 
-def test_rerunner_connection_settings():
+def test_rerunner_connection_settings(rerunner_app):
     """Test that the app is initialized with the Rerunner settings"""
 
     host = "test_rerunner_url"
     port = 9000
-    timeout = 10
     key = "test_key"
-
-    # WHEN app is created
-    test_app = create_app(
-        config=dict(
-            TESTING=True,
-            RERUNNER_HOST=host,
-            RERUNNER_PORT=port,
-            RERUNNER_TIMEOUT=timeout,
-            RERUNNER_API_KEY=key,
-        )
-    )
 
     assert rerunner.connection_settings["host"] == f"{host}:{port}"
     assert rerunner.connection_settings["api_key"] == key
