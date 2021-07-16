6d2633cdb9aa68e84d89bd97b203b4f4dd515fbf
elevu <e.m.container@gmail.com>
2021-07-16 15:53:23 +0200

    Switch to antd in CasesTable

--- src/components/CasesTable/CasesTable.tsx
+++ src/components/CasesTable/CasesTable.tsx
@@ -1,56 +1,53 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import Table from '@material-ui/core/Table'
-import TableBody from '@material-ui/core/TableBody'
-import TableCell from '@material-ui/core/TableCell'
-import TableContainer from '@material-ui/core/TableContainer'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
-import Paper from '@material-ui/core/Paper'
-import { Chip } from '@material-ui/core'
-
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-})
+import { Space, Table, Tag } from 'antd'
 
 interface CasesTableProps {
   cases: any[]
 }
 
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'display_name',
+    key: 'display_name',
+    render: (name: string) => <a href={`${window.location}/${name}`}>{name}</a>,
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: 'analysis date',
+    dataIndex: 'analysis_date',
+    key: 'analysis_date',
+    render: (date: string) => <div>{new Date(date).toISOString().slice(0, 10)}</div>,
+  },
+  {
+    title: 'Link',
+    key: 'tags',
+    dataIndex: 'tags',
+  },
+  {
+    title: 'Sequencing',
+    dataIndex: 'analysis_types',
+    key: 'analysis_types',
+    render: (analysisTypes: string[]) => (
+      <div>
+        {analysisTypes.map((analysisType) => (
+          <Tag color={'blue'} key={analysisType}>
+            {analysisType}
+          </Tag>
+        ))}
+      </div>
+    ),
+  },
+  {
+    title: 'Track',
+    key: 'track',
+  },
+]
+
 export const CasesTable = ({ cases }: CasesTableProps) => {
-  const classes = useStyles()
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="cases table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Cases</TableCell>
-            <TableCell align="center">Status</TableCell>
-            <TableCell align="center">Analysis date</TableCell>
-            <TableCell align="center">Link</TableCell>
-            <TableCell align="center">Sequencing</TableCell>
-            <TableCell align="center">Track</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {cases.map((scoutCase) => (
-            <TableRow key={scoutCase.display_name}>
-              <TableCell align="center">{scoutCase.display_name}</TableCell>
-              <TableCell align="center">{scoutCase.status}</TableCell>
-              <TableCell align="center">
-                {new Date(scoutCase.analysis_date).toISOString().slice(0, 10)}
-              </TableCell>
-              <TableCell align="center">{scoutCase.link}</TableCell>
-              <TableCell align="center">{scoutCase.analysis_types.toString()}</TableCell>
-              <TableCell align="center">
-                <Chip label={scoutCase.track} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  )
+  return <Table columns={columns} dataSource={cases} pagination={false} rowKey={'display_name'} />
 }
