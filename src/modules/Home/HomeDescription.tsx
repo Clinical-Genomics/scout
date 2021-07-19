97815cceb45b7aa1f9c14d4213d56f409049fc64
elevu <e.m.container@gmail.com>
2021-07-19 16:17:24 +0200

    Remove material-ui from Home Description

--- src/modules/Home/HomeDescription.tsx
+++ src/modules/Home/HomeDescription.tsx
@@ -1,9 +1,9 @@
+import { Typography } from 'antd'
 import React from 'react'
-import Typography from '@material-ui/core/Typography'
 import styles from './HomeDescription.module.css'
 
 export const HomeDescription = () => (
-  <Typography variant="body1" component="p" className={styles.body}>
+  <Typography className={styles.body}>
     Scout allows you to browse VCFs in a web browser, identify compound pairs, and solve cases as a
     team.
   </Typography>
