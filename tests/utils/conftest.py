7c599634b1cc44a50378b6c308a1f6f6ac257ce8
mikaell <mikael.laaksonen@scilifelab.se>
2021-07-28 16:05:37 +0200

    Now Correct File Commited

--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -60,29 +60,29 @@ def refseq_response_non_existing():
 @pytest.fixture(name="ensembl_biomart_xml_query")
 def fixture_ensembl_biomart_xml_query():
     """Fixture for query ensembl biomart with xml"""
-    _xml = """<?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE Query>
+    _xml = '<?xml version="1.0" encoding="UTF-8"?>\
+    <!DOCTYPE Query>\
     <Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0"\
-    count = "" datasetConfigVersion = "0.6" completionStamp = "1">
-        <Dataset name = "hsapiens_gene_ensembl" interface = "default" >
-            <Filter name = "ensembl_gene_id" value = "ENSG00000115091"/>
-            <Attribute name = "hgnc_symbol" />
-            <Attribute name = "ensembl_transcript_id" />
-        </Dataset>
-    </Query>
-    """
-    _xmlbin = (
-        b'<?xml version="1.0" encoding="UTF-8"?>'
-        b"<!DOCTYPE Query>"
-        b'<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0"'
-        b'count = "" datasetConfigVersion = "0.6" completionStamp = "1">'
-        b'<Dataset name = "hsapiens_gene_ensembl" interface = "default" >'
-        b'<Filter name = "ensembl_gene_id" value = "ENSG00000115091"/>'
-        b'<Attribute name = "hgnc_symbol" />'
-        b'<Attribute name = "ensembl_transcript_id" />'
-        b"</Dataset>"
-        b"</Query>"
-    )
+    count = "" datasetConfigVersion = "0.6" completionStamp = "1">\
+        <Dataset name = "hsapiens_gene_ensembl" interface = "default" >\
+            <Filter name = "ensembl_gene_id" value = "ENSG00000115091"/>\
+            <Attribute name = "hgnc_symbol" />\
+            <Attribute name = "ensembl_transcript_id" />\
+        </Dataset>\
+    </Query>'
+
+    
+    _xmlbin = (b'<?xml version="1.0" encoding="UTF-8"?>'
+            b'<!DOCTYPE Query>'
+            b'<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0"'
+            b'count = "" datasetConfigVersion = "0.6" completionStamp = "1">'
+            b'<Dataset name = "hsapiens_gene_ensembl" interface = "default" >'
+            b'<Filter name = "ensembl_gene_id" value = "ENSG00000115091"/>'
+            b'<Attribute name = "hgnc_symbol" />'
+            b'<Attribute name = "ensembl_transcript_id" />'
+            b'</Dataset>'
+            b'</Query>'
+)
     return _xml
 
 
