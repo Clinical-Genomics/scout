8c1912a7704de9112bb1692fba630253382f531d
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-07-29 08:27:27 +0200

    fix broken tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -828,6 +828,8 @@ def real_populated_database(request, real_panel_database, parsed_case):
 
     LOG.info("Adding case to real adapter")
     case_obj = build_case(parsed_case, adapter)
+    # adapt custom images
+    case_obj["custom_images"] = parse_custom_images(case_obj)
     adapter._add_case(case_obj)
 
     return adapter
