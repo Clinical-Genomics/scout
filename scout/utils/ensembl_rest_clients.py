0e6b99b85852974a44ba7b2b662a922c652b54ae
mikaell <mikael.laaksonen@scilifelab.se>
2021-07-29 13:09:38 +0200

    Don't Add Newlines in XML Query

--- scout/utils/ensembl_rest_clients.py
+++ scout/utils/ensembl_rest_clients.py
@@ -223,7 +223,7 @@ class EnsemblBiomartClient:
             xml_lines.append("\t\t" + line)
         xml_lines += ["\t</Dataset>", "</Query>"]
 
-        return "\n".join(xml_lines)
+        return "".join(xml_lines)
 
     @staticmethod
     def xml_filters(filters):
