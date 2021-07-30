f5f6436b9be6c4585468a015e00831cd913f4a27
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-07-30 15:06:11 +0200

    iSorted imports

--- scout/server/blueprints/public/views.py
+++ scout/server/blueprints/public/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
+from pathlib import Path
 
 from flask import Blueprint, current_app, render_template, send_from_directory
 from flask_ldap3_login.forms import LDAPLoginForm
 
 from scout import __version__
 from scout.server.utils import public_endpoint
-from pathlib import Path
 
 LOG = logging.getLogger(__name__)
 
