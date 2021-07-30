bc2f6b456b5858daf612fc98cfd433f5a7155a0a
mikaell <mikael.laaksonen@scilifelab.se>
2021-07-30 09:44:19 +0200

    Please Linter

--- scout/server/blueprints/public/views.py
+++ scout/server/blueprints/public/views.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from flask import (
+    abort,
     Blueprint,
     current_app,
     redirect,
     render_template,
     send_from_directory,
-    url_for,
-    abort,
+    url_for
 )
 from flask_ldap3_login.forms import LDAPLoginForm
 
 from scout import __version__
 from scout.server.utils import public_endpoint
-import logging
 
 LOG = logging.getLogger(__name__)
 
