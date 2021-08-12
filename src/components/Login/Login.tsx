49c8d1c6438de449934df26557f01bfaa52a788e
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2021-08-12 16:38:11 +0200

    fixing login bug

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -33,7 +33,6 @@ export const LoginComponent = ({
   resetGoogleToken,
 }: Props) => {
   const clientId = GOOGLE_OAUTH_CLIENT_ID || 'no-id'
-
   const onLoginSuccess = (response: any) => {
     setUserInfo(response.profileObj)
     setGoogleToken(response.tokenId)
@@ -49,34 +48,9 @@ export const LoginComponent = ({
     Cookies.remove('scout_remember_me', { path: '' })
   }
 
-  const loginMenu = (
-    <Menu>
-      <Menu.Item key="1" icon={<GoogleOutlined />}>
-        <GoogleLogin
-          render={(renderProps) => (
-            <button
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-              className="google-login_button"
-            >
-              Login with Google
-            </button>
-          )}
-          clientId={clientId}
-          onSuccess={onLoginSuccess}
-          isSignedIn={true}
-          cookiePolicy="single_host_origin"
-        />
-      </Menu.Item>
-      <Menu.Item key="2" icon={<MailOutlined />} disabled>
-        Login with Email
-      </Menu.Item>
-    </Menu>
-  )
-
   const loginOutMenu = (
     <Menu>
-      <Menu.Item key="1">
+      <Menu.Item>
         <GoogleLogout
           render={(renderProps) => (
             <button
@@ -102,18 +76,35 @@ export const LoginComponent = ({
         {settings.googleToken && (
           <Dropdown overlay={loginOutMenu} placement="bottomLeft">
             <Button>
-              <span className="login-dropdown-header">{`Hi ${settings?.user?.givenName}!`}</span>{' '}
+              <span className="login-dropdown-header">{`Hi ${settings?.user?.givenName}!`}</span>
               <DownOutlined />
             </Button>
           </Dropdown>
         )}
         {/* Login button */}
         {!settings?.googleToken && (
-          <Dropdown overlay={loginMenu} placement="bottomLeft">
-            <Button>
-              Login <DownOutlined />
-            </Button>
-          </Dropdown>
+          <Menu>
+            <Menu.Item icon={<GoogleOutlined />}>
+              <GoogleLogin
+                render={(renderProps) => (
+                  <button
+                    onClick={renderProps.onClick}
+                    disabled={renderProps.disabled}
+                    className="google-login_button"
+                  >
+                    Login with Google
+                  </button>
+                )}
+                clientId={clientId}
+                onSuccess={onLoginSuccess}
+                isSignedIn={true}
+                cookiePolicy="single_host_origin"
+              />
+            </Menu.Item>
+            <Menu.Item icon={<MailOutlined />} disabled>
+              Login with Email
+            </Menu.Item>
+          </Menu>
         )}
       </div>
     </>
