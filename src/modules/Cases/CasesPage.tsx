1906df1c51de34962e9f185bbe34d209ab23a5b0
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2021-08-17 13:39:55 +0200

    Fixing fetch call

--- src/modules/Cases/CasesPage.tsx
+++ src/modules/Cases/CasesPage.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { CasesTable } from 'components/CasesTable/CasesTable'
 import styles from './CasesPage.module.css'
+import { getCases } from 'services/ScoutApi'
 
 export const CasesPage = () => {
   const [cases, setCases] = useState<any>()
 
-  useEffect(() => {
+  const getMockCases = () => {
     fetch('/cases')
       .then((res) => res.json())
       .then((json) => setCases(json.cases[0].getMockCases.cases.cases))
       .catch((err) => console.log(err))
+  }
+  useEffect(() => {
+    getCases().then((response: any) => {
+      setCases(response.cases.cases ? response.cases.cases : getMockCases())
+    })
   }, [])
 
   return (
