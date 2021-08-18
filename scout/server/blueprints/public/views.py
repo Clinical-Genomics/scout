f88f11434840398f5e80484ed6e32c9ccee19741
Chiara Rasi <rasi.chiara@gmail.com>
2021-08-18 11:37:53 +0200

    remove unused import

--- scout/server/blueprints/public/views.py
+++ scout/server/blueprints/public/views.py
@@ -2,7 +2,7 @@
 import logging
 from pathlib import Path
 
-from flask import Blueprint, current_app, flash, render_template, send_from_directory
+from flask import Blueprint, current_app, render_template, send_from_directory
 from flask_ldap3_login.forms import LDAPLoginForm
 
 from scout import __version__
