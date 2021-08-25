e87c2ec98cd45c7f562acb26a02adaf6577a52b9
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-08-25 15:13:41 +0200

    Rename hide alamut (#2818)
    * Actually do move config use to links
    
    * Changelog
    
    * Fix test to use app context
    
    * Fix next test..
    
    * Update docs
    
    Co-authored-by: Chiara Rasi <rasi.chiara@gmail.com>

--- scout/server/config.py
+++ scout/server/config.py
@@ -68,7 +68,7 @@ ACCEPT_LANGUAGES = ["en", "sv"]
 # FEATURE FLAGS
 SHOW_CAUSATIVES = True
 SHOW_OBSERVED_VARIANT_ARCHIVE = True
-SHOW_ALAMUT_LINK = True
+HIDE_ALAMUT_LINK = False
 
 # OMIM API KEY: Required for downloading definitions from OMIM (https://www.omim.org/api)
 # OMIM_API_KEY = 'valid_omim_api_key'
