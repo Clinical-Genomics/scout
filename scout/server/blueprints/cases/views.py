d42d34bee4cdc0dc93cf7c2775c84836c3963c7d
Chiara Rasi <rasi.chiara@gmail.com>
2021-09-10 08:38:19 +0200

    move more conde from view to controller

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -84,7 +84,7 @@ def sma(institute_id, case_name):
     """Visualize case SMA data - SMN CN calls"""
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     data = controllers.case(store, institute_obj, case_obj)
-    return dict(institute=institute_obj, case=case_obj, format="html", **data)
+    return dict(format="html", **data)
 
 
 @cases_bp.route("/beacon_submit", methods=["POST"])
