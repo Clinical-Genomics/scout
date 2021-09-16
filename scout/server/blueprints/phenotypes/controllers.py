01e03df90746103184d46ea5345ddfb20bc5ce84
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-09-16 19:52:27 +0200

    Add pagination to hpo terms api

--- scout/server/blueprints/phenotypes/controllers.py
+++ scout/server/blueprints/phenotypes/controllers.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def hpo_terms(store, query=None):
+def hpo_terms(store, query=None, limit=None, page=None):
     """Retrieves a list of HPO terms from scout database
 
     Args:
@@ -13,8 +13,19 @@ def hpo_terms(store, query=None):
 
     """
     hpo_phenotypes = {}
+
+    skip = None
+    if limit:
+        limit = int(limit)
+
+    if page and limit:
+        page=int(page)
+        if page > 0:
+            skip = (page - 1)*limit
+        else:
+            skip = 0
     if query:
-        hpo_phenotypes["phenotypes"] = list(store.hpo_terms(query=query))
+        hpo_phenotypes["phenotypes"] = list(store.hpo_terms(query=query, limit=limit, skip=skip))
     else:
-        hpo_phenotypes["phenotypes"] = list(store.hpo_terms())
+        hpo_phenotypes["phenotypes"] = list(store.hpo_terms(limit=limit, skip=skip))
     return hpo_phenotypes
