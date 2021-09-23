cc9dcc9abe41d45c1088b55bebc6933e2aa13ef0
Chiara Rasi <rasi.chiara@gmail.com>
2021-09-23 05:50:57 +0200

    fix syntax

--- scout/server/static/scripts/Phenotypes.js
+++ scout/server/static/scripts/Phenotypes.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Phenotypes extends React.Component {
     render() {
         return (
-            <h1>Here comes the contect from React!</h1>
+            <h1>Here comes the content from React!</h1>
         );
     }}
 export default Phenotypes;
