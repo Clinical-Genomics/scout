337e9396e33d6d2dcda03ee201f12373b91b5446
Moe Darrah <41540288+mmdarrah@users.noreply.github.com>
2021-09-24 08:56:13 +0200

    Fix lint errors

--- react-modules/src/components/Loading/Loading.tsx
+++ react-modules/src/components/Loading/Loading.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Spin } from 'antd'
 
 export const Loading = () => {
-  return (
-    <Spin
-      size="large"
-      style={{
-        position: 'fixed',
-        zIndex: 2,
-        top: '50%',
-        left: '50%',
-      }}
-    />
-  )
+	return (
+		<Spin
+			size="large"
+			style={{
+				position: 'fixed',
+				zIndex: 2,
+				top: '50%',
+				left: '50%',
+			}}
+		/>
+	)
 }
--- react-modules/src/components/PhenotypesTable/PhenotypesTable.tsx
+++ react-modules/src/components/PhenotypesTable/PhenotypesTable.tsx
@@ -2,53 +2,53 @@ import React from 'react'
 import { Table } from 'antd'
 
 interface PhenotypesTableProps {
-  phenotypes: any[]
+	phenotypes: any[]
 }
 
 const columns = [
-  {
-    title: 'HPO term',
-    dataIndex: 'hpo_id',
-    key: '_id',
-    sorter: (a: any, b: any) => {
-      if (a.hpo_id && b.hpo_id) {
-        return a.hpo_id.localeCompare(b.hpo_id)
-      }
-      return 0
-    },
-    render: (name: string) => (
-      <a
-        href={`http://hpo.jax.org/app/browse/term/${name}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {name}
-      </a>
-    ),
-  },
-  {
-    title: 'Phenotype description',
-    dataIndex: 'description',
-    key: '_id',
-  },
-  {
-    title: 'Number of associated genes',
-    dataIndex: 'genes',
-    key: '_id',
-    sorter: (a: any, b: any) => a?.genes?.length - b?.genes?.length,
-    render: (genes: string) => <span>{genes.length}</span>,
-  },
+	{
+		title: 'HPO term',
+		dataIndex: 'hpo_id',
+		key: '_id',
+		sorter: (a: any, b: any) => {
+			if (a.hpo_id && b.hpo_id) {
+				return a.hpo_id.localeCompare(b.hpo_id)
+			}
+			return 0
+		},
+		render: (name: string) => (
+			<a
+				href={`http://hpo.jax.org/app/browse/term/${name}`}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				{name}
+			</a>
+		),
+	},
+	{
+		title: 'Phenotype description',
+		dataIndex: 'description',
+		key: '_id',
+	},
+	{
+		title: 'Number of associated genes',
+		dataIndex: 'genes',
+		key: '_id',
+		sorter: (a: any, b: any) => a?.genes?.length - b?.genes?.length,
+		render: (genes: string) => <span>{genes.length}</span>,
+	},
 ]
 
 export const PhenotypesTable = ({ phenotypes }: PhenotypesTableProps) => {
-  return (
-    <Table
-      bordered
-      columns={columns}
-      dataSource={phenotypes}
-      rowKey={'_id'}
-      expandIconColumnIndex={-1}
-      pagination={false}
-    />
-  )
+	return (
+		<Table
+			bordered
+			columns={columns}
+			dataSource={phenotypes}
+			rowKey={'_id'}
+			expandIconColumnIndex={-1}
+			pagination={false}
+		/>
+	)
 }
