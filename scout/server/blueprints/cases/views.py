fb04cad636edf2075504dc63becd7ea138417b00
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-12-03 16:32:30 +0100

    lint

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -802,12 +802,13 @@ def rerun(institute_id, case_name):
 def monitor(institute_id, case_name):
     """Request a case to be rerun."""
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
+    user_obj = store.user(current_user.email)
     link = url_for(".case", institute_id=institute_id, case_name=case_name)
 
     if request.form["rerun_monitoring"]:
-        store.disable_monitoring(institute_obj, case_obj, user_id, link)
+        store.disable_monitoring(institute_obj, case_obj, user_obj, link)
     else:
-        store.enable_monitoring(institute_obj, case_obj, user_id, link)
+        store.enable_monitoring(institute_obj, case_obj, user_obj, link)
 
 
 @cases_bp.route("/<institute_id>/<case_name>/reanalysis", methods=["POST"])
