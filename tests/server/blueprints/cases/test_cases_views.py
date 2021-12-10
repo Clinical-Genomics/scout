2f629fae7a15bada321d1f23e0736790e430d1f2
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-12-10 13:50:33 +0100

    once more, with mock

--- tests/server/blueprints/cases/test_cases_views.py
+++ tests/server/blueprints/cases/test_cases_views.py
@@ -89,8 +89,10 @@ def test_reanalysis(app, institute_obj, case_obj, mocker, mock_redirect):
         assert resp.status_code == 302
 
 
-def test_monitor(app, institute_obj):
+def test_monitor(app, institute_obj, mocker, mock_redirect):
     """test case rerun monitoring function"""
+
+    mocker.patch("scout.server.blueprints.cases.views.redirect", return_value=mock_redirect)
 
     # GIVEN an initialized app
     # GIVEN a valid user
