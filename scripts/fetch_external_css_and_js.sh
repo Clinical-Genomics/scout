e6a4e4a9219096b6b2b4db3e15997aac31e26cdf
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2021-12-20 12:19:35 +0100

    backticks lost in translation..

--- scripts/fetch_external_css_and_js.sh
+++ scripts/fetch_external_css_and_js.sh
@@ -6,9 +6,9 @@
 # CSS
 mkdir scout/server/blueprints/public/static/external
 # create a url list for later reference
-for url in git grep \\.css |grep "src\|href" |grep -v "url_for" |perl -ne 'm/href=\"([^"]+)/; print $1,"\n";'|sort|uniq; do echo $url >> tmp.css_urls; done
+for url in `git grep \\.css` |grep "src\|href" |grep -v "url_for" |perl -ne 'm/href=\"([^"]+)/; print $1,"\n";'|sort|uniq; do echo $url >> tmp.css_urls; done
 # retrieve external css files with http(s)
-for url in git grep \\.css |grep "src\|href" |grep -v "url_for" |perl -ne 'm/href=\"([^"]+)/; print $1,"\n";'|sort|uniq; do wget -P scout/server/blueprints/public/static/ $url; done
+for url in `git grep \\.css` |grep "src\|href" |grep -v "url_for" |perl -ne 'm/href=\"([^"]+)/; print $1,"\n";'|sort|uniq; do wget -P scout/server/blueprints/public/static/ $url; done
 # list files that need url changes
 for file in scout/server/blueprints/public/static/external/*css ; do git grep `basename $file`; done |grep -v demo |cut -f 1 -d\: |sort|uniq> tmp.css_files
 # construct a substitution perl script to change the urls to point to url_for('public.static', filename)
@@ -20,9 +20,9 @@ for file in `cat tmp.css_files`; do perl -n tmp.css_patterns < $file > ${file}.e
 # JS
 
 # create a url list for later reference
-for url in `git grep \\.js |grep "src" |grep -v "url_for\|demo" |perl -ne 'm/src=\"([^"]+)/; print $1,"\n";'|sort|uniq`;do echo $url >> tmp.js_urls; done
+for url in `git grep \\.js` |grep "src" |grep -v "url_for\|demo" |perl -ne 'm/src=\"([^"]+)/; print $1,"\n";'|sort|uniq`;do echo $url >> tmp.js_urls; done
 # retrieve external js files with http(s)
-for url in git grep \\.js |grep "src\|href" |grep -v "demo\|url_for" |perl -ne 'm/src=\"([^"]+)/; print $1,"\n";'|sort|uniq ; do wget -P scout/server/blueprints/public/static/ $url; done
+for url in `git grep \\.js` |grep "src\|href" |grep -v "demo\|url_for" |perl -ne 'm/src=\"([^"]+)/; print $1,"\n";'|sort|uniq ; do wget -P scout/server/blueprints/public/static/ $url; done
 # list files that need url changes
 for file in scout/server/blueprints/public/static/external/*js ; do git grep `basename $file`; done |grep -v demo |cut -f 1 -d\: |sort|uniq > tmp.js_files
 # construct a substitution perl script to change the urls to point to url_for('public.static', filename)
