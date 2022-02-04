23ad15a08f52ba38b43e41ac3bd228e82e49184e
Chiara Rasi <rasi.chiara@gmail.com>
2022-02-04 13:00:41 +0100

    Update scout/server/blueprints/cases/views.py
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -847,7 +847,7 @@ def reset_research(institute_id, case_name):
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     user_obj = store.user(current_user.email)
     link = url_for(".case", institute_id=institute_id, case_name=case_name)
-    store.close_research(institute_obj, case_obj, user_obj, link)
+    store.reset_research(institute_obj, case_obj, user_obj, link)
     return redirect(request.referrer)
 
 
