8368636f606b13c06d71ac99d34335120080213f
Chiara Rasi <rasi.chiara@gmail.com>
2022-02-08 10:05:56 +0100

    1.5cm margin top

--- scout/server/utils.py
+++ scout/server/utils.py
@@ -15,7 +15,7 @@ from flask_login import current_user
 LOG = logging.getLogger(__name__)
 
 
-def html_to_pdf_file(html_string, orientation, dpi=96, margins=["2cm", "1cm", "1cm", "1cm"]):
+def html_to_pdf_file(html_string, orientation, dpi=96, margins=["1.5cm", "1cm", "1cm", "1cm"]):
     """Creates a pdf file from the content of an HTML file
     Args:
         html_string(string): an HTML string to be rendered as PDF
