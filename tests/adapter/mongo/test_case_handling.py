78b40f4d675f2a86df059a07105c197def5085cc
Chiara Rasi <rasi.chiara@gmail.com>
2022-03-07 13:49:36 +0100

    Added a test

--- tests/adapter/mongo/test_case_handling.py
+++ tests/adapter/mongo/test_case_handling.py
@@ -595,6 +595,25 @@ def test_update_case_rerun_status(adapter, case_obj, institute_obj, user_obj):
     assert res["status"] == "inactive"
 
 
+def test_cases_by_diagnosis(adapter, case_obj, omim_term):
+    """Test filtering cases by assigned OMIM terms"""
+
+    # GIVEN a case with a diagnosis:
+    case_obj["diagnosis_phenotypes"] = {
+        "disease_nr": omim_term["disease_nr"],
+        "disease_id": omim_term["disease_id"],
+        "description": omim_term["description"],
+    }
+    adapter.case_collection.insert_one(case_obj)
+    # WHEN cases are filtered using OMIM terms containing that term
+    name_query = f"exact_dia:{omim_term['disease_id']},OMIM:000001"
+
+    # WHEN querying for active cases
+    cases = adapter.cases(collaborator=case_obj["owner"], name_query=name_query)
+    # THEN a case should be returned
+    assert sum(1 for i in cases) == 1
+
+
 def test_cases_by_phenotype(hpo_database, test_hpo_terms, case_obj):
     adapter = hpo_database
 
