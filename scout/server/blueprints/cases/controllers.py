e7e96351c565766accc67e69a603f8998090d6a4
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2022-03-16 14:58:14 +0100

    Fix #3243 - IGV overview link from case page

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -41,7 +41,7 @@ from scout.parse.matchmaker import genomic_features, hpo_terms, omim_terms, pars
 from scout.server.blueprints.variant.controllers import variant as variant_decorator
 from scout.server.blueprints.variants.controllers import get_manual_assessments
 from scout.server.extensions import RerunnerError, gens, matchmaker, rerunner, store
-from scout.server.utils import institute_and_case
+from scout.server.utils import case_append_alignments, institute_and_case
 from scout.utils.scout_requests import delete_request_json, post_request_json
 
 LOG = logging.getLogger(__name__)
@@ -134,6 +134,9 @@ def case(store, institute_obj, case_obj):
 
     case_obj["assignees"] = [store.user(user_email) for user_email in case_obj.get("assignees", [])]
 
+    # case bam_files for quick access to alignment view.
+    case_append_alignments(case_obj)
+
     # Fetch ids for grouped cases
     case_groups = {}
     case_group_label = {}
@@ -141,6 +144,8 @@ def case(store, institute_obj, case_obj):
         for group in case_obj.get("group"):
             case_groups[group] = list(store.cases(group=group))
             case_group_label[group] = store.case_group_label(group)
+            for grouped_case in case_groups[group]:
+                case_append_alignments(grouped_case)
 
     # Fetch the variant objects for suspects and causatives
     suspects = [
