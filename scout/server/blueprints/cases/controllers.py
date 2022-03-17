4cc877775f0da6a5ad891b474cd16b7c1b8de04c
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2022-03-17 12:07:09 +0100

    viewer agnostic

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -102,9 +102,9 @@ def coverage_report_contents(base_url, institute_obj, case_obj):
     return html_body_content
 
 
-def populate_case_group(store, case_obj, case_groups, case_group_label):
+def _populate_case_group(store, case_obj, case_groups, case_group_label):
     """Case groups allow display of information about user linked cases together on one case.
-    Notably variantS lists show shared annotations and IGV views show all alignments
+    Notably variantS lists show shared annotations and alignment views show all alignments
     available for the group.
 
     Each case may belong to any of a list of groups. Groups have human readable labels
@@ -162,7 +162,7 @@ def case(store, institute_obj, case_obj):
 
     case_groups = {}
     case_group_label = {}
-    populate_case_groups(store, case_obj, case_groups, case_group_label)
+    _populate_case_groups(store, case_obj, case_groups, case_group_label)
 
     # Fetch the variant objects for suspects and causatives
     suspects = [
