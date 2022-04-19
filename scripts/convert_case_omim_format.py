08b3219fa3d5f8b77b4e305e63fad0ee0e9b0fd9
Chiara Rasi <rasi.chiara@gmail.com>
2022-04-19 14:55:27 +0200

    Added the --fix option

--- scripts/convert_case_omim_format.py
+++ scripts/convert_case_omim_format.py
@@ -19,11 +19,11 @@ SELECT_FIELDS = {
 
 @click.command()
 @click.option("--db_uri", required=True, help="mongodb://user:password@db_url:db_port/db_name")
-@click.option("--test", help="Use this flag to test the function", is_flag=True)
-def omim_case_fix_format(db_uri, test):
+@click.option("--fix", help="Use this flag to fix the OMIM format in old cases", is_flag=True)
+def omim_case_fix_format(db_uri, fix):
     try:
         db_name = db_uri.split("/")[-1]  # get database name from connection string
-        client = MongoClient(db_uri, test)
+        client = MongoClient(db_uri)
         db = client[db_name]
         # test connection
         click.echo("database connection info:{}".format(db))
@@ -33,13 +33,36 @@ def omim_case_fix_format(db_uri, test):
 
         # Display cases with old format of diagnosis (a list of integers)
         cases_with_old_dia = [
-            case for case in cases_with_dia if isinstance(case["diagnosis_phenotypes"], int)
+            case for case in cases_with_dia if isinstance(case["diagnosis_phenotypes"][0], int)
         ]
         click.echo(f"Total number of cases with old diagnosis format:{len(cases_with_old_dia)}")
 
         for i, case in enumerate(cases_with_old_dia):
+            old_dia = case["diagnosis_phenotypes"]
+            new_dia = []
+
+            for dia_nr in old_dia:
+                disease_term = db.disease_term.find_one({"disease_nr": dia_nr})
+                if disease_term is None:
+                    click.echo(f"Could not find a disease term with id:{dia_nr}")
+                    continue
+                new_dia.append(
+                    {
+                        "disease_nr": dia_nr,
+                        "disease_id": disease_term["disease_id"],
+                        "description": disease_term["description"],
+                    }
+                )
+
+            if fix is False:
+                new_dia = old_dia
+            else:
+                db.case.find_one_and_update(
+                    {"_id": case["_id"]}, {"$set": {"diagnosis_phenotypes": new_dia}}
+                )
+
             click.echo(
-                f"\nn:{i}\t{case['owner']}\t{case['display_name']}:{case['diagnosis_phenotypes']}"
+                f"\nn:{i}\t{case['owner']}\t{case['display_name']}\told dia:{old_dia}--->new dia:{new_dia}"
             )
 
     except Exception as err:
