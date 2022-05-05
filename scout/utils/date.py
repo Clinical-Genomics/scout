aec8070d360f15c069974d65b8999d4aafdda84b
mikaell <mikael.laaksonen@scilifelab.se>
2022-05-05 09:36:53 +0200

    Add Datetime Prettyprint

--- scout/utils/date.py
+++ scout/utils/date.py
@@ -50,3 +50,54 @@ def get_date(date, date_format=None):
                 raise ValueError("Date %s is invalid" % date)
 
     return date_obj
+
+
+def pretty_date(time=False):
+    """Given time as datetime() or integer, return the time passed from now
+    as a string in human readable format"""
+
+    def more_than_a_day(days):
+        """Return a string for passed time more than a day"""
+        if days == 1:
+            return "Yesterday"
+        if days < 7:
+            return str(days) + " days ago"
+        if days == 7:
+            return str(days // 7) + " week ago"
+        if days < 31:
+            return str(days // 7) + " weeks ago"
+        if days < 365:
+            return str(days // 30) + " months ago"
+        return str(days // 365) + " years ago"
+
+    def within_24_h(seconds):
+        """Return a string for passed time is less than a day"""
+        if seconds < 10:
+            return "just now"
+        if seconds < 60:
+            return str(seconds) + " seconds ago"
+        if seconds < 120:
+            return "a minute ago"
+        if seconds < 3600:
+            return str(seconds // 60) + " minutes ago"
+        if seconds < 7200:
+            return "an hour ago"
+        if seconds < 86400:
+            return str(seconds // 3600) + " hours ago"
+
+    (diff_seconds, diff_days) = get_time_diff(time)
+    if diff_days == 0:
+        return within_24_h(diff_seconds)
+    return more_than_a_day(diff_days)
+
+
+def get_time_diff(time):
+    """Get diff from now() minus time."""
+    now = datetime.datetime.now()
+    if type(time) is int:
+        diff = now - datetime.datetime.fromtimestamp(time)
+    elif isinstance(time, datetime.datetime):
+        diff = now - time
+    diff_seconds = diff.seconds
+    diff_days = diff.days
+    return (diff_seconds, diff_days)
