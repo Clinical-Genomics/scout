dc5860b6dea0bafbbbfc48e96d42292dc99f0be7
mikaell <mikael.laaksonen@scilifelab.se>
2022-05-09 13:46:40 +0200

    Update Tests

--- tests/server/blueprints/public/test_public_controllers.py
+++ tests/server/blueprints/public/test_public_controllers.py
@@ -1,75 +1,68 @@
 # -*- coding: utf-8 -*-
+import pymongo
+
+import datetime
 from flask import url_for
 
 from scout import __version__
 from scout.server.blueprints.public.controllers import (
     events_to_string,
-    get_important_events,
-    sum_occurrences,
-    verb_index,
+    get_compact_events,
+    CompactEvent
 )
 
+def test_get_compact_events(real_variant_database, institute_obj, case_obj, user_obj, variant_obj):
+    # GIVEN A event database with two events
+    adapter = real_variant_database
 
-def test_verb_index():
-    # GIVEN a subset of VERBS_MACRO
-    verbs_map = {
-        "mark_causative": "marked causative for",
-        "update_diagnosis": "updated diagnosis for",
-        "rerun": "requested rerun of",
-        "status": "updated the status for",
-        "comment": "commented on",
-        "comment_update": "updated a comment for",
-        "pin": "pinned variant",
-        "assign": "was assigned to",
-    }
+    adapter.comment(
+        institute=institute_obj,
+        case=case_obj,
+        user=user_obj,
+        link="variant_link",
+        variant=variant_obj,
+        content="Hello, locally",
+        comment_level="specific",
+    )
 
-    # THEN calling `verb_index` will return the correct index
-    assert verb_index(("assign", "sadf")) == 7
-    assert verb_index(("status", "sadf")) == 3
+    # and a global comment
+    adapter.comment(
+        institute=institute_obj,
+        case=case_obj,
+        user=user_obj,
+        link="variant_link",
+        variant=variant_obj,
+        content="Hello, globally",
+        comment_level="global",
+    )
 
+    # WHEN quering the database for all events and calling `get_compact_events()`
+    query = dict()
+    query["user_id"] = "john@doe.com"
+    query["case"] = "internal_id"
+    events_list = list(adapter.event_collection.find(query).sort("updated_at", pymongo.DESCENDING))
+    compact_events = get_compact_events(events_list)
 
-def test_sum_occurences():
-    # GIVEN list of events
-    test_list = [
-        ("pin", "variant"),
-        ("pin", "variant"),
-        ("pin", "variant"),
-        ("pin", "case"),
-        ("assign", "variant"),
-    ]
-    # THEN calling `sum_occurrences()` will return a dict
-    # where the keys are events and values the number of occurences
-    # of each event respectively
-    sums = sum_occurrences(test_list)
-    assert sums[("pin", "variant")] == 3
-    assert sums[("pin", "case")] == 1
-
-
-def test_get_important_events():
-    # GIVEN a dict where keys are verb-tuples
-    test = {
-        ("pin", "case"): 1,
-        ("assign", "variant"): 1,
-        ("pin", "variant"): 3,
-    }
-
-    # WHEN calling `get_important_events(...)`
-    importants = get_important_events(test)
-
-    # THEN the result is ordered according to VERBS_MAP
-    (key, _value) = importants[0]
-    print(importants)
-    assert list(test)[0] == key
+    # THEN the returned event will behave as a simulated CompactEvent()
+    test_a = CompactEvent("comment", "variant", datetime.datetime.now())
+    test_a.increment()
+    assert compact_events[0] == test_a
 
 
 def test_events_to_string():
-    # GIVEN a list of test tuples
-    test = [(("assign", "variant"), 1), (("pin", "variant"), 3), (("pin", "case"), 1)]
+    # GIVEN a list fo CompactEvents. Where one is incremented.
+    # Note that date attribute is ignored when testing
+    test_a = CompactEvent("assign", "variant", 1)
+    test_b = CompactEvent("pin", "variant", 1)
+    test_c = CompactEvent("pin", "case", 1)
+    test_c.increment()
+    test_c.increment()
+    test = [test_a, test_b, test_c]
 
     # THEN expanding the test list into sentences via the
     # VERBS_MAP macro, the correct and enumerated events are
     # found
     events_string = events_to_string(test)
     assert "Assigned 1 variant" in events_string
-    assert "Pinned 3 variants" in events_string
-    assert "Pinned 1 case" in events_string
+    assert "Pinned 1 variant" in events_string
+    assert "Pinned 3 cases" in events_string
