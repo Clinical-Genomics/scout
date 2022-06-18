07d4eceacca9e39cb4084e800fcddb0fac935937
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2022-06-18 16:14:47 +0200

    cancer variants as well

--- scout/adapter/mongo/event.py
+++ scout/adapter/mongo/event.py
@@ -4,8 +4,6 @@ from datetime import datetime
 import pymongo
 from bson import ObjectId
 
-from scout.constants import CASE_STATUSES, REV_ACMG_MAP
-
 from .case_events import CaseEventHandler
 from .variant_events import VariantEventHandler
 
