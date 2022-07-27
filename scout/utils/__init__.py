4a065316e2889a2b5397ecd6c5c4368ce0919022
mikaell <mikael.laaksonen@scilifelab.se>
2022-07-27 08:14:24 +0200

    Add Missing Fun

--- scout/utils/__init__.py
+++ scout/utils/__init__.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import logging
+LOG = logging.getLogger(__name__)
+
+def call_safe(fun, unknown_var):
+     """Call unknown_var using fun, return None if exception is caught.
+     Args: unknown_var: Object
+           fun: Function
+     Returns: Object"""
+     try:
+         return fun(unknown_var)
+     except ValueError:
+          print(ValueError)
+          return None
+     except TypeError as _er:
+          LOG.error("TypeError {} {}. {}".format(fun, unknown_var, _er))
+          return None
