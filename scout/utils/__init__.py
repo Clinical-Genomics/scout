b4206f2bcbae49f0cf235d6df4b6a641eab35dc2
mikaell <mikael.laaksonen@scilifelab.se>
2022-08-16 13:50:25 +0200

    Move Function

--- scout/utils/__init__.py
+++ scout/utils/__init__.py
@@ -1,14 +1,3 @@
 # -*- coding: utf-8 -*-
 
 
-def call_safe(fun, unknown_var):
-    """Call unknown_var using fun, return None if exception is caught.
-    Args: unknown_var: Object
-          fun: Function
-    Returns: Object"""
-    try:
-        return fun(unknown_var)
-    except ValueError:
-        return None
-    except TypeError:
-        return None
