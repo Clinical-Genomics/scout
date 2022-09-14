7814f9426a551858585f029b0f6393894505f48f
Chiara Rasi <rasi.chiara@gmail.com>
2022-09-14 13:06:33 +0200

    Revert the header of the 2 cgv files

--- scout/constants/clinvar.py
+++ scout/constants/clinvar.py
@@ -1,4 +1,7 @@
-# clinvar Variant sheet
+ASSERTION_METHOD = "ACMG Guidelines, 2015"
+ASSERTION_METHOD_CIT = "PMID:25741868"
+
+# Header used to create the Variant CSV file for the manual ClinVar submission
 CLINVAR_HEADER = {
     "local_id": "##Local ID",
     "linking_id": "Linking ID",
@@ -13,6 +16,8 @@ CLINVAR_HEADER = {
     "var_type": "Variant type",
     "ncopy": "Copy number",
     "ref_copy": "Reference copy number",
+    "breakpoint1": "Breakpoint 1",
+    "breakpoint2": "Breakpoint 2",
     "outer_start": "Outer start",
     "inner_start": "Inner start",
     "inner_stop": "Inner stop",
@@ -30,19 +35,41 @@ CLINVAR_HEADER = {
     "inheritance_mode": "Mode of inheritance",
     "clinsig_cit": "Clinical significance citations",
     "clinsig_comment": "Comment on clinical significance",
+    "drug_response": "Drug response condition",
+    "funct_conseq": "Functional consequence",
+    "funct_conseq_comment": "Comment on functional consequence",
 }
 
-# clinvar CaseData sheet
+# Header used to create the CaseData CSV file for the manual ClinVar submission
 CASEDATA_HEADER = {
     "linking_id": "Linking ID",
     "individual_id": "Individual ID",
     "collection_method": "Collection method",  # default = 'clinical testing'
     "allele_origin": "Allele origin",
     "is_affected": "Affected status",
+    "sv_analysis_method": "Structural variant method/analysis type",
+    "clin_features": "Clinical features",
+    "tissue": "Tissue",
+    "sex": "Sex",
+    "age": "Age",
+    "ethnicity": "Population Group/Ethnicity",
+    "fam_history": "Family history",
+    "is_proband": "Proband",
+    "is_secondary_finding": "Secondary finding",
+    "is_mosaic": "Mosaicism",
+    "zygosity": "Zygosity",
+    "co_occurr_gene": "Co-occurrences, same gene",
+    "co_occurr_other": "Co-occurrence, other genes",
+    "platform_type": "Platform type",
+    "platform_name": "Platform name",
+    "method": "Method",
+    "method_purpose": "Method purpose",
+    "method_cit": "Method citations",
+    "testing_lab": "Testing laboratory",
+    "reported_at": "Date variant was reported to submitter",
 }
-# silence fields in ClinVar CSV output if other fields exist to avoid validation error - the latter are preferred
-CLINVAR_SILENCE_IF_EXISTS = {"chromosome": "hgvs", "start": "hgvs", "stop": "hgvs"}
 
+# The following items are defined in the ClinVar API specifications: https://www.ncbi.nlm.nih.gov/clinvar/docs/api_http/
 CLNSIG_TERMS = {
     "Pathogenic",
     "Likely pathogenic",
@@ -75,8 +102,6 @@ REVSTAT_TERMS = {
     "reviewed_by_expert_panel",
 }
 
-# Models of inheritance are defined here:
-# https://ftp.ncbi.nlm.nih.gov/pub/GTR/standard_terms/Mode_of_inheritance.txt
 CLINVAR_INHERITANCE_MODELS = [
     "Autosomal dominant inheritance",
     "Autosomal recessive inheritance",
@@ -100,8 +125,6 @@ CLINVAR_INHERITANCE_MODELS = [
     "Oligogenic inheritance",
 ]
 
-ASSERTION_METHOD = "ACMG Guidelines, 2015"
-ASSERTION_METHOD_CIT = "PMID:25741868"
 SV_TYPES = [
     "Insertion",
     "Deletion",
@@ -113,7 +136,9 @@ SV_TYPES = [
     "Translocation",
     "Complex",
 ]
+
 AFFECTED_STATUS = ["yes", "no", "unknown", "not provided", "not applicable"]
+
 ALLELE_OF_ORIGIN = [
     "germline",
     "somatic",
@@ -125,6 +150,7 @@ ALLELE_OF_ORIGIN = [
     "biparental",
     "not applicable",
 ]
+
 COLLECTION_METHOD = [
     "curation",
     "literature only",
