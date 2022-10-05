d9bfe40d796bb2f4aace11cea77fecae28bfb266
Chiara Rasi <rasi.chiara@gmail.com>
2022-10-05 10:38:11 +0200

    fix other test

--- tests/server/blueprints/institutes/test_institute_controllers.py
+++ tests/server/blueprints/institutes/test_institute_controllers.py
@@ -14,7 +14,7 @@ def test_phenomodel_checkgroups_filter(app, institute_obj, hpo_checkboxes, omim_
     # GIVEN a phenotype model with one panel and 3 checkboxes (checkbox2 nested inside checkbox1)
     hpo_id1 = hpo_checkboxes[0]["_id"]
     hpo_id2 = hpo_checkboxes[1]["_id"]
-    omim_id = omim_checkbox["_id"]
+    omim_id = omim_checkbox["disease_id"]
     checkbox2 = dict(
         name=hpo_id2,
         description=hpo_checkboxes[1]["description"],
