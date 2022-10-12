af85da5fe3336f97e0ef942a872c4da69d7eead4
Chiara Rasi <rasi.chiara@gmail.com>
2022-10-12 08:05:46 +0200

    Add a comment

--- scout/adapter/mongo/institute.py
+++ scout/adapter/mongo/institute.py
@@ -185,7 +185,7 @@ class InstituteHandler(object):
         safe_genes = []
         institute_obj = self.institute(institute_id)
         if not institute_obj:
-            return safe_genes
+            return safe_genes  # return an empty list
         for panel_name in institute_obj.get("gene_panels_matching", {}).keys():
             safe_genes += self.panel_to_genes(panel_name=panel_name, gene_format="hgnc_id")
         return safe_genes
