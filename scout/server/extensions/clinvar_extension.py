c85101b05e0aae1972e3dd198e5355e8902b8764
Chiara Rasi <rasi.chiara@gmail.com>
2022-11-02 08:53:09 +0100

    Work in progress

---
+++ scout/server/extensions/clinvar_extension.py
@@ -0,0 +1,11 @@
+SERVICE_URL = "https://preclinvar-stage.scilifelab.se/"
+
+
+class ClinVarApi:
+    """This class contains the code to covert CSV-based ClinVar submissions to json data
+    using the preClinVar service: https://preclinvar-stage.scilifelab.se/docs. Json data is validated against
+    the ClinVar submission schema (https://www.ncbi.nlm.nih.gov/clinvar/docs/api_http/) and if valid can be directly submitted to ClinVar.
+    """
+
+    def __init__(self):
+        self.service = SERVICE_URL
