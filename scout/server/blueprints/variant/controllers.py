66bcd8589fbf350c0eefd9d6a0d17c5872bc542d
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2022-12-08 16:47:15 +0100

    precedence

--- scout/server/blueprints/variant/controllers.py
+++ scout/server/blueprints/variant/controllers.py
@@ -582,9 +582,11 @@ def str_variant_reviewer(
         srs_query_data = {
             "reads": ind_reviewer.get("alignment"),
             "reads_index": ind_reviewer.get("alignment_index")
-            or f"{ind_reviewer.get('alignment').bai}"
-            if os.path.exists(ind_reviewer.get("alignment") + ".bai")
-            else None,
+            or (
+                f"{ind_reviewer.get('alignment').bai}"
+                if os.path.exists(ind_reviewer.get("alignment") + ".bai")
+                else None
+            ),
             "vcf": ind_reviewer.get("vcf"),
             "catalog": ind_reviewer.get("catalog"),
             "locus": str_repid,
