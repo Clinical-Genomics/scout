0e1f74af11cd18542bfe9aa60a3e45126ecd3228
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-10 12:10:11 +0100

    Simoplify some more

--- scout/resources/__init__.py
+++ scout/resources/__init__.py
@@ -2,9 +2,11 @@ import importlib_resources
 
 ###### Paths ######
 
+BASE_PATH = importlib_resources.files("scout")
+
 # Cytoband path
-cytobands_37_path = importlib_resources.files("scout") / "resources/cytoBand_hg19.txt.gz"
-cytobands_38_path = importlib_resources.files("scout") / "resources/cytoBand_hg38.txt.gz"
+cytobands_37_path = BASE_PATH / "resources/cytoBand_hg19.txt.gz"
+cytobands_38_path = BASE_PATH / "resources/cytoBand_hg38.txt.gz"
 
 cytoband_files = {
     "37": cytobands_37_path,
