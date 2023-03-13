0e42ae72af388c7e104eb844ac44554426556fe5
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-13 14:06:32 +0100

    Fix an import

--- scout/commands/view/panels.py
+++ scout/commands/view/panels.py
@@ -3,6 +3,7 @@ import logging
 import click
 from flask.cli import with_appcontext
 
+from scout.constants import DATE_DAY_FORMATTER
 from scout.server.extensions import store
 
 LOG = logging.getLogger(__name__)
