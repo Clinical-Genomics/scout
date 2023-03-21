34e1c234e428e12779c425fab98a25e393dc6265
Chiara Rasi <rasi.chiara@gmail.com>
2023-03-21 11:39:37 +0100

    fix_cli

--- scout/commands/load/report.py
+++ scout/commands/load/report.py
@@ -19,7 +19,7 @@ LOG = logging.getLogger(__name__)
 def delivery_report(case_id, report_path, update):
     """Add delivery report to an existing case."""
     try:
-        update_case_report(case_id, report_path, "delivery")
+        update_case_report(case_id, report_path, "delivery_report")
         LOG.info("saved report to case!")
     except Exception as err:
         LOG.error(err)
