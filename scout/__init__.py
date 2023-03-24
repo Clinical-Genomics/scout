1cc2056d1b3d9599e66856e907a77e354b53580a
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2023-03-24 15:52:12 +0100

    Fix #3870 - require importlib metadata explicitly (#3871)
    * Fix #3870 - require importlib metadata explicitly
    
    * except ImportError
    
    * requirements fix not needed with except

--- scout/__init__.py
+++ scout/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:  # Backport support for importlib metadata on Python 3.7
+    from importlib_metadata import version
 
 __version__ = version("scout-browser")
