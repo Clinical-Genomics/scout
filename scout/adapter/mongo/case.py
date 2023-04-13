88023b065f45e38fc19a2ffa1f8e2df412489afe
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2023-04-13 16:37:15 +0200

    more weird chars

--- scout/adapter/mongo/case.py
+++ scout/adapter/mongo/case.py
@@ -576,7 +576,7 @@ class CaseHandler(object):
         """
 
         pipeline = []
-        if institute_id:å
+        if institute_id:
             pipeline.append({"$match": {"collaborators": institute_id}})
         pipeline.append({"$count": "nr_cases"})
         result = {"nr_cases": res["nr_cases"] for res in self.case_collection.aggregate(pipeline)}
