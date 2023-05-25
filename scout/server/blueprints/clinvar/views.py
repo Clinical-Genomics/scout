12a4416cc278b896428c6e194857547fe6c0559b
Chiara Rasi <rasi.chiara@gmacil.com>
2023-05-25 15:38:20 +0200

    Some js errors still

--- scout/server/blueprints/clinvar/views.py
+++ scout/server/blueprints/clinvar/views.py
@@ -2,17 +2,25 @@ import csv
 import logging
 from tempfile import NamedTemporaryFile
 
-from flask import Blueprint, flash, redirect, render_template, request, send_file, url_for
+from flask import (
+    Blueprint,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_file,
+    url_for,
+)
 
 from scout.constants.clinvar import CASEDATA_HEADER, CLINVAR_HEADER, CLNSIG_TERMS
 from scout.server.extensions import store
-from scout.server.utils import institute_and_case
+from scout.server.utils import institute_and_case, templated
 
 from . import controllers
 
 LOG = logging.getLogger(__name__)
 
-blueprint = Blueprint(
+clinvar_bp = Blueprint(
     "clinvar",
     __name__,
     template_folder="templates",
@@ -21,31 +29,40 @@ blueprint = Blueprint(
 )
 
 
-@blueprint.route("/<institute_id>/<case_name>/clinvar/add_variant", methods=["POST"])
+@clinvar_bp.route("/<institute_id>/<case_name>/clinvar/add_variant", methods=["POST"])
+@templated("clinvar/multistep_add_variant.html")
 def clinvar_add_variant(institute_id, case_name):
     """Create a ClinVar submission document in database for one or more variants from a case."""
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     data = {"institute": institute_obj, "case": case_obj, "clinsig_terms": CLNSIG_TERMS}
     controllers.set_clinvar_form(request.form.get("var_id"), data)
-    return render_template("multistep_add_variant.html", **data)
+    return data
 
 
-@blueprint.route("/<institute_id>/<case_name>/clinvar/save", methods=["POST"])
+@clinvar_bp.route("/<institute_id>/<case_name>/clinvar/save", methods=["POST"])
 def clinvar_save(institute_id, case_name):
     """Adds one variant with eventual CaseData observations to an open (or new) ClinVar submission"""
     variant_data = controllers.parse_variant_form_fields(request.form)  # dictionary
-    casedata_list = controllers.parse_casedata_form_fields(request.form)  # a list of dictionaries
+    casedata_list = controllers.parse_casedata_form_fields(
+        request.form
+    )  # a list of dictionaries
 
     # retrieve or create an open ClinVar submission:
     subm = store.get_open_clinvar_submission(institute_id)
     # save submission objects in submission:
     result = store.add_to_submission(subm["_id"], (variant_data, casedata_list))
     if result:
-        flash("An open ClinVar submission was updated correctly with submitted data", "success")
-    return redirect(url_for("cases.case", institute_id=institute_id, case_name=case_name))
+        flash(
+            "An open ClinVar submission was updated correctly with submitted data",
+            "success",
+        )
+    return redirect(
+        url_for("cases.case", institute_id=institute_id, case_name=case_name)
+    )
 
 
-@blueprint.route("/<institute_id>/clinvar_submissions", methods=["GET"])
+@clinvar_bp.route("/<institute_id>/clinvar_submissions", methods=["GET"])
+@templated("clinvar/clinvar_submissions.html")
 def clinvar_submissions(institute_id):
     """Handle clinVar submission objects and files"""
 
@@ -57,10 +74,10 @@ def clinvar_submissions(institute_id):
         "variant_header_fields": CLINVAR_HEADER,
         "casedata_header_fields": CASEDATA_HEADER,
     }
-    return render_template("clinvar_submissions.html", **data)
+    return data
 
 
-@blueprint.route("/<submission>/<case>/rename/<old_name>", methods=["POST"])
+@clinvar_bp.route("/<submission>/<case>/rename/<old_name>", methods=["POST"])
 def clinvar_rename_casedata(submission, case, old_name):
     """Rename one or more casedata individuals belonging to the same clinvar submission, same case"""
 
@@ -69,7 +86,7 @@ def clinvar_rename_casedata(submission, case, old_name):
     return redirect(request.referrer + f"#cdata_{submission}")
 
 
-@blueprint.route("/<submission>/<object_type>", methods=["POST"])
+@clinvar_bp.route("/<submission>/<object_type>", methods=["POST"])
 def clinvar_delete_object(submission, object_type):
     """Delete a single object (variant_data or case_data) associated with a clinvar submission"""
 
@@ -81,8 +98,8 @@ def clinvar_delete_object(submission, object_type):
     return redirect(request.referrer)
 
 
-@blueprint.route("/<submission>/validate")
-@blueprint.route("/<submission>/<save_id>/validate")
+@clinvar_bp.route("/<submission>/validate")
+@clinvar_bp.route("/<submission>/<save_id>/validate")
 def clinvar_validate(submission, save_id=False):
     """Validate a ClinVar submission object using the ClinVar API"""
 
@@ -102,18 +119,20 @@ def clinvar_validate(submission, save_id=False):
     return redirect(request.referrer)
 
 
-@blueprint.route("/<institute_id>/<submission>/update_status", methods=["POST"])
+@clinvar_bp.route("/<institute_id>/<submission>/update_status", methods=["POST"])
 def clinvar_update_submission(institute_id, submission):
     """Update a submission status to open/closed, register an official SUB number or delete the entire submission"""
     controllers.update_clinvar_submission_status(request, institute_id, submission)
     return redirect(request.referrer)
 
 
-@blueprint.route("/<submission>/download/<csv_type>/<clinvar_id>", methods=["GET"])
+@clinvar_bp.route("/<submission>/download/<csv_type>/<clinvar_id>", methods=["GET"])
 def clinvar_download_csv(submission, csv_type, clinvar_id):
     """Download a csv (Variant file or CaseData file) for a clinVar submission"""
 
-    clinvar_file_data = controllers.clinvar_submission_file(submission, csv_type, clinvar_id)
+    clinvar_file_data = controllers.clinvar_submission_file(
+        submission, csv_type, clinvar_id
+    )
 
     if clinvar_file_data is None:
         return redirect(request.referrer)
