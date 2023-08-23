5cc7ffdbb897e2ac655ff2e1c585bc2803d080af
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2023-08-23 12:11:40 +0200

    rename

--- scout/server/blueprints/cases/views.py
+++ scout/server/blueprints/cases/views.py
@@ -97,7 +97,7 @@ def sma(institute_id, case_name):
 
 @cases_bp.route("/<institute_id>/<case_name>/bionano", methods=["GET"])
 @templated("cases/case_bionano.html")
-def sma(institute_id, case_name):
+def bionano(institute_id, case_name):
     """Visualize case BioNano data - FSHD calls"""
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     data = controllers.case(store, institute_obj, case_obj)
