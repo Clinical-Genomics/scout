b339514004ec6b87726b11eafca7db8bf3eb6f4f
Chiara Rasi <rasi.chiara@gmacil.com>
2023-09-25 15:11:43 +0200

    hpo_number as int

--- scout/models/phenotype_term.py
+++ scout/models/phenotype_term.py
@@ -14,7 +14,9 @@ class HpoTerm(BaseModel):
     """
 
     hpo_id: str  # id field in the hpo.obo file
-    hpo_number: Optional[str] = ""  # id field in the hpo.obo file, stripped of the 'HP:' part
+    hpo_number: Optional[
+        int
+    ] = None  # id field in the hpo.obo file, stripped of the 'HP:' part and the zeroes
     description: str  # name field in the hpo.obo file
     ancestors: List = []
     all_ancestors: List = []
@@ -23,7 +25,7 @@ class HpoTerm(BaseModel):
 
     @model_validator(mode="after")
     def set_hpo_number(self) -> "HpoTerm":
-        self.hpo_number = self.hpo_id.split(":")[1]
+        self.hpo_number = int(self.hpo_id.split(":")[1])
         return self
 
 
