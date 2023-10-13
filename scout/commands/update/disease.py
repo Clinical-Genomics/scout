fbce0370c049302b9be1389b4e5e9f77ddd8c013
Chiara Rasi <rasi.chiara@gmacil.com>
2023-10-13 11:52:39 +0200

    Reintroduce files check

--- scout/commands/update/disease.py
+++ scout/commands/update/disease.py
@@ -28,6 +28,17 @@ from scout.utils.scout_requests import (
 LOG = logging.getLogger(__name__)
 
 
+def _check_resources(resources):
+    """Check that resource lines file contain valid data
+    Args:
+        resources(dict): resource names as keys and resource file lines as values
+    """
+    for resname, lines in resources.items():
+        if not lines:
+            LOG.error(f"Resource file '{resname}' doesn't contain valid data.")
+            raise click.Abort()
+
+
 def _fetch_downloaded_resources(resources, downloads_folder):
     """Populate resource lines if a resource exists in downloads folder
 
@@ -86,6 +97,8 @@ def diseases(downloads_folder, api_key):
         except Exception as err:
             LOG.warning(err)
             raise click.Abort()
+
+    _check_resources(resources)
 
     load_disease_terms(
         adapter=adapter,
