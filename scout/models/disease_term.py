194739555d813a30e4cc5c6d4834a93baf4c02c3
Chiara Rasi <rasi.chiara@gmacil.com>
2023-10-13 10:30:43 +0200

    Validate the disease_nr before

--- scout/models/disease_term.py
+++ scout/models/disease_term.py
@@ -9,14 +9,14 @@ class DiseaseTerm(BaseModel):
     """
 
     disease_id: str  # example: OMIM:600233
-    disease_nr: Union[str, int]  # example: 600233
+    disease_nr: int  # example: 600233
     description: str
     source: str
     genes: Optional[List[int]] = []  # List of HGNC IDs
     inheritance: Optional[list] = []
     hpo_terms: Optional[List[str]]  # List of HPO terms associated with the disease
 
-    @field_validator("disease_nr", mode="after")
+    @field_validator("disease_nr", mode="before")
     def set_disease_nr(cls, disease_nr: str) -> Optional[int]:
         if disease_nr:
             return int(disease_nr)
