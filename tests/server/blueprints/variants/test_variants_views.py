bd0b6fc15d0b83dc3495137e0ce00be57be503b4
Eva Caceres <eva.caceres@scilifelab.se>
2023-11-22 15:50:14 +0100

    add fusion variants test

--- tests/server/blueprints/variants/test_variants_views.py
+++ tests/server/blueprints/variants/test_variants_views.py
@@ -254,6 +254,27 @@ def test_str_variants(app, institute_obj, case_obj):
         assert resp.status_code == 200
 
 
+def test_fusion_variants(app, institute_obj, case_obj):
+    # GIVEN an initialized app
+    # GIVEN a valid user and institute
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for("auto_login"))
+        assert resp.status_code == 200
+
+        # WHEN accessing the mei-variants page
+        resp = client.get(
+            url_for(
+                "variants.fusion_variants",
+                institute_id=institute_obj["internal_id"],
+                case_name=case_obj["display_name"],
+            )
+        )
+        # THEN it should return a page
+        assert resp.status_code == 200
+
+
 def test_mei_variants(app, institute_obj, case_obj):
     # GIVEN an initialized app
     # GIVEN a valid user and institute
