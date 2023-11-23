ed94b268b5d97ba96e14a75d3d79f3d57a2759c9
Chiara Rasi <rasi.chiara@gmacil.com>
2023-11-23 11:37:29 +0100

    remove debug test

--- tests/server/blueprints/variants/test_variants_views.py
+++ tests/server/blueprints/variants/test_variants_views.py
@@ -254,9 +254,7 @@ def test_str_variants(app, institute_obj, case_obj):
         assert resp.status_code == 200
 
 
-def test_fusion_variants(
-    app, institute_obj, fusion_case_obj, fusion_variant_objs, one_fusion_variant
-):
+def test_fusion_variants(app, institute_obj, fusion_case_obj, fusion_variant_objs, one_fusion_variant):
     """Test the page that displays a list of RNA fusion variants."""
 
     # GIVEN an initialized app
@@ -280,9 +278,6 @@ def test_fusion_variants(
         )
         # THEN it should return a page
         assert resp.status_code == 200
-
-        # THEN the variant should be found in the list of variants shown on the page
-        assert one_fusion_variant["case"] == fusion_case_obj["_id"]
 
 
 def test_mei_variants(app, institute_obj, case_obj):
