b544b77a4b647fa5478ceac34a129077f440b995
Chiara Rasi <rasi.chiara@gmacil.com>
2023-11-28 12:06:41 +0100

    Fixed also variant images

--- scout/server/blueprints/cases/static/case_images.js
+++ scout/server/blueprints/cases/static/case_images.js
@@ -36,30 +36,6 @@ const CHROMSPECS_LIST =
        {name: 'Y', length: 107, cent_start: 32, cent_length: 4 }]
 
 /**
- * This function fetches an image from disk and displays it in a specified div
- * @param {string} imageUrl - The URL of the image to fetch
- * @param {string} divId - The ID of the div to display the image in
- */
-  function fetchAndDisplayImage(imageUrl, divId, width=none, height=none) {
-		fetch(imageUrl)
-		 .then(response => response.blob())
-		 .then(blob => {
-				 const img = document.createElement("img");
-    		 img.src = URL.createObjectURL(blob);
-				 if (width) {
-				 	img.style.width = width+'px';
-				 }
-				 if (height){
-				 	img.style.height = height+'px';
-				 }
-    		 // Display the image in the specified div
-				 const div = document.getElementById(divId);
-				 div.appendChild(img);
-		})
-		.catch(error => console.error(error));
-}
-
-/**
  * Iterate case.individuals. If a path to a image directory
  * is set, get panels on the page and add image content
  */
