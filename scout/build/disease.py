12c4e4d3a42b80e01485a48aefce8155c3116e68
tereseboderus <terese.boderus@gmail.com>
2023-12-01 15:06:35 +0100

    Build of disease now indexed from disease_id and not disease_nr

--- scout/build/disease.py
+++ scout/build/disease.py
@@ -5,15 +5,18 @@ from scout.models.disease_term import DiseaseTerm
 LOG = logging.getLogger(__name__)
 
 
-def build_disease_term( disease_info: dict, alias_genes: dict = {}) -> dict:
+def build_disease_term(
+    disease_id: str, disease_info: dict, alias_genes: dict = {}
+) -> dict:
     """Build a disease term object."""
-
     disease_obj = {}
-    disease_nr = disease_info.get("mim_number")
-    if disease_nr:
+    disease_nr = disease_id.split(":")[1]
+    disease_source = disease_id.split(":")[0]
+
+    if disease_nr and disease_source:
         disease_obj["disease_nr"] = disease_nr
-        disease_obj["disease_id"] = f"OMIM:{disease_nr}"
-        disease_obj["source"] = "OMIM"
+        disease_obj["disease_id"] = disease_id
+        disease_obj["source"] = disease_source
     for key in ["hpo_terms", "inheritance"]:
         if key in disease_info:
             disease_obj[key] = list(disease_info[key])
@@ -23,23 +26,30 @@ def build_disease_term( disease_info: dict, alias_genes: dict = {}) -> dict:
 
     hgnc_symbols_not_found = set()
     hgnc_ids = set()
-    for hgnc_symbol in disease_info.get("hgnc_symbols", []):
-        if hgnc_symbol is None:
-            continue
-        if hgnc_symbol in alias_genes:
-            # If the symbol identifies a unique gene we add that
-            if alias_genes[hgnc_symbol]["true"]:
-                hgnc_ids.add(alias_genes[hgnc_symbol]["true"])
+    if "hgnc_id" in disease_info:
+        hgnc_ids = hgnc_ids.union(disease_info["hgnc_id"])
+
+    if "hgnc_symbols" in disease_info:
+        for hgnc_symbol in disease_info.get("hgnc_symbols", []):
+
+            if hgnc_symbol is None:
+                LOG.info(f"I did not have hgnc_symbol {disease_id}")
+                continue
+
+            if hgnc_symbol in alias_genes:
+                # If the symbol identifies a unique gene we add that
+                if alias_genes[hgnc_symbol]["true"]:
+                    hgnc_ids.add(alias_genes[hgnc_symbol]["true"])
+                else:
+                    for hgnc_id in alias_genes[hgnc_symbol]["ids"]:
+                        hgnc_ids.add(hgnc_id)
             else:
-                for hgnc_id in alias_genes[hgnc_symbol]["ids"]:
-                    hgnc_ids.add(hgnc_id)
-        else:
-            hgnc_symbols_not_found.add(hgnc_symbol)
+                hgnc_symbols_not_found.add(hgnc_symbol)
+
 
     if hgnc_symbols_not_found:
         LOG.debug(
-            "The following gene symbols could not be found in database: %s",
-            hgnc_symbols_not_found,
+            f"The following gene symbols could not be found in database: {hgnc_symbols_not_found}"
         )
 
     disease_obj["genes"] = list(hgnc_ids)
@@ -47,4 +57,5 @@ def build_disease_term( disease_info: dict, alias_genes: dict = {}) -> dict:
     DiseaseTerm(**disease_obj)
 
     disease_obj["_id"] = disease_obj["disease_id"]
+
     return disease_obj
