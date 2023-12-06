76262fbd3bd8199eea4948226da9b62801dd1444
tereseboderus <terese.boderus@gmail.com>
2023-12-06 12:16:16 +0100

    Add typing to orpha download commands

--- scout/commands/download/orpha.py
+++ scout/commands/download/orpha.py
@@ -1,7 +1,8 @@
 """Code for handling downloading of orphadata files used by scout from CLI"""
 
 import logging
-import pathlib
+from pathlib import Path
+from typing import Dict
 
 import click
 
@@ -10,17 +11,17 @@ from scout.utils.scout_requests import fetch_orpha_files
 LOG = logging.getLogger(__name__)
 
 
-def print_orpha(out_dir: pathlib.Path) -> None:
+def print_orpha(out_dir: Path) -> None:
     """Downloads orphadata product4 (Orphacodes mapped to HPO)
     and product6 (Orphacodes mapped to genes) and writes these as files in
     the specified directory
     """
 
-    orpha_files = fetch_orpha_files()
+    orpha_files: Dict = fetch_orpha_files()
 
-    for key, contents in orpha_files.items():
-        file_name = key + ".xml"
-        file_path = out_dir / file_name
+    for key in orpha_files.keys():
+        file_name: str = key + ".xml"
+        file_path: Path = out_dir / file_name
         LOG.info(f"Print Orphadata to {file_path}")
         with file_path.open("w", encoding="utf-8") as outfile:
             for line in orpha_files[key]:
@@ -29,9 +30,9 @@ def print_orpha(out_dir: pathlib.Path) -> None:
 
 @click.command("orpha", help="Download files from Orphadata")
 @click.option("-o", "--out-dir", default="./", show_default=True)
-def orpha(out_dir):
+def orpha(out_dir: str):
     """Download the ORPHA codes with gene and HPO annotations"""
-    out_dir = pathlib.Path(out_dir)
+    out_dir: Path = Path(out_dir)
     out_dir.mkdir(parents=True, exist_ok=True)
     LOG.info(f"Download ORPHA resources to {out_dir}")
 
