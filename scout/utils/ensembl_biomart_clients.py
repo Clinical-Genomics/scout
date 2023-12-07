20bb45c8f0cf5ffd7877ed7ca2ff129b1a455357
Chiara Rasi <rasi.chiara@gmacil.com>
2023-12-07 11:48:45 +0100

    Small fix

--- scout/utils/ensembl_biomart_clients.py
+++ scout/utils/ensembl_biomart_clients.py
@@ -1,7 +1,9 @@
 import logging
-from typing import Dict, Iterator, List
+from typing import Callable, Dict, Iterator, List
 
+import requests
 from schug.load.biomart import EnsemblBiomartClient
+from schug.load.ensembl import fetch_ensembl_exons, fetch_ensembl_genes, fetch_ensembl_transcripts
 from schug.models.common import Build as SchugBuild
 
 LOG = logging.getLogger(__name__)
@@ -14,84 +16,10 @@ BUILDS: Dict[str, str] = {
     "38": BUILD_38,
 }
 
-CHROMOSOME_NAME_37: str = "Chromosome Name"
-CHROMOSOME_NAME_38: str = "Chromosome/scaffold name"
-ENSEMBL_GENE_ID_37: str = "Ensembl Gene ID"
-ENSEMBL_GENE_ID_38: str = "Gene stable ID"
-
-GENES_FILE_HEADER: Dict[str, List[str]] = {
-    BUILD_37: [
-        CHROMOSOME_NAME_37,
-        "Gene Start (bp)",
-        "Gene End (bp)",
-        ENSEMBL_GENE_ID_37,
-        "HGNC symbol",
-        "HGNC ID(s)",
-    ],
-    BUILD_38: [
-        CHROMOSOME_NAME_38,
-        "Gene start (bp)",
-        "Gene end (bp)",
-        ENSEMBL_GENE_ID_38,
-        "HGNC symbol",
-        "HGNC ID",
-    ],
-}
-
-TRANSCRIPTS_FILE_HEADER: Dict[str, List[str]] = {
-    BUILD_37: [
-        CHROMOSOME_NAME_37,
-        ENSEMBL_GENE_ID_37,
-        "Ensembl Transcript ID",
-        "Transcript Start (bp)",
-        "Transcript End (bp)",
-        "RefSeq mRNA [e.g. NM_001195597]",
-        "RefSeq mRNA predicted [e.g. XM_001125684]",
-        "RefSeq ncRNA [e.g. NR_002834]",
-    ],
-    BUILD_38: [
-        CHROMOSOME_NAME_38,
-        ENSEMBL_GENE_ID_38,
-        "Transcript stable ID",
-        "Transcript start (bp)",
-        "Transcript end (bp)",
-        "RefSeq mRNA ID",
-        "RefSeq mRNA predicted ID",
-        "RefSeq ncRNA ID",
-        "RefSeq match transcript (MANE Select)",
-        "RefSeq match transcript (MANE Plus Clinical)",
-    ],
-}
-
-EXONS_FILE_HEADER: Dict[str, List[str]] = {
-    BUILD_37: [
-        CHROMOSOME_NAME_37,
-        ENSEMBL_GENE_ID_37,
-        "Ensembl Transcript ID",
-        "Ensembl Exon ID",
-        "Exon Chr Start (bp)",
-        "Exon Chr End (bp)",
-        "5' UTR Start",
-        "5' UTR End",
-        "3' UTR Start",
-        "3' UTR End",
-        "Strand",
-        "Exon Rank in Transcript",
-    ],
-    BUILD_38: [
-        CHROMOSOME_NAME_38,
-        ENSEMBL_GENE_ID_38,
-        "Transcript stable ID",
-        "Exon stable ID",
-        "Exon region start (bp)",
-        "Exon region end (bp)",
-        "5' UTR start",
-        "5' UTR end",
-        "3' UTR start",
-        "3' UTR end",
-        "Strand",
-        "Exon rank in transcript",
-    ],
+ENSEMBL_RESOURCE_CLIENT: Dict[str, Callable] = {
+    "genes": fetch_ensembl_genes,
+    "transcripts": fetch_ensembl_transcripts,
+    "exons": fetch_ensembl_exons,
 }
 
 
@@ -108,7 +36,5 @@ class EnsemblBiomartHandler:
             build=SchugBuild(self.build)
         )
         url: str = shug_client.build_url(xml=shug_client.xml)
-
         response = requests.get(url, stream=True)
-        LOG.warning(type(response))
         return response.iter_lines(decode_unicode=True)
