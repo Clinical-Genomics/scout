52e43bbb4c10d0a589321c89604696724daf9073
tereseboderus <terese.boderus@gmail.com>
2023-12-14 11:43:44 +0100

    Updated handling of hgnc_ids for disease

--- scout/build/disease.py
+++ scout/build/disease.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict
+from typing import Dict, List, Set
 
 from scout.models.disease_term import DiseaseTerm
 
@@ -8,6 +8,7 @@ LOG = logging.getLogger(__name__)
 
 def build_disease_term(disease_id: str, disease_info: Dict, alias_genes: Dict) -> Dict:
     """Build a disease term object."""
+
     disease_obj = {}
     disease_nr = disease_id.split(":")[1]
     disease_source = disease_id.split(":")[0]
@@ -20,36 +21,39 @@ def build_disease_term(disease_id: str, disease_info: Dict, alias_genes: Dict) -
         if key in disease_info:
             disease_obj[key] = list(disease_info[key])
 
-    if disease_info.get("description"):
-        disease_obj["description"] = disease_info["description"]
+    disease_obj["description"] = disease_info.get("description", "-")
 
-    hgnc_symbols_not_found = set()
-    hgnc_ids = set()
-    if "hgnc_ids" in disease_info:
-        hgnc_ids = hgnc_ids.union(disease_info["hgnc_ids"])
+    translate_hgnc_symbols_to_ids(disease_info=disease_info, alias_genes=alias_genes)
 
-    if "hgnc_symbols" in disease_info:
-        for hgnc_symbol in disease_info.get("hgnc_symbols", []):
-            if hgnc_symbol is None:
-                continue
-
-            if hgnc_symbol in alias_genes:
-                if alias_genes[hgnc_symbol]["true"]:
-                    hgnc_ids.add(alias_genes[hgnc_symbol]["true"])
-                else:
-                    for hgnc_id in alias_genes[hgnc_symbol]["ids"]:
-                        hgnc_ids.add(hgnc_id)
-            else:
-                hgnc_symbols_not_found.add(hgnc_symbol)
-
-    if hgnc_symbols_not_found:
-        LOG.debug(
-            f"The following gene symbols could not be found in database: {hgnc_symbols_not_found}"
-        )
-
-    disease_obj["genes"] = list(hgnc_ids)
+    disease_obj["genes"] = list(disease_info["hgnc_ids"])
     disease_obj["_id"] = disease_obj["disease_id"]
 
     DiseaseTerm(**disease_obj)
 
     return disease_obj
+
+
+def translate_hgnc_symbols_to_ids(disease_info: Dict, alias_genes: Dict) -> None:
+    """Translates hgnc symbols to hgnc id:s and adds them to disease[hgnc_ids]."""
+    hgnc_ids = disease_info.get("hgnc_ids", set())
+    hgnc_symbols_not_found = set()
+
+    #: If the symbol is present in alias_genes, add the corresponding id
+    for hgnc_symbol in disease_info.get("hgnc_symbols", []):
+        # LOG.info(f"Symbol: {hgnc_symbol} alias genes {alias_genes}")
+        if hgnc_symbol is None:
+            continue
+        if hgnc_symbol in alias_genes:
+            if alias_genes[hgnc_symbol]["true"]:
+                hgnc_ids.add(alias_genes[hgnc_symbol]["true"])
+            else:
+                for hgnc_id in alias_genes[hgnc_symbol]["ids"]:
+                    hgnc_ids.add(hgnc_id)
+        else:
+            hgnc_symbols_not_found.add(hgnc_symbol)
+
+    if hgnc_symbols_not_found:
+        LOG.debug(
+            f"The following gene symbols could not be found in database: {hgnc_symbols_not_found}"
+        )
+    disease_info["hgnc_ids"].update(hgnc_ids)
