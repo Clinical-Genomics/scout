d6f2b056614216121e2bd853974fa2b06906756f
tereseboderus <terese.boderus@gmail.com>
2024-02-13 09:33:46 +0100

    Cleanup

--- scout/server/blueprints/clinvar/static/clinvar_submissions.js
+++ scout/server/blueprints/clinvar/static/clinvar_submissions.js
@@ -1,26 +1,24 @@
 async function getJson(url) {
     try {
+    //Get base url and fetch data
         const baseUrl = window.location.origin
-        console.log(`url: ${baseUrl}${url}`)
-        //Fetch data
         const response = await fetch(`${baseUrl}${url}`)
         if (!response.ok) {
             throw new Error('Failed to fetch')
         }
         const jsonRes = await response.json()
-
 				let fileName=url.split("/").pop()
 
+				//Create and download file
         download(fileName, jsonRes)
-
     } catch (error) {
-
         if (error.toString().includes('Failed to fetch')) {
             console.log(error)
         }
     }
 }
 
+//Function creates a file download from an Object
 function download(filename, object){
     const json= JSON.stringify(object)
     let downloadLink=document.createElement('a')
@@ -32,10 +30,9 @@ function download(filename, object){
     document.body.removeChild(downloadLink)
 }
 
+//Add eventlistener to button for json download of preClinVar response
 document.querySelector("#download_clinvar_json").addEventListener("click",(e)=>{
-
 getJson(e.target.value)
-
 })
 
 
