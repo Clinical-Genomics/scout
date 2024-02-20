58ae241c098939238a2bf068c06d33de1c73639a
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-02-20 15:33:29 +0100

    Fix #4157 - add pin api endpoint

--- scout/server/blueprints/api/views.py
+++ scout/server/blueprints/api/views.py
@@ -19,6 +19,42 @@ def case(institute_id, case_name):
 @api_bp.route("/<institute_id>/<case_name>/<variant_id>")
 def variant(institute_id, case_name, variant_id):
     """Display a specific SNV variant."""
+    variant_obj = store.variant(variant_id)
+    return Response(json_util.dumps(variant_obj), mimetype="application/json")
+
+
+@api_bp.route("/<institute_id>/<case_name>/<variant_id>/pin")
+def pin_variant(institute_id, case_name, variant_id):
+    """Pin an existing variant"""
+
     institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
     variant_obj = store.variant(variant_id)
+
+    user_obj = store.user(current_user.email)
+    link = url_for(
+        "variant.variant",
+        institute_id=institute_id,
+        case_name=case_name,
+        variant_id=variant_id,
+    )
+    store.pin_variant(institute_obj, case_obj, user_obj, link, variant_obj)
+
+    return Response(json_util.dumps(variant_obj), mimetype="application/json")
+
+
+@api_bp.route("/<institute_id>/<case_name>/<variant_id>/unpin")
+def unpin_variant(institute_id, case_name, variant_id):
+    """Un-pin an existing, pinned variant"""
+    institute_obj, case_obj = institute_and_case(store, institute_id, case_name)
+    variant_obj = store.variant(variant_id)
+
+    user_obj = store.user(current_user.email)
+    link = url_for(
+        "variant.variant",
+        institute_id=institute_id,
+        case_name=case_name,
+        variant_id=variant_id,
+    )
+    store.unpin_variant(institute_obj, case_obj, user_obj, link, variant_obj)
+
     return Response(json_util.dumps(variant_obj), mimetype="application/json")
