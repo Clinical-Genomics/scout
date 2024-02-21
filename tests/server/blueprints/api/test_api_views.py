b6234b5c0d1ef53e3a605dbdec2c922003580d45
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-02-21 08:38:29 +0100

    add tests

--- tests/server/blueprints/api/test_api_views.py
+++ tests/server/blueprints/api/test_api_views.py
@@ -45,3 +45,66 @@ def test_variant(app, case_obj, variant_obj):
         # THEN it should return a valid response
         assert resp.status_code == 200
         assert resp.json["_id"] == variant_id
+
+
+def test_pin(app, case_obj, variant_obj):
+    """Test the api endpoint that pins variants"""
+
+    case_name = case_obj["display_name"]
+    variant_id = variant_obj["_id"]
+
+    # GIVEN an initialized app
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        client.get(url_for("auto_login"))
+        # WHEN the API is invoked with the right params
+        resp = client.get(
+            url_for(
+                "api.pin_variant",
+                institute_id=case_obj["owner"],
+                case_name=case_name,
+                variant_id=variant_id,
+                link="pinned it",
+            )
+        )
+        # THEN it should return a valid response
+        assert resp.status_code == 200
+        assert resp.json["_id"] == variant_id
+
+
+def test_unpin(app, institute_obj, case_obj, variant_obj):
+    """Test the api endpoint that unpins variants"""
+
+    case_name = case_obj["display_name"]
+    variant_id = variant_obj["_id"]
+
+    # GIVEN an initialized app
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        client.get(url_for("auto_login"))
+
+        # GIVEN a pinned variant
+        resp = client.get(
+            url_for(
+                "api.pin_variant",
+                institute_id=case_obj["owner"],
+                case_name=case_name,
+                variant_id=variant_id,
+                link="pinned it for testing",
+            )
+        )
+        assert resp.status_code == 200
+
+        # WHEN the API is invoked with the right params
+        resp = client.get(
+            url_for(
+                "api.unpin_variant",
+                institute_id=case_obj["owner"],
+                case_name=case_name,
+                variant_id=variant_id,
+                link="unpinned it",
+            )
+        )
+        # THEN it should return a valid response
+        assert resp.status_code == 200
+        assert resp.json["_id"] == variant_id
