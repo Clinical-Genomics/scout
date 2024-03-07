07720fb6e10f975084d51bd793d04b6ba32c69a5
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-03-07 10:01:17 +0100

    rearrange

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -589,23 +589,22 @@ def case_report_variants(store: MongoAdapter, case_obj: dict, institute_obj: dic
                 var_obj["phenotypes"] = case_obj["partial_causatives"][var_id]
             evaluated_variants_by_type[eval_category].append(_get_decorated_var(var_obj=var_obj))
 
-    def _partition_var_by_type(evaluated_variants_by_type: dict, var_obj: dict):
-        """We collect all evaluated variants except causative, partial causative and suspected variants,
-        then partition them according to event type.
-        Ensure variant actually has the corresponding key set, and that it is not just None.
-        """
-        for eval_category, variant_key in CASE_REPORT_VARIANT_TYPES.items():
-            if variant_key in var_obj and var_obj["variant_key"] is not None:
-                evaluated_variants_by_type[eval_category].append(
-                    _get_decorated_var(var_obj=var_obj)
-                )
-
     for var_obj in store.evaluated_variants(
         case_id=case_obj["_id"], institute_id=institute_obj["_id"]
     ):
         _partition_var_by_type(evaluated_variants_by_type, var_obj)
 
     data["variants"] = evaluated_variants_by_type
+
+
+def _partition_var_by_type(evaluated_variants_by_type: dict, var_obj: dict):
+    """We collect all evaluated variants except causative, partial causative and suspected variants,
+    then partition them according to event type.
+    Ensure variant actually has the corresponding key set, and that it is not just None.
+    """
+    for eval_category, variant_key in CASE_REPORT_VARIANT_TYPES.items():
+        if variant_key in var_obj and var_obj["variant_key"] is not None:
+            evaluated_variants_by_type[eval_category].append(_get_decorated_var(var_obj=var_obj))
 
 
 def case_report_content(store: MongoAdapter, institute_obj: dict, case_obj: dict) -> dict:
