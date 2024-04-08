c9c7e22ae0e737b460b204f96fffb8608400080a
Chiara Rasi <rasi.chiara@gmacil.com>
2024-04-08 10:54:37 +0200

    Keep case views and controllers as they were

--- scout/server/blueprints/cases/controllers.py
+++ scout/server/blueprints/cases/controllers.py
@@ -85,7 +85,7 @@ def phenomizer_diseases(hpo_ids, case_obj, p_value_treshold=1):
         flash("Could not establish a conection to Phenomizer", "danger")
 
 
-def chanjo2_coverage_report(
+def chanjo2_coverage_report_contents(
     institute_obj: dict, case_obj: dict, panel_name: str, panel_id: Optional[str], report_type: str
 ) -> Optional[str]:
     """Retrieve the HTML contents of the Chanjo2 coverage report/overview for a case."""
@@ -136,7 +136,8 @@ def chanjo2_coverage_report(
     }
 
     report_url: str = "/".join([current_app.config.get("CHANJO2_URL"), report_type])
-    redirect(report_url, json=report_query, code=307)
+    response = requests.post(report_url, json=report_query)
+    return response.text
 
 
 def coverage_report_contents(base_url, institute_obj, case_obj):
