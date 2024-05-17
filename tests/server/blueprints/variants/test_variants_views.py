a9b162a682d8bd8f84202a880699edd1ff7c90c3
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-05-17 11:13:14 +0200

    and two more i to _

--- tests/server/blueprints/variants/test_variants_views.py
+++ tests/server/blueprints/variants/test_variants_views.py
@@ -134,13 +134,13 @@ def test_bulk_reset_dismiss_variants(app, institute_obj, case_obj, mocker, mock_
         assert updated_variant.get("dismiss_variant") is None
 
         # 2 variant-relative events should be found in the database
-        assert sum(1 for i in store.event_collection.find({"category": "variant"})) == 2
+        assert sum(1 for _ in store.event_collection.find({"category": "variant"})) == 2
 
         # And one case-specific event should also be created
         assert (
             sum(
                 1
-                for i in store.event_collection.find(
+                for _ in store.event_collection.find(
                     {"category": "case", "verb": "reset_dismiss_all_variants"}
                 )
             )
