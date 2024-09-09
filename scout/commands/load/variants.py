19d618370bafd995a2f5c1a55dea30a48283b5c9
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-09-09 14:48:52 +0200

    click deprecate

--- scout/commands/load/variants.py
+++ scout/commands/load/variants.py
@@ -37,7 +37,8 @@ LOG = logging.getLogger(__name__)
 @click.option(
     "--rank-treshold",
     default=5,
-    help="Specify the rank score threshold. Deprecation warning: this parameter will change names to rank-threshold in a later release.",
+    deprecated=True,
+    help="Specify the rank score threshold. Deprecation warning: this parameter will change name in a later release.",
     show_default=True,
 )
 @click.option("-f", "--force", is_flag=True, help="upload without request")
