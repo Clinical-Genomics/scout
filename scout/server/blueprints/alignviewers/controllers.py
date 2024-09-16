4cd037d8c3a3f59a557ad3df361848f205206c8f
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-09-16 16:16:58 +0200

    Update scout/server/blueprints/alignviewers/controllers.py
    Co-authored-by: Chiara Rasi <rasi.chiara@gmail.com>

--- scout/server/blueprints/alignviewers/controllers.py
+++ scout/server/blueprints/alignviewers/controllers.py
@@ -140,7 +140,6 @@ def make_sashimi_tracks(
     if "37" in str(case_obj.get("rna_genome_build", "38")):
         build = "37"
 
-    variant_obj = None
 
     if variant_id:
         variant_obj = store.variant(document_id=variant_id)
