e9ced70dfe4789a2fc55176eb13ad0d25879cc1b
Lint Action <lint-action@samuelmeuli.com>
2024-10-10 09:49:35 +0000

    Fix code style issues with Black

--- tests/utils/test_acmg.py
+++ tests/utils/test_acmg.py
@@ -429,11 +429,13 @@ def test_acmg_modifier_on_both_benign_and_pathogenic():
     res = get_acmg(acmg_terms)
     assert res == "uncertain_significance"
 
+
 def test_acmg_benign_moderate():
     acmg_terms = {"BP1_Moderate", "BS1"}
     res = get_acmg(acmg_terms)
     assert res == "likely_benign"
-    
+
+
 def test_acmg_temperature():
     acmg_terms = {"PS3_Moderate", "PP1_Moderate", "PP3", "BS1_Supporting"}
     res = get_acmg_temperature(acmg_terms)
