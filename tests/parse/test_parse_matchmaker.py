2178e0df9ef4e1b87f1cf4178fa5e45876b7909d
Chiara Rasi <rasi.chiara@gmacil.com>
2024-10-18 07:47:36 +0200

    Rename test

--- tests/parse/test_parse_matchmaker.py
+++ tests/parse/test_parse_matchmaker.py
@@ -11,8 +11,8 @@ from scout.parse.matchmaker import (
 )
 
 
-def test_parse_datetime_milliseconds():
-    """Test parsing matching results date in timestamp format."""
+def test_parse_datetime_timestamp_milliseconds():
+    """Test parsing matching results date in timestamp milliseconds format."""
     datetime_int = 1729229925000
     assert parse_datetime(datetime_int) == datetime(2024, 10, 18, 7, 38, 45)
 
