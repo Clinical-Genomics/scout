f9e164f159b696c6be2f9ce512323019d47da88a
Chiara Rasi <rasi.chiara@gmacil.com>
2024-10-29 13:28:16 +0100

    It works

--- scout/server/blueprints/dashboard/forms.py
+++ scout/server/blueprints/dashboard/forms.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask_wtf import FlaskForm
 from wtforms import SelectField, StringField, SubmitField, validators
 
-from scout.constants import CASE_SEARCH_TERMS
 from scout.server.blueprints.institutes.forms import CaseFilterForm
-
-CASE_SEARCH_KEY = [("", "")] + [
-    (value["prefix"], value["label"]) for key, value in CASE_SEARCH_TERMS.items()
-]
 
 
 class NonValidatingSelectField(SelectField):
