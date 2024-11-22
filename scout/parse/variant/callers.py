4dfd8275dd083d790604208d2285644e467de42c
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-11-22 11:35:39 +0100

    improve parsing - set filter status exact if single caller, show also on variant page

--- scout/parse/variant/callers.py
+++ scout/parse/variant/callers.py
@@ -13,6 +13,14 @@ def parse_callers(variant, category="snv"):
     2. If a svdb_origin tag (pipe separated) is found, callers listed will be marked Pass
     3. If a set tag (dash separated, GATK CombineVariants) is found, callers will be marked Pass or Filtered accordingly
 
+    If the FILTER status is not PASS (e.g. None - cyvcf2 FILTER is None if VCF file column FILTER is "PASS"),
+    in all of these cases, if a single caller is found, the "Pass" status is replaced with variant filter
+    status. If there is more than one caller, we can't quite tell which one is which, so all will be "Filtered" instead.
+
+    As a final fallback, to accommodate a version range of MIP where no SNV caller was set for GATK,
+    if this is an SNV variant, and no other call was found, set as if it was a GATK call with Pass or filter status.
+
+
     Args:
         variant (cyvcf2.Variant): A variant object
 
@@ -28,40 +36,48 @@ def parse_callers(variant, category="snv"):
     svdb_origin = variant.INFO.get("svdb_origin")
     raw_info = variant.INFO.get("set")
 
+    filter_status_default = "Pass"
+    if variant.FILTER is not None and len(info) == 1:
+        filter_status_default = "Filtered - {}".format(filter_status.replace(";", " - "))
+
     if other_info:
         for info in other_info.split(","):
             called_by = info.split("|")[0]
             if called_by in callers_keys:
-                callers[called_by] = "Pass"
+                callers[called_by] = filter_status_default
     elif svdb_origin:
         for called_by in svdb_origin.split("|"):
             if called_by in callers_keys:
-                callers[called_by] = "Pass"
+                callers[called_by] = filter_status_default
     elif raw_info:
         info = raw_info.split("-")
         for call in info:
             if call == "FilteredInAll":
+                filter_status = filter_status_default
+                if "Filtered" not in filter_status_default:
+                    filter_status = "Filtered"
                 for caller in callers:
-                    callers[caller] = "Filtered"
+                    callers[caller] = filter_status
             elif call == "Intersection":
                 for caller in callers:
-                    callers[caller] = "Pass"
+                    callers[caller] = filter_status_default
             elif "filterIn" in call:
+                filter_status = filter_status_default
+                if "Filtered" not in filter_status_default:
+                    filter_status = "Filtered"
                 for caller in callers:
                     if caller in call:
-                        callers[caller] = "Filtered"
+                        callers[caller] = filter_status
             elif call in callers_keys:
-                callers[call] = "Pass"
+                callers[call] = filter_status_default
 
     if raw_info or svdb_origin or other_info:
         return callers
 
     if category == "snv":
-        # cyvcf2 FILTER is None if VCF file column FILTER is "PASS"
         filter_status = "Pass"
         if variant.FILTER is not None:
             filter_status = "Filtered - {}".format(filter_status.replace(";", " - "))
-
         callers["gatk"] = filter_status
 
     return callers
