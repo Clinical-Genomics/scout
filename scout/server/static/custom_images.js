74453c329c132efde42ea024521cfc2b9a47760a
Chiara Rasi <rasi.chiara@gmacil.com>
2024-12-02 12:05:23 +0100

    Contain custom image size and add a link to show them full size in another tab

--- scout/server/static/custom_images.js
+++ scout/server/static/custom_images.js
@@ -1,3 +1,11 @@
+imgMaxWidth = 1200;
+imgMaxHeight = 1000;
+
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+    return [srcWidth*ratio, srcHeight*ratio];
+}
+
 /**
  * This function fetches an image from disk and displays it in a specified div
  * @param {string} imageUrl - The URL of the image to fetch
@@ -9,15 +17,24 @@
 		 .then(blob => {
 				 const img = document.createElement("img");
 				 img.src = URL.createObjectURL(blob);
+				 if (width > imgMaxWidth || height> imgMaxHeight) {
+				 		var resized_pixels = calculateAspectRatioFit(width, height, imgMaxWidth, imgMaxHeight);
+				 		width = resized_pixels[0];
+				 		height = resized_pixels[1];
+				 }
 				 if (width) {
 				 	img.style.width = width+'px';
 				 }
 				 if (height) {
 				 	img.style.height = height+'px';
 				 }
-				 // Display the image in the specified div
+				 // Add the image to a link and add the link to the div
+				 const link = document.createElement('a');
+				 link.appendChild(img);
+				 link.href = img.src;
+				 link.setAttribute('target', "_blank");
 				 const div = document.getElementById(divId);
-				 div.appendChild(img);
+				 div.appendChild(link);
 		})
 		.catch(error => console.error(error));
 }
