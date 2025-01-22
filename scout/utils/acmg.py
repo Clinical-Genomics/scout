19b0112785e04ef829f43cbc0f39b527a89918f8
Chiara Rasi <rasi.chiara@gmacil.com>
2025-01-22 10:01:35 +0100

    Work in progress

--- scout/utils/acmg.py
+++ scout/utils/acmg.py
@@ -363,11 +363,11 @@ def get_acmg_temperature(acmg_terms: set) -> Optional[dict]:
 
 
 def get_acmg_conflicts(acmg_terms: set) -> list:
-    """Check potential conflict paris, return list of reference strings."""
+    """Check potential conflicting terms, return list of reference strings."""
 
     conflicts = []
-    for t1, t2, reference in ACMG_POTENTIAL_CONFLICTS:
-        if t1 in acmg_terms and t2 in acmg_terms:
+    for conflict_set, reference in ACMG_POTENTIAL_CONFLICTS:
+        if conflict_set.issubset(acmg_terms):
             conflicts.append(reference)
 
     return conflicts
