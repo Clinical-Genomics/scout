e1e5809d521b99b8ec0d2ef03eecad1da8fe9a82
Chiara Rasi <rasi.chiara@gmail.com>
2025-02-26 13:55:21 +0100

    Remove any variant type (#5219)
    * Improve the cli options
    
    * Remove sys messages
    
    * some adjustments
    
    * move lines
    
    * Remove also omics-variants
    
    * delete wts only if specified
    
    * some fixes + changelog
    
    * Restore some code to not break tests
    
    * Thanks SonarQube
    
    * Added a test
    
    * tiny thing
    
    * Removed unused imports
    
    * Small fix
    
    * Update scout/adapter/mongo/query.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Fix code style issues with Black
    
    * Update CHANGELOG.md
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/delete/delete_command.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/delete/delete_command.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update tests/commands/delete/test_delete_cmd.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Update scout/commands/delete/delete_command.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Rename to oulier in another place
    
    * if category encompassing all types of vars
    
    * Fixed test to keep omics variants
    
    * Simplify slightly code
    
    * Adjust a test
    
    * Update scout/commands/delete/delete_command.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * For variants removal, reuse the logic for both dry run and normal run
    
    * Space in changelog
    
    ---------
    
    Co-authored-by: Chiara Rasi <rasi.chiara@gmacil.com>
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- scout/commands/delete/delete_command.py
+++ scout/commands/delete/delete_command.py
@@ -1,15 +1,17 @@
 import logging
+from typing import List, Optional, Tuple
 
 import click
 from flask import current_app, url_for
 from flask.cli import with_appcontext
 
-from scout.constants import CASE_STATUSES
+from scout.adapter.mongo import MongoAdapter
+from scout.constants import ANALYSIS_TYPES, CASE_STATUSES, VARIANTS_TARGET_FROM_CATEGORY
 from scout.server.extensions import store
 
 LOG = logging.getLogger(__name__)
 
-BYTES_IN_ONE_GIGABYTE = 1073741824  # (1024*1024*1024)
+BYTES_IN_ONE_GIGABYTE = 1073741824
 DELETE_VARIANTS_HEADER = [
     "Case n.",
     "Ncases",
@@ -23,8 +25,55 @@ DELETE_VARIANTS_HEADER = [
     "Total variants",
     "Removed variants",
 ]
+VARIANT_CATEGORIES = list(VARIANTS_TARGET_FROM_CATEGORY.keys())
 
-VARIANT_CATEGORIES = ["mei", "snv", "sv", "cancer", "cancer_sv", "str"]
+
+def _set_keep_ctg(keep_ctg: Tuple[str], rm_ctg: Tuple[str]) -> List[str]:
+    """Define the categories of variants that should not be removed."""
+    if keep_ctg and rm_ctg:
+        raise click.UsageError("Please use either '--keep-ctg' or '--rm-ctg', not both.")
+    if keep_ctg:
+        return list(keep_ctg)
+    if rm_ctg:
+        return list(set(VARIANT_CATEGORIES).difference(set(rm_ctg)))
+    return []
+
+
+def get_case_ids(case_file: Optional[str], case_id: List[str]) -> List[str]:
+    """Fetch the _id of the cases to remove variants from."""
+    if case_file and case_id:
+        click.echo(
+            "You should specify either case ID (multiple times if needed) or the path to a text file containing a list of case IDs (one per line)."
+        )
+        return []
+    return (
+        [line.strip() for line in open(case_file).readlines() if line.strip()]
+        if case_file
+        else list(case_id)
+    )
+
+
+def handle_delete_variants(
+    store: MongoAdapter, keep_ctg: List[str], dry_run: bool, variants_query: dict
+) -> Tuple[int]:
+    """Handle variant removal for a case or count how many variants would be removed if it's a simulation.."""
+
+    if dry_run:
+        remove_n_variants = store.variant_collection.count_documents(variants_query)
+        remove_n_omics_variants = (
+            store.omics_variant_collection.count_documents(variants_query)
+            if "outlier" not in keep_ctg
+            else 0
+        )
+    else:
+        remove_n_variants = store.variant_collection.delete_many(variants_query).deleted_count
+        remove_n_omics_variants = (
+            store.omics_variant_collection.delete_many(variants_query).deleted_count
+            if "outlier" not in keep_ctg
+            else 0
+        )
+
+    return remove_n_variants, remove_n_omics_variants
 
 
 @click.command("variants", short_help="Delete variants for one or more cases")
@@ -47,18 +96,25 @@ VARIANT_CATEGORIES = ["mei", "snv", "sv", "cancer", "cancer_sv", "str"]
 @click.option("--older-than", type=click.INT, default=0, help="Older than (months)")
 @click.option(
     "--analysis-type",
-    type=click.Choice(["wgs", "wes", "panel"]),
+    type=click.Choice(ANALYSIS_TYPES),
     multiple=True,
     help="Type of analysis",
 )
 @click.option("--rank-threshold", type=click.INT, default=5, help="With rank threshold lower than")
 @click.option("--variants-threshold", type=click.INT, help="With more variants than")
 @click.option(
+    "--rm-ctg",
+    type=click.Choice(VARIANT_CATEGORIES),
+    multiple=True,
+    required=False,
+    help="Remove only the following categories",
+)
+@click.option(
     "--keep-ctg",
     type=click.Choice(VARIANT_CATEGORIES),
     multiple=True,
     required=False,
-    help="Do not delete one of more variant categories",
+    help="Keep the following categories",
 )
 @click.option(
     "--dry-run",
@@ -68,43 +124,38 @@ VARIANT_CATEGORIES = ["mei", "snv", "sv", "cancer", "cancer_sv", "str"]
 @with_appcontext
 def variants(
     user: str,
-    case_id: list,
+    case_id: tuple,
     case_file: str,
     institute: str,
-    status: list,
+    status: tuple,
     older_than: int,
-    analysis_type: list,
+    analysis_type: tuple,
     rank_threshold: int,
     variants_threshold: int,
-    keep_ctg: list,
+    rm_ctg: tuple,
+    keep_ctg: tuple,
     dry_run: bool,
 ) -> None:
     """Delete variants for one or more cases"""
-
-    if case_file and case_id:
-        click.echo(
-            "You should specify either case ID (multiple times if needed) or the path to a text file containing a list of case IDs (one per line)."
-        )
-        return
 
     user_obj = store.user(user)
     if user_obj is None:
         click.echo(f"Could not find a user with email '{user}' in database")
         return
 
+    case_ids = get_case_ids(case_file=case_file, case_id=case_id)
+
     total_deleted = 0
-    items_name = "deleted variants"
+
     if dry_run:
         click.echo("--------------- DRY RUN COMMAND ---------------")
         items_name = "estimated deleted variants"
     else:
         click.confirm("Variants are going to be deleted from database. Continue?", abort=True)
-
-    if case_file:
-        case_id = [line.strip() for line in open(case_file).readlines() if line.strip()]
+        items_name = "deleted variants"
 
     case_query = store.build_case_query(
-        case_ids=case_id,
+        case_ids=case_ids,
         institute_id=institute,
         status=status,
         older_than=older_than,
@@ -120,10 +171,15 @@ def variants(
         f"Rank-score threshold:{rank_threshold}, case n. variants threshold:{variants_threshold}."
     )
     click.echo("\t".join(DELETE_VARIANTS_HEADER))
+    keep_ctg = _set_keep_ctg(keep_ctg=keep_ctg, rm_ctg=rm_ctg)
+
     for nr, case in enumerate(cases, 1):
+
         case_id = case["_id"]
         institute_id = case["owner"]
-        case_n_variants = store.variant_collection.count_documents({"case_id": case_id})
+        case_n_variants = store.variant_collection.count_documents(
+            {"case_id": case_id}
+        ) + store.omics_variant_collection.count_documents({"case_id": case_id})
         # Skip case if user provided a number of variants to keep and this number is less than total number of case variants
         if variants_threshold and case_n_variants < variants_threshold:
             continue
@@ -136,36 +192,15 @@ def variants(
         variants_to_keep = (
             case.get("suspects", []) + case.get("causatives", []) + evaluated_not_dismissed or []
         )
-        variants_query = store.delete_variants_query(
+
+        variants_query: dict = store.delete_variants_query(
             case_id, variants_to_keep, rank_threshold, keep_ctg
         )
 
-        if dry_run:
-            # Just print how many variants would be removed for this case
-            remove_n_variants = store.variant_collection.count_documents(variants_query)
-            total_deleted += remove_n_variants
-            click.echo(
-                "\t".join(
-                    [
-                        str(nr),
-                        str(n_cases),
-                        case["owner"],
-                        case["display_name"],
-                        case_id,
-                        case.get("track", ""),
-                        str(case["analysis_date"]),
-                        case.get("status", ""),
-                        str(case.get("is_research", "")),
-                        str(case_n_variants),
-                        str(remove_n_variants),
-                    ]
-                )
-            )
-            continue
-
-        # delete variants specified by variants_query
-        result = store.variant_collection.delete_many(variants_query)
-        total_deleted += result.deleted_count
+        remove_n_variants, remove_n_omics_variants = handle_delete_variants(
+            store=store, keep_ctg=keep_ctg, dry_run=dry_run, variants_query=variants_query
+        )
+        total_deleted += remove_n_variants + remove_n_omics_variants
         click.echo(
             "\t".join(
                 [
@@ -179,10 +214,13 @@ def variants(
                     case.get("status", ""),
                     str(case.get("is_research", "")),
                     str(case_n_variants),
-                    str(result.deleted_count),
+                    str(remove_n_variants + remove_n_omics_variants),
                 ]
             )
         )
+
+        if dry_run:  # Do not create an associated event
+            continue
 
         # Create event in database
         institute_obj = store.institute(case["owner"])
