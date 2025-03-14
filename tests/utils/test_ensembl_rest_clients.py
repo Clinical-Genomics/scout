c83eaadc718ec32d03e851ed956253e8633b8362
Chiara Rasi <rasi.chiara@gmacil.com>
2025-03-14 11:57:42 +0100

    Fix liftover

--- tests/utils/test_ensembl_rest_clients.py
+++ tests/utils/test_ensembl_rest_clients.py
@@ -4,31 +4,31 @@ import responses
 from requests.exceptions import MissingSchema
 from requests.models import Response
 
-from scout.utils.ensembl_rest_clients import RESTAPI_37
+from scout.utils.ensembl_rest_clients import RESTAPI_URL
 
 
 @responses.activate
-def test_liftover(ensembl_rest_client_37, ensembl_liftover_response):
+def test_liftover(ensembl_rest_client, ensembl_liftover_response):
     """Test send request for coordinates liftover"""
     # GIVEN a patched response from Ensembl
-    url = f"{RESTAPI_37}/map/human/GRCh37/X:1000000..1000100/GRCh38?content-type=application/json"
+    url = f"{RESTAPI_URL}/map/human/GRCh37/X:1000000..1000100/GRCh38?content-type=application/json"
     responses.add(
         responses.GET,
         url,
         json=ensembl_liftover_response,
         status=200,
     )
-    client = ensembl_rest_client_37
+    client = ensembl_rest_client
     # WHEN sending the liftover request the function should return a mapped locus
     mapped_coords = client.liftover("37", "X", 1000000, 1000100)
     assert mapped_coords[0]["mapped"]
 
 
 @responses.activate
-def test_send_gene_request(ensembl_gene_response, ensembl_rest_client_37):
+def test_send_gene_request(ensembl_gene_response, ensembl_rest_client):
     """Test send request with correct params and endpoint"""
-    url = f"{RESTAPI_37}/overlap/id/ENSG00000103591?feature=gene"
-    client = ensembl_rest_client_37
+    url = f"{RESTAPI_URL}/overlap/id/ENSG00000103591?feature=gene"
+    client = ensembl_rest_client
     responses.add(
         responses.GET,
         url,
@@ -44,13 +44,13 @@ def test_send_gene_request(ensembl_gene_response, ensembl_rest_client_37):
     assert data[0]["end"]
 
 
-def test_send_request_fakey_url(mock_app, ensembl_rest_client_37, mocker):
+def test_send_request_fakey_url(mock_app, ensembl_rest_client, mocker):
     """Test the Ensembl REST client with an URL that is raising missing schema error."""
 
     # GIVEN a completely invalid URL
     url = "fakeyurl"
     # GIVEN a patched Ensembl client
-    client = ensembl_rest_client_37
+    client = ensembl_rest_client
     mocker.patch("requests.get", side_effect=MissingSchema("Invalid URL"))
 
     # THEN the client should return no content
@@ -59,12 +59,12 @@ def test_send_request_fakey_url(mock_app, ensembl_rest_client_37, mocker):
         assert data is None
 
 
-def test_send_request_unavaailable(mock_app, ensembl_rest_client_37, mocker):
+def test_send_request_unavaailable(mock_app, ensembl_rest_client, mocker):
     """Test the Ensembl REST client with an URL that is not available (500 error)."""
 
-    url = f"{RESTAPI_37}/fakeyurl"
+    url = f"{RESTAPI_URL}/fakeyurl"
     # GIVEN a patched Ensembl client
-    client = ensembl_rest_client_37
+    client = ensembl_rest_client
 
     # GIVEN a mocked 550 response from Ensembl
     mock_response = Response()
