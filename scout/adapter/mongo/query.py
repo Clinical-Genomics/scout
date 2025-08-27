ffc73091da3cfb4bc1ad9978f6cf77e8ba84f568
Chiara Rasi <rasi.chiara@gmacil.com>
2025-08-27 07:29:08 +0200

    Remove check if criterion is not None

--- scout/adapter/mongo/query.py
+++ scout/adapter/mongo/query.py
@@ -838,21 +838,19 @@ class QueryHandler(object):
 
             if criterion in ["l2fc", "delta_psi"]:
                 criterion_value = query.get(criterion)
-                if criterion_value is not None:
-                    abs_criterion_value = {
-                        "$or": [
-                            {criterion: {"$gt": criterion_value}},
-                            {criterion: {"$lt": -criterion_value}},
-                            {criterion: {"$exists": False}},
-                            {criterion: None},
-                        ]
-                    }
-                    mongo_secondary_query.append(abs_criterion_value)
+                abs_criterion_value = {
+                    "$or": [
+                        {criterion: {"$gt": criterion_value}},
+                        {criterion: {"$lt": -criterion_value}},
+                        {criterion: {"$exists": False}},
+                        {criterion: None},
+                    ]
+                }
+                mongo_secondary_query.append(abs_criterion_value)
 
             if criterion == "p_value":
                 p_value = query.get("p_value")
-                if p_value is not None:
-                    mongo_secondary_query.append({"p_value": {"$lt": p_value}})
+                mongo_secondary_query.append({"p_value": {"$lt": p_value}})
 
         return mongo_secondary_query
 
