256148d0ed22ad6d60979ca633a1ed66a382a93b
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2025-11-05 07:18:50 +0100

    Fix #5837 - chromosome order hg38

--- scout/constants/igv_tracks.py
+++ scout/constants/igv_tracks.py
@@ -48,6 +48,9 @@ HUMAN_REFERENCE_38 = {
     "indexURL": HG38REF_INDEX_URL,
     "cytobandURL": HG38CYTOBAND_URL,
     "aliasURL": HG38ALIAS_URL,
+    "chromosomeOrder": (
+        "chr1, chr2, chr3, chr4, chr5, chr6, chr7, chr8, chr9, chr10, chr11, chr12, chr13, chr14, chr15, chr16, chr17, chr18, chr19, chr20, chr21, chr22, chrX, chrY, chrM"
+    ),
 }
 
 # igv.js track settings common for all users and all cases. Selectable by users
