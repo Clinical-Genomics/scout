005234c92591bd364304eed6e7f5b455f35d8e69
Chiara Rasi <rasi.chiara@gmacil.com>
2025-12-08 11:44:41 +0100

    Add a test

--- tests/commands/delete/test_delete_cmd.py
+++ tests/commands/delete/test_delete_cmd.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from scout.commands import cli
+from scout.commands.delete.delete_command import CASE_RNA_KEYS
 from scout.server.extensions import store
 
 VARIANTS_QUERY = {"rank_score": {"$lt": 0}}
@@ -670,3 +671,22 @@ def test_delete_case_correct_institute(empty_mock_app, case_obj):
     assert store.case_collection.find_one() is None
     ## THEN assert the CLI exits without problems
     assert result.exit_code == 0
+
+
+def test_delete_rna(empty_mock_app, case_obj):
+    """Test the command that is removing RNA data from a case."""
+
+    # GIVEN a case with RNA data
+    assert any(key in case_obj and case_obj[key] for key in CASE_RNA_KEYS)
+    store.case_collection.insert_one(case_obj)
+
+    mock_app = empty_mock_app
+    runner = mock_app.test_cli_runner()
+
+    # WHEN data is erased using the cli
+    result = runner.invoke(cli, ["delete", "rna", "-c", case_obj["_id"]], input="y\n")
+
+    # THEN RNA data is removed from the case
+    assert result.exit_code == 0
+    updated_case = store.case_collection.find_one({"_id": case_obj["_id"]})
+    assert all(not updated_case.get(key) for key in CASE_RNA_KEYS)
