3d3e9c856a7a873ab6c5fd6c969817e402325742
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-02-02 13:44:06 +0100

    Fix #6003 - parsing reduced penetrance from HPO genes to phenotypes

--- scout/parse/hpo_terms.py
+++ scout/parse/hpo_terms.py
@@ -4,6 +4,7 @@ https://raw.githubusercontent.com/obophenotype/human-phenotype-ontology/master/h
 """
 
 import logging
+from typing import Iterable
 
 LOG = logging.getLogger(__name__)
 
@@ -114,19 +115,11 @@ def get_all_ancestors(hpo_tree, term, res=set()):
         return get_all_ancestors(hpo_tree, ancestor_term, res)
 
 
-def parse_hpo_genes(hpo_lines):
-    """Parse HPO gene information
-
-    Args:
-        hpo_lines(iterable(str))
-
-    Returns:
-        genes(dict): A dictionary with hgnc symbols as keys
+def parse_hpo_genes(hpo_lines: Iterable[str]) -> dict:
+    """Parse HPO gene information.
+    The first line in the genes_to_phenotype file is a header
     """
-    LOG.info("Parsing HPO genes ...")
-    genes = {}
     for index, line in enumerate(hpo_lines):
-        # First line is header
         if index == 0:
             continue
         if len(line) < 5:
@@ -134,61 +127,38 @@ def parse_hpo_genes(hpo_lines):
         gene_info = parse_hpo_gene(line)
         hgnc_symbol = gene_info["hgnc_symbol"]
         description = gene_info["description"]
+        hpo_term_id = gene_info["hpo_id"]
 
         if hgnc_symbol not in genes:
             genes[hgnc_symbol] = {"hgnc_symbol": hgnc_symbol}
 
         gene = genes[hgnc_symbol]
-        if description == "Incomplete penetrance":
+        if (
+            description in ("Incomplete penetrance", "Typified by incomplete penetrance")
+            or hpo_term_id == "HP:0003829"
+        ):
             gene["incomplete_penetrance"] = True
-        if description == "Autosomal dominant inheritance":
-            gene["ad"] = True
-        if description == "Autosomal recessive inheritance":
-            gene["ar"] = True
-        if description == "Mithochondrial inheritance":
-            gene["mt"] = True
-        if description == "X-linked dominant inheritance":
-            gene["xd"] = True
-        if description == "X-linked recessive inheritance":
-            gene["xr"] = True
-        if description == "Y-linked inheritance":
-            gene["x"] = True
-        if description == "X-linked inheritance":
-            gene["y"] = True
-    LOG.info("Parsing done.")
+
     return genes
 
 
-def parse_hpo_gene(hpo_line):
-    """Parse hpo gene information
-
-    Args:
-        hpo_line(str): A iterable with hpo phenotype lines
-
-    Yields:
-        hpo_info(dict)
-    """
+def parse_hpo_gene(hpo_line: str) -> dict:
+    """Parse hpo gene information"""
     if not len(hpo_line) > 3:
         return {}
     hpo_line = hpo_line.rstrip().split("\t")
     hpo_info = {}
     hpo_info["hgnc_symbol"] = hpo_line[1]
-    hpo_info["description"] = hpo_line[2]
-    hpo_info["hpo_id"] = hpo_line[3]
+    hpo_info["hpo_id"] = hpo_line[2]
+    hpo_info["description"] = hpo_line[3]
 
     return hpo_info
 
 
-def get_incomplete_penetrance_genes(hpo_lines):
+def get_incomplete_penetrance_genes(hpo_lines: Iterable[str]) -> set:
     """Get a set with all genes that have incomplete penetrance according to HPO
 
-    Args:
-        hpo_lines(iterable(str))
-
-    Returns:
-        incomplete_penetrance_genes(set): A set with the hgnc symbols of all
-                                          genes with incomplete penetrance
-
+    Returns a set with the hgnc symbols of all genes with incomplete penetrance
     """
     genes = parse_hpo_genes(hpo_lines)
     incomplete_penetrance_genes = set()
