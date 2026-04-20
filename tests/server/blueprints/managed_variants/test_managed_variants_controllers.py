ecd8373fd714a8923448b87d75cd1b65e3bf29a6
Lint Action <lint-action@samuelmeuli.com>
2026-04-20 10:06:21 +0000

    Fix code style issues with Black

--- tests/server/blueprints/managed_variants/test_managed_variants_controllers.py
+++ tests/server/blueprints/managed_variants/test_managed_variants_controllers.py
@@ -7,9 +7,7 @@ def test_upload_managed_variants(adapter, managed_variants_lines):
     # when calling upload_managed_variants for these lines
     test_user = "noone"
 
-    result = controllers.upload_managed_variants(
-        adapter, managed_variants_lines, test_user
-    )
+    result = controllers.upload_managed_variants(adapter, managed_variants_lines, test_user)
     # THEN all lines are correctly parsed
     assert result[0] == 3
     assert result[1] == 3
