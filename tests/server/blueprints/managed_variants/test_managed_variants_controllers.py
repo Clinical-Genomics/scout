4cea94811433c3282064fe6324969f321393c768
Chiara Rasi <rasi.chiara@gmacil.com>
2026-04-20 12:05:55 +0200

    Fix test accordingly

--- tests/server/blueprints/managed_variants/test_managed_variants_controllers.py
+++ tests/server/blueprints/managed_variants/test_managed_variants_controllers.py
@@ -6,10 +6,9 @@ from scout.server.blueprints.managed_variants import controllers
 def test_upload_managed_variants(adapter, managed_variants_lines):
     # when calling upload_managed_variants for these lines
     test_user = "noone"
-    test_institutes = ["cust000"]
 
     result = controllers.upload_managed_variants(
-        adapter, managed_variants_lines, test_institutes, test_user
+        adapter, managed_variants_lines, test_user
     )
     # THEN all lines are correctly parsed
     assert result[0] == 3
