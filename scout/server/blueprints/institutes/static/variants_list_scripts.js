7ab1e858671b79ab44b4dc5401c024ceed2c81cf
Chiara Rasi <rasi.chiara@gmail.com>
2026-04-21 15:27:46 +0200

    Export causatives and verified variants to managed variants infile (#6205)
    * Download causatives and verified as managed vars input
    
    * Fix code style issues with Black
    
    * unused line
    
    * Remove type hints
    
    * Add header
    
    * The file needs a header, added
    
    * Stop the spinner from spinning after downloading the file
    
    * Use globalThis instead of window
    
    * Specify endpoint method, thanks linter
    
    * Fixed loading cancer snv
    
    * Make it works also for verified variants
    
    * Fix code style issues with Black
    
    * Fix changelog
    
    * Fixed parsing of institute
    
    * Fix parsing of institute, also in docs
    
    * Shorten ref and alt cells which can be super long for SVs
    
    * Fix the parsing for good
    
    * Fix code style issues with Black
    
    * Trigger tests
    
    * Works. Added a test
    
    * Fix code style issues with Black
    
    * Trigger tests
    
    * Fix according to review
    
    * Fix code style issues with Black
    
    * Trigger tests
    
    * Trigger tests
    
    * Fix test accordingly
    
    * Fix code style issues with Black
    
    * Trigger tests
    
    * Add link to variant also
    
    * Fix code style issues with Black
    
    * Trigger tets
    
    * Trigger test
    
    * Fix test
    
    * Update scout/server/blueprints/institutes/views.py
    
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>
    
    * Fix export with cancer causatives
    
    * Full link, no relative link
    
    * Fix code style issues with Black
    
    * Circular imports
    
    * Use the pretty_variant filter
    
    * Fix test accordingly
    
    * Fix code style issues with Black
    
    * Retrigger tests
    
    * Replace variant ID with constant
    
    * Cut a line
    
    ---------
    
    Co-authored-by: Chiara Rasi <rasi.chiara@gmacil.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Daniel Nilsson <daniel.k.nilsson@gmail.com>

--- scout/server/blueprints/institutes/static/variants_list_scripts.js
+++ scout/server/blueprints/institutes/static/variants_list_scripts.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
-  $('#variants_table').DataTable( {
+  $('#variants_table').DataTable({
       paging: false,
-			searching: true,
-			layout: {
-				topStart: 'buttons',
-				topEnd: {
-            search: {
-                placeholder: 'Search'
-            }
-        }
-			},
+      searching: true,
+      layout: {
+          topStart: 'buttons',
+          topEnd: {
+              search: {
+                  placeholder: 'Search'
+              }
+          }
+      },
       buttons: [
           'copyHtml5',
           'excelHtml5',
+          {
+              text: 'Managed Variants infile',
+              className: 'btn btn-primary',
+              action: function () {
+              		$(globalThis).off('beforeunload');
+									globalThis.location.href = exportAsManagedUrl;
+              }
+          }
       ]
-  } );
+  });
 
   $('[data-toggle="tooltip"]').tooltip();
-} );
+});
