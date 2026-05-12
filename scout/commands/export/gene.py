daf6bbb7080c748a0d27875db616fcae8d28373b
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-05-12 13:22:47 +0200

    unused imports

--- scout/commands/export/gene.py
+++ scout/commands/export/gene.py
@@ -4,8 +4,6 @@ import click
 from bson.json_util import dumps
 from flask.cli import with_appcontext
 
-from scout.commands.utils import builds_option
-from scout.export.gene import export_genes
 from scout.server.extensions import store
 
 from .utils import build_option, json_option
