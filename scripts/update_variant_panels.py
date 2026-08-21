e840c95a70d8f42657a77c8297224cba56410425
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-08-21 13:50:47 +0200

    Add a couple more tests, and safe up the variant hgnc_ids retrieval with get

--- scripts/update_variant_panels.py
+++ scripts/update_variant_panels.py
@@ -94,7 +94,7 @@ def update_panels(context, mongodb, username, password, authdb, host, port, logl
 
         for variant_obj in variants:
             panel_names = set()
-            for hgnc_id in variant_obj["hgnc_ids"]:
+            for hgnc_id in variant_obj.get("hgnc_ids", []):
                 gene_panels = gene_to_panels.get(hgnc_id, set())
                 panel_names = panel_names.union(gene_panels)
 
