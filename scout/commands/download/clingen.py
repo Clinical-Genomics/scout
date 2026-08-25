f85b71f00a41425a8719d9dd166ce9ab37b60fe4
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-08-25 13:52:22 +0200

    Fix #6509 - download ClinGen dosage and gene-disease csv files (#6512)
    * Fix #6509 - download ClinGen dosage and gene-disease csv files
    
    * ClinGen file dict
    
    * typo
    
    * Add test - and fix typo accordingly
    
    * changelog pr id
    
    * Add to download everything. Add reduced resource files.
    
    * resource filenames lowcase
    
    * Manual edit to reduced files

---
+++ scout/commands/download/clingen.py
@@ -0,0 +1,50 @@
+import logging
+import pathlib
+
+import click
+
+from scout.commands.utils import outdir_option
+from scout.utils.scout_requests import fetch_clingen_disease, fetch_clingen_dosage
+
+LOG = logging.getLogger(__name__)
+
+CLINGEN_FILES = [
+    {
+        "desc": "ClinGen Gene-Disease Validity Curations",
+        "file_name": "clingen-gene-disease-summary.csv",
+        "info_key": "disease",
+    },
+    {
+        "desc": "ClinGen Dosage Sensitivity Curations",
+        "file_name": "clingen-dosage-sensitivity.csv",
+        "info_key": "dosage",
+    },
+]
+
+
+def print_clingen(out_dir: str, clingen_info: dict) -> None:
+    """Download ClinGen dosage sensitivity (regions and genes) and disease-gene files."""
+    out_dir = pathlib.Path(out_dir)
+    out_dir.mkdir(parents=True, exist_ok=True)
+    LOG.info("Download ClinGen resources to %s", out_dir)
+
+    for clingen_file in CLINGEN_FILES:
+        file_name = clingen_file["file_name"]
+        file_path = out_dir / file_name
+        LOG.info("Download %s ClinGen file to %s", clingen_file["desc"], file_path)
+        with file_path.open("w", encoding="utf-8") as outfile:
+            for line in clingen_info[clingen_file["info_key"]]:
+                outfile.write(line + "\n")
+
+
+@click.command("clingen", help="Download ClinGen files")
+@outdir_option
+def clingen(out_dir: str) -> None:
+    """Download ClinGen dosage sensitivity (regions and genes) and disease-gene files."""
+
+    clingen_info = {
+        "dosage": fetch_clingen_dosage(),
+        "disease": fetch_clingen_disease(),
+    }
+
+    print_clingen(out_dir, clingen_info)
