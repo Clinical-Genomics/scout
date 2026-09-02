9269b36a665bd726e8c786ef76312347c4d1846b
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2026-09-02 11:04:20 +0200

    heh, destination key..

--- scout/models/omics_variant.py
+++ scout/models/omics_variant.py
@@ -237,10 +237,10 @@ class OmicsVariantLoader(BaseModel):
                 values[symbol_alias] = [str(values.get(symbol_alias))]
 
         if "hgncId" in values:
-            convert_to_list(values, "hgncId")
+            convert_to_hgncId_list(values, "hgncId")
 
         if "hgnc_id" in values and not values.get("hgncId"):
-            convert_to_list(values, "hgnc_id")
+            convert_to_hgncId_list(values, "hgnc_id")
 
         if "cpg_label" in values and not values.get("hgncId"):
             convert_cpg_label(values)
@@ -305,12 +305,12 @@ def get_qualification(values: dict) -> str:
     return qualification
 
 
-def convert_to_list(values, key_name):
+def convert_to_hgncId_list(values, key_name):
     """Test each entry with a try / except to avoid ValueErrors when converting to int, and log a warning if the conversion fails."""
     try:
-        values[key_name] = [int(values.get(key_name))]
+        values["hgncId"] = [int(values.get(key_name))]
     except (ValueError, TypeError):
-        values[key_name] = None
+        values["hgncId"] = None
         LOG.warning(
             "Blank or invalid %s (%s) found in omics variant.",
             key_name,
