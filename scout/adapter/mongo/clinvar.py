f7346e04a12c1ff053d8cfd8c3e573cfa055a213
Lint Action <lint-action@samuelmeuli.com>
2026-09-24 07:59:36 +0000

    Fix code style issues with Black

--- scout/adapter/mongo/clinvar.py
+++ scout/adapter/mongo/clinvar.py
@@ -244,7 +244,8 @@ class ClinVarHandler(object):
         limit: int = 15,
     ) -> tuple[list[dict], int]:
         """Collect open and closed ClinVar submissions of type oncogenicity or germline for an institute.
-        Further filter and limit the submissions returned by submission id, gene symbol or skip/limit values."""
+        Further filter and limit the submissions returned by submission id, gene symbol or skip/limit values.
+        """
 
         query = {"institute_id": institute_id, "type": type}
 
